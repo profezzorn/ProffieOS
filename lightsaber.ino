7b58bbc1d1cddedf5f31c86bab8628b56b439374
hubbe <>
2017-05-24 03:07:54 +0000

    comment

--- lightsaber.ino
+++ lightsaber.ino
@@ -28,9 +28,11 @@
 
 
 // Board version
-#define VERSION_MAJOR 2
-#define VERSION_MINOR 3
+#define VERSION_MAJOR 1
+#define VERSION_MINOR 0
 
+// Number of simultaneously connected blades.
+// (For interchangeable blades, see the blades[] array.)
 #define NUM_BLADES 1
 
 // If you have two 144 LED/m strips in your blade, connect
