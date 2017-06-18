98d406c102a1ba5a5c6353ebc660f8911a0ee07b
hubbe <>
2017-06-18 19:41:00 +0000

    gplv3

--- lightsaber.ino
+++ lightsaber.ino
@@ -1,26 +1,21 @@
 /*
  Teensy Lightsaber Firmware
  http://fredrik.hubbe.net/lightsaber/teensy_saber.html
- Copyright (c) 2016 Fredrik Hubinette
+ Copyright (c) 2016-2017 Fredrik Hubinette
  Additional copyright holders listed inline below.
 
- Permission is hereby granted, free of charge, to any person obtaining a copy
- of this software and associated documentation files (the "Software"), to deal
- in the Software without restriction, including without limitation the rights
- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- copies of the Software, and to permit persons to whom the Software is
- furnished to do so, subject to the following conditions:
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
 
- The above copyright notice and this permission notice shall be included in
- all copies or substantial portions of the Software.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
 
- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- THE SOFTWARE.
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 // You can have multiple configuration files, and specify which one
@@ -1466,7 +1461,7 @@ public:
     x1 = x0 + (((int32_t)synthK1*u0) >> 15);
     x0 = u0;
 
-    return u0 << 4;
+    return u0 << 5;
   }
 
   // Very very stupid upsamler, slightly better than
