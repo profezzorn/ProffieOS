bf7d2a5efafa77e769a8fc9abb768a27f456bd1e
hubbe <>
2017-11-10 06:16:40 +0000

    RGBCycle

--- lightsaber.ino
+++ lightsaber.ino
@@ -4526,6 +4526,32 @@ private:
   float pos_ = 0.0;
 };
 
+// Very fast Red, Green, Blue cycle, result should essentially be white
+// until you start swinging it around.
+class RGBCycle {
+public:
+  void run(BladeBase* base) {
+    if (millis() == millis_at_last_call_)
+      return;
+    millis_at_last_call_ = millis();
+    n_++;
+    if (n_ >= 3) n_ = 0;
+  }  
+  OverDriveColor getColor(int led) {
+    OverDriveColor ret;
+    ret.overdrive = false;
+    switch (n_) {
+      case 0: ret.c = Color16(65535, 0, 0); break;
+      case 1: ret.c = Color16(0, 65535, 0); break;
+      case 2: ret.c = Color16(0, 0, 65535); break;
+    }
+    return ret;
+  }
+private:
+  uint32_t millis_at_last_call_;
+  uint8_t n_;
+};
+
 // Let's us use a different color right in the beginning.
 template<class T, class SPARK_COLOR = Rgb<255,255,255>, int MILLIS = 200>
 class OnSpark {
