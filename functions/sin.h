d99b8889bc2ed79014b467308871b3e591ed5a1a
hubbe <>
2018-02-12 00:02:52 +0000

    updated sin function

--- functions/sin.h
+++ functions/sin.h
@@ -1,9 +1,10 @@
 #ifndef FUNCTIONS_SIN_H
 #define FUNCTIONS_SIN_H
 
-// Usage: Sin<RPM, AMPLITUDE>
-// pulses between 0-amplitude RPM times per minute
-// AMPLITUDE: INTEGER (defaults to Int<32768>)
+// Usage: Sin<RPM, LOW, HIGH>
+// pulses between LOW - HIGH RPM times per minute
+// LOW: INTEGER (defaults to Int<0>)
+// HIGH: INTEGER (defaults to Int<32768>)
 // RPM: INTEGER
 // return value: INTEGER
 
@@ -12,23 +13,28 @@
 
 class BladeBase;
 
-template<class RPM, class AMPLITUDE=Int<32768>>
+template<class RPM, class LOWclass=Int<0>, class HIGHclass=Int<32768>>
 class Sin {
 public:
   void run(BladeBase* blade) {
     rpm_.run(blade);
-    amplitude_.run(blade);
+    high_.run(blade);
+    low_.run(blade);
 
     uint32_t now = micros();
     uint64_t delta = now - last_micros_;
     last_micros_ = now;
     pos_ = fract(pos_ + delta / 60000000.0 * rpm_.getInteger());
-    value_ = (sin_table[(int)floor(pos_ * 1024)] + 16384) * amplitude_.getInteger() >> 15;
+    int high = high_.getInteger();
+    int low = low_.getInteger();
+    float tmp = sin_table[(int)floor(pos_ * 1024)] / 32768.0;
+    value_ = (int)( (tmp + 0.5) * (high - low) + low );
   }
   int getInteger(int led) { return value_; }
 private:
   RPM rpm_;
-  AMPLITUDE amplitude_;
+  LOWclass low_;
+  HIGHclass high_;
   int value_;
   uint32_t last_micros_;
   float pos_;
