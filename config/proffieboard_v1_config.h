bdf8fa3190042c5d1f3746cb59c30b80f815f735
hubbe <>
2018-05-13 07:39:59 +0000

    proffieboard pin map configured

--- config/proffieboard_v1_config.h
+++ config/proffieboard_v1_config.h
@@ -11,50 +11,52 @@
 
 // Proffieboard pin map
 enum SaberPins {
-  // I2S (fixed)
-  bclkPin = 3,                    // BCLK (digital audio)  PB13
+  // I2S
+  bclkPin = 3,                   // BCLK (digital audio)   PB13
   txd0Pin = 5,                   // TXD0 (digital audio)   PB15
   lrclkPin = 2,                  // LRCLK (digital audio)  PB12
 
-  // I2C (fixed)
-  i2cDataPin = 20,                // I2C bus, Used by motion sensors  PB7
-  i2cClockPin = 21,               // I2C bus, Used by motion sensors  PB6
+  // I2C
+  i2cDataPin = 7,                 // I2C bus, Used by motion sensors  PB9
+  i2cClockPin = 30,               // I2C bus, Used by motion sensors  PA9
 
-  // Buttons (don't change)
+  // Buttons
   powerButtonPin = 21,            // power button  PB6
   auxPin = 23,                    // AUX button    PB5
   aux2Pin = 22,                   // AUX2 button   PB4
 
-  // Memory card (fixed)
-  sdCardSelectPin = 10,           // PA8
+  // Memory card
+  sdCardSelectPin = 14,           // PA4
   spiDataOut = 11,                // PA7
   spiDataIn = 12,                 // PA6
   spiClock = 13,                  // PA1
 
-  // These may be changed
-  amplifierPin = 26,              // Amplifier enable pin
-  boosterPin = 38,                // Booster enable pin
-  motionSensorInterruptPin = 2,   // motion sensor interrupt (TeensySaber V2)   // TODO: is this correct?
+  amplifierPin = 26,              // Amplifier enable pin PH1
+  boosterPin = 38,                // Booster enable pin   PH0
+  motionSensorInterruptPin = 39,   // motion sensor interrupt PC13
 
-  // Not sure if these should be free pins or what...
-  spiLedSelect = -1,              // APA102/dotstar chip select
-  spiLedDataOut = 7,
-  spiLedClock = 8,
+  // No fastled support yet
+  spiLedSelect = -1,
+  spiLedDataOut =-1,
+  spiLedClock = -1,
 
   // Neopixel pins
-  bladePin = 16,                  // blade control, either WS2811 or PWM  PA0_TIM2_CH1
+  bladePin = 16,                  // blade control, either WS2811 or PWM  PA4
   bladeIdentifyPin = 16,          // blade identify input / FoC
-  blade2Pin = 17,                 // PA3 TIM2 CH4
-  blade3Pin = -1,                 //     TIM2 CH
+  blade2Pin = 1,                  // PB10
+  blade3Pin = 0,                  // PB11
 
   // Blade power control
-  bladePowerPin1 = 18,            // PB0 TIM1 CH2N
-  bladePowerPin2 = -1,            // TIM1
-  bladePowerPin3 = 14,            // blade power control    NO TIMER AVAILABLE!!!!!
-  bladePowerPin4 = 9,             // blade power control    TIM2 CH3
-  bladePowerPin5 = 15,            // blade power control   TIM2 CH1!!!!! PA5!!!
-  bladePowerPin6 = 24,            // blade power control   TIM2 CH2
+  bladePowerPin1 = 17,            // blade power control PA3
+  bladePowerPin2 = 31,            // blade power control PA10
+  bladePowerPin3 = 18,            // blade power control PB0
+  bladePowerPin4 = 10,            // blade power control PA8
+  bladePowerPin5 = 4,             // blade power control PB5
+  bladePowerPin6 = 6,             // blade power control PB6
 
   // Analog pins
-  batteryLevelPin = 5,           // battery level input
+  batteryLevelPin = 19,           // battery level input PB1
+
+  FreePin1 = 15,  // PA5
+  FreePin2 = 24,  // PB3
 };
