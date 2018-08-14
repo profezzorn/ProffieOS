ba3d428b082f70fb07695ea99e09726573a70705
hubbe <>
2018-08-14 04:38:48 +0000

    break InOutHelper into smaller pieces

--- functions/ifon.h
+++ functions/ifon.h
@@ -28,4 +28,39 @@ private:
   bool on_;
 };
 
+// InOutFunc<OUT_MILLIS, IN_MILLIS>
+// IN_MILLIS, OUT_MILLIS: a number
+// RETURN VALUE: FUNCTION
+// 0 when off, 32768 when on, takes OUT_MILLIS to go from 0 to 32768
+// takes IN_MILLIS to go from 32768 to 0.
+template<int OUT_MILLIS, int IN_MILLIS>
+class InOutFunc {
+public:
+  void run(BladeBase* blade) {
+    uint32_t now = micros();
+    uint32_t delta = now - last_micros_;
+    last_micros_ = now;
+    if (blade->is_on()) {
+      if (extension == 0.0) {
+         // We might have been off for a while, so delta might
+         // be insanely high.
+         extension = 0.00001;
+      } else {
+         extension += delta / (OUT_MILLIS * 1000.0);
+         extension = min(extension, 1.0f);
+      }
+    } else {
+      extension -= delta / (IN_MILLIS * 1000.0);
+      extension = max(extension, 0.0f);
+    }
+    ret_ = extension * 32768.0;
+  }
+  int getInteger(int led) { return ret_; }
+
+private:
+  float extension = 0.0;
+  uint32_t last_micros_;
+  int ret_;
+};
+
 #endif
--- styles/inout_helper.h
+++ styles/inout_helper.h
@@ -1,43 +1,26 @@
 #ifndef STYLES_INOUT_HELPER_H
 #define STYLES_INOUT_HELPER_H
 
-// Usage: InOutHelper<BASE, OUT_MILLIS, IN_MILLIS>
-// or: InOutHelper<BASE, OUT_MILLIS, IN_MILLIS, OFF_COLOR>
+// InOutHelperX<BASE, EXTENSION, OFF_COLOR>
 // BASE, OFF_COLOR: COLOR
-// OUT_MILLIS, IN_MILLIS: a number
+// EXTENSION: FUNCTION
 // return value: COLOR
 // This class does a basic extend/retract. Basically it fades between
-// BASE and OFF_COLOR (which defaults to black). It starts by just
-// displaying OFF_COLOR, and when you turn the saber on it starts mixing
-// in BASE at the base of the saber. After OUT_MILLIS milliseconds, it
-// will be displaying the BASE color on the entire blade.
+// BASE and OFF_COLOR (which defaults to black). The amount of extension
+// is determined by EXTENSION. If EXTENSION returns 32768, the blade is fully
+// extended. If it returns zero, it is not extended.
 
-template<class T, int OUT_MILLIS, int IN_MILLIS, class OFF_COLOR=Rgb<0,0,0> >
-class InOutHelper {
+template<class T, class EXTENSION, class OFF_COLOR=Rgb<0,0,0>, bool ALLOW_DISABLE=1 >
+class InOutHelperX {
 public:
   void run(BladeBase* blade) {
     base_.run(blade);
     off_color_.run(blade);
-    uint32_t now = micros();
-    uint32_t delta = now - last_micros_;
-    last_micros_ = now;
-    if (blade->is_on()) {
-      if (extension == 0.0) {
-         // We might have been off for a while, so delta might
-         // be insanely high.
-         extension = 0.00001;
-      } else {
-         extension += delta / (OUT_MILLIS * 1000.0);
-         extension = min(extension, 1.0f);
-      }
-    } else {
-      if (is_same_type<OFF_COLOR, Rgb<0,0,0> >::value)
-        if (extension == 0.0)
-          blade->allow_disable();
-      extension -= delta / (IN_MILLIS * 1000.0);
-      extension = max(extension, 0.0f);
-    }
-    thres = extension * blade->num_leds() * 256;
+    extension_.run(blade);
+    thres = extension_.getInteger(0) * blade->num_leds() * 128;
+
+    if (ALLOW_DISABLE && is_same_type<OFF_COLOR, Rgb<0,0,0> >::value && thres == 0) 
+      blade->allow_disable();
   }
   OverDriveColor getColor(int led) {
     int black_mix = clampi32(thres - led * 256, 0, 255);
@@ -49,9 +32,24 @@ public:
 private:
   T base_;
   OFF_COLOR off_color_;
+  EXTENSION extension_;
   int thres = 0;
-  float extension = 0.0;
-  uint32_t last_micros_;
 };
+
+// Usage: InOutHelper<BASE, OUT_MILLIS, IN_MILLIS>
+// or: InOutHelper<BASE, OUT_MILLIS, IN_MILLIS, OFF_COLOR>
+// BASE, OFF_COLOR: COLOR
+// OUT_MILLIS, IN_MILLIS: a number
+// return value: COLOR
+// This class does a basic extend/retract. Basically it fades between
+// BASE and OFF_COLOR (which defaults to black). It starts by just
+// displaying OFF_COLOR, and when you turn the saber on it starts mixing
+// in BASE at the base of the saber. After OUT_MILLIS milliseconds, it
+// will be displaying the BASE color on the entire blade.
+
+#include "../functions/ifon.h"
+  
+template<class T, int OUT_MILLIS, int IN_MILLIS, class OFF_COLOR=Rgb<0,0,0> >
+  using InOutHelper = InOutHelperX<T, InOutFunc<OUT_MILLIS, IN_MILLIS>, OFF_COLOR>;
 
 #endif
