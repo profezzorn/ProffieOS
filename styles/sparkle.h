536d5fa168d7cfba2dc4e228ffe9e8b772e870df
hubbe <>
2018-08-15 04:52:57 +0000

    sparkle fixes

--- styles/sparkle.h
+++ styles/sparkle.h
@@ -7,12 +7,12 @@
 // SPARK_CHANCE_PROMILLE: a number
 // SPARK_INTENSITY: a number
 // Generally displays BASE, but creates little sparkles of SPARKLE_COLOR
-// SPARK_CHANCE_PROMILLE decides how often a spark is generated, defaults to 150 (15%)
+// SPARK_CHANCE_PROMILLE decides how often a spark is generated, defaults to 100 (10%)
 // SPARK_INTENSITY specifies how intens the spark is, defaults to 256.
 template<class BASE,
   class SPARKLE_COLOR = Rgb<255,255,255>,
-  int SPARK_CHANCE_PROMILLE = 150,
-  int SPARK_INTENSITY = 256>
+  int SPARK_CHANCE_PROMILLE = 100,
+  int SPARK_INTENSITY = 1024>
 class Sparkle {
 public:
   Sparkle() {
@@ -24,19 +24,28 @@ public:
     sparkle_color_.run(blade);
     uint32_t m = millis();
     if (m - last_update_ >= 10) {
+      last_update_ = m;
       uint16_t fifo[2];
+      fifo[0] = 0;
+      fifo[1] = 0;
       int N = blade->num_leds();
       for (size_t i = 2; i < N + 2; i++) {
+#if 1
 	int32_t x = (sparks_[i - 1] + sparks_[i + 1]) * 3 - (sparks_[i - 2] + sparks_[i + 2]);
 	sparks_[i-2] = fifo[0];
 	fifo[0] = fifo[1];
-	fifo[1] = (sparks_[i] * 27 + x) >> 4;
+	fifo[1] = (sparks_[i] * 11 + x) >> 4;
+#else
+	sparks_[i-1] = fifo[0];
+	fifo[0] = fifo[1];
+	int32_t x = ((sparks_[i-1] + sparks_[i+1]) * 63 + sparks_[i] * 895) >> 10;
+#endif	
       }
-      sparks_[N-2] = fifo[0];
-      sparks_[N-1] = fifo[1];
-    }
-    if (random(1000) < SPARK_CHANCE_PROMILLE) {
-      sparks_[random(blade->num_leds())+2] += SPARK_INTENSITY;
+      sparks_[N] = fifo[0];
+      sparks_[N+1] = fifo[1];
+      if (random(1000) < SPARK_CHANCE_PROMILLE) {
+	sparks_[random(blade->num_leds())+2] += SPARK_INTENSITY;
+      }
     }
   }
 
@@ -50,7 +59,7 @@ public:
 private:  
   BASE base_;
   SPARKLE_COLOR sparkle_color_;
-  unsigned short sparks_[maxLedsPerStrip + 4];
+  short sparks_[maxLedsPerStrip + 4];
   uint32_t last_update_;
 };
 
