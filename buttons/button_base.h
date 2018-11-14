4ef2e9f75b55e766a621f02bdf2c0e1b93e28d25
SA-22C <45020724+SA-22C@users.noreply.github.com>
2018-11-13 18:42:42 -0600

    Add files via upload

--- buttons/button_base.h
+++ buttons/button_base.h
@@ -5,67 +5,73 @@
 
 // Simple button handler. Keeps track of clicks and lengths of pushes.
 class ButtonBase : public Looper,
-                   public CommandParser,
-                   public DebouncedButton {
+public CommandParser,
+public DebouncedButton {
 public:
-  ButtonBase(const char* name, enum BUTTON button)
+    ButtonBase(const char* name, enum BUTTON button)
     : Looper(),
-      CommandParser(),
-      name_(name),
-      button_(button) {
-  }
-
-  const char* name() override { return name_; }
-
+    CommandParser(),
+    name_(name),
+    button_(button) {
+    }
+    
+    const char* name() override { return name_; }
+    
 protected:
-  void Loop() override {
-    STATE_MACHINE_BEGIN();
-    while (true) {
-      while (!DebouncedRead()) YIELD();
-      saber.Event(button_, EVENT_PRESSED);
-      if (millis() - push_millis_ < 500) {
-        saber.Event(button_, EVENT_DOUBLE_CLICK);
-      } else {
-        push_millis_ = millis();
-        current_modifiers |= button_;
-      }
-      while (DebouncedRead()) YIELD();
-      saber.Event(button_, EVENT_RELEASED);
-      if (current_modifiers & button_) {
-        current_modifiers &=~ button_;
-        if (millis() - push_millis_ < 500) {
-          saber.Event(button_, EVENT_CLICK_SHORT);
-        } else {
-          saber.Event(button_, EVENT_CLICK_LONG);
+    void Loop() override {
+        STATE_MACHINE_BEGIN();
+        while (true) {
+            while (!DebouncedRead()) YIELD();
+            saber.Event(button_, EVENT_PRESSED);
+            if (millis() - push_millis_ > 250 && millis() - push_millis_ < 500) {
+                saber.Event(button_, EVENT_DOUBLE_CLICK);
+            } else {
+                push_millis_ = millis();
+                current_modifiers |= button_;
+            }
+            while (DebouncedRead()) {
+                if (millis() - push_millis_ > 250) {
+                    saber.Event(button_, EVENT_HELD);
+                    while (DebouncedRead()) YIELD();
+               }
+               YIELD();
+           }
+            saber.Event(button_, EVENT_RELEASED);
+            if (current_modifiers & button_) {
+                current_modifiers &=~ button_;
+                if (millis() - push_millis_ < 500) {
+                    saber.Event(button_, EVENT_CLICK_SHORT);
+                } else if (millis() - push_millis_ > 500 && millis() - push_millis_ < 2500){
+                    saber.Event(button_, EVENT_CLICK_LONG);
+                }
+            } else {
+                // someone ate our clicks
+                push_millis_ = millis() - 10000; // disable double click
+            }
         }
-      } else {
-        // someone ate our clicks
-        push_millis_ = millis() - 10000; // disable double click
-      }
+        STATE_MACHINE_END();
     }
-    STATE_MACHINE_END();
-  }
-
-  bool Parse(const char* cmd, const char* arg) override {
-    if (!strcmp(cmd, name_)) {
-      saber.Event(button_, EVENT_CLICK_SHORT);
-      return true;
+    
+    bool Parse(const char* cmd, const char* arg) override {
+        if (!strcmp(cmd, name_)) {
+            saber.Event(button_, EVENT_CLICK_SHORT);
+            return true;
+        }
+        return false;
     }
-    return false;
-  }
-
-  void Help() override {
-    STDOUT.print(" ");
-    STDOUT.print(name_);
-    STDOUT.print(" - clicks the ");
-    STDOUT.print(name_);
-    STDOUT.println(" button");
-  }
-
-  const char* name_;
-  enum BUTTON button_;
-  uint32_t push_millis_;
-  StateMachineState state_machine_;
+    
+    void Help() override {
+        STDOUT.print(" ");
+        STDOUT.print(name_);
+        STDOUT.print(" - clicks the ");
+        STDOUT.print(name_);
+        STDOUT.println(" button");
+    }
+    
+    const char* name_;
+    enum BUTTON button_;
+    uint32_t push_millis_;
+    StateMachineState state_machine_;
 };
 
 #endif
