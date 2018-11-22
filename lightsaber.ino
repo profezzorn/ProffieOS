fc93e95d73c87165d611888019e4a2e7c2eaaf43
SA-22C <45020724+SA-22C@users.noreply.github.com>
2018-11-22 14:49:32 -0600

    Add files via upload
    Implemented volume menu when saber is off.
    
    Usage:
    Long-press AUX when saber is off to enter volume menu.  While in volume menu, a short press of POW will increase the volume and a short press of AUX will decrease the volume.  Volume will never go above the value specified in the config file or go below 10%.  Long-press AUX again to leave the volume menu.  Beeps will play under the following conditions: Volume menu enter/exit and volume up/down.  
    
    Mute on activation still works if you want silent operation of the saber.

--- lightsaber.ino
+++ lightsaber.ino
@@ -21,9 +21,8 @@
 // You can have multiple configuration files, and specify which one
 // to use here.
 
-#define CONFIG_FILE "config/proffiesaber.h"
-// #define CONFIG_FILE "config/default_proffieboard_config.h"
 // #define CONFIG_FILE "config/default_v3_config.h"
+#define CONFIG_FILE "config/proffiesaber.h"
 // #define CONFIG_FILE "config/crossguard_config.h"
 // #define CONFIG_FILE "config/graflex_v1_config.h"
 // #define CONFIG_FILE "config/prop_shield_fastled_v1_config.h"
@@ -607,7 +606,6 @@ struct is_same_type<T, T> { static const bool value = true; };
 #include "styles/cylon.h"
 #include "styles/ignition_delay.h"
 #include "styles/pulsing.h"
-#include "styles/blinking.h"
 #include "styles/on_spark.h"
 #include "styles/rgb_cycle.h"
 #include "styles/clash.h"
@@ -1311,11 +1309,10 @@ public:
 	  if (SetMute(true)) {
 	    unmute_on_deactivation_ = true;
 	  }
-   
+   }
+   else{
+    SaberBase::DoForce();
 	}
- else{
-      SaberBase::DoForce();
-    }
 	break;
 	
       case EVENTID(BUTTON_POWER, EVENT_CLICK_LONG, MODE_ON):
@@ -1327,6 +1324,8 @@ public:
 #endif
         Off();
         break;
+
+      
 
       case EVENTID(BUTTON_AUX, EVENT_CLICK_SHORT, MODE_ON):
         // Avoid the base and the very tip.
@@ -1348,17 +1347,16 @@ public:
           handled = false;
         }
         break;
-    
-    case EVENTID(BUTTON_AUX, EVENT_HELD, MODE_ON):
-        if (!SaberBase::Lockup()) {
-            if (pointing_down_) {
-                SaberBase::SetLockup(SaberBase::LOCKUP_DRAG);
-            } else {
-                SaberBase::SetLockup(SaberBase::LOCKUP_NORMAL);
-            }
-            SaberBase::DoBeginLockup();
+      case EVENTID(BUTTON_AUX, EVENT_HELD, MODE_ON):
+      if (!SaberBase::Lockup()) {
+          if (pointing_down_) {
+            SaberBase::SetLockup(SaberBase::LOCKUP_DRAG);
+          } else {
+            SaberBase::SetLockup(SaberBase::LOCKUP_NORMAL);
+          }
+          SaberBase::DoBeginLockup();
         } else {
-            handled = false;
+          handled = false;
         }
         break;
 
@@ -2584,8 +2582,6 @@ public:
     if (*e) {
       *e = 0;
       e++;  // e is now argument (if any)
-    } else {
-      e = nullptr;
     }
     if (monitor.IsMonitoring(Monitoring::MonitorSerial) &&
         default_output != &SA::stream()) {
