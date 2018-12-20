471c340b3d75c1a7b32aba9e89544bdab3d8ca51
hubbe <>
2018-12-20 08:06:01 +0000

    compiles, but does not work

--- styles/stripes.h
+++ styles/stripes.h
@@ -13,29 +13,29 @@ class StripesHelper<> {
 public:
   static const size_t size = 0;
   void run(BladeBase* blade) {}
-  void get(int p, OverDriveColor& c) {}
+  void get(int led, int p, OverDriveColor* c) {}
 };
 
-template<class A, class B, class... C>
-class StripesHelper<A, B, C...> {
+template<class A, class... B>
+class StripesHelper<A, B...> {
 public:
-  static const size_t size = StripesHelper<B, C...>::size + 1;
+  static const size_t size = StripesHelper<B...>::size + 1;
   void run(BladeBase* blade) {
     a_.run(blade);
     b_.run(blade);
   }
-  void get(int p, OverDriveColor& ret) {
-    b_.get(p + 341, ret);
+  void get(int led, int p, OverDriveColor* ret) {
+    b_.get(led, p + 341, ret);
     if (p > 0 && p < 512) {
-      OverDriveColor tmp = a_.getColor();
+      OverDriveColor tmp = a_.getColor(led);
       int mul = sin_table[p];
-      ret.c.r += (tmp.c.r * mul) >> 14;
-      ret.c.g += (tmp.c.g * mul) >> 14;
-      ret.c.b += (tmp.c.b * mul) >> 14;
+      ret->c.r += (tmp.c.r * mul) >> 14;
+      ret->c.g += (tmp.c.g * mul) >> 14;
+      ret->c.b += (tmp.c.b * mul) >> 14;
     }
   }
   A a_;
-  StripesHelper<B, C...> b_;
+  StripesHelper<B...> b_;
 };
 
 
@@ -46,18 +46,18 @@ public:
     uint32_t now_micros = micros();
     uint32_t delta_micros = now_micros - last_micros_;
     last_micros_ = now_micros;
-    m = (m + delta_micros * SPEED / 333333) % (colors_.size() * 341);
+    m = (m + delta_micros * SPEED / 333333) % (colors_.size * 341);
     colors_.run(base);
   }
   OverDriveColor getColor(int led) {
     // p = 0..341*len(colors)
-    int p = ((m + led * (50000 / WIDTH)) >> 10) % (colors_.size() * 341);
+    int p = ((m + led * (50000 / WIDTH)) >> 10) % (colors_.size * 341);
     
     OverDriveColor ret;
     ret.overdrive = false;
     ret.c = Color16(0,0,0);
-    colors_.get(p, &ret);
-    colors_.get(p - 341 * colors_.size(), &ret);
+    colors_.get(led, p, &ret);
+    colors_.get(led, p - 341 * colors_.size, &ret);
     return ret;
   }
 private:
