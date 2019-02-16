bef950f5b7a7581596335b0a7fe0de0ff9a6a709
hubbe <>
2019-02-16 20:18:40 +0000

    move most config file reading/writing functions into FileReader

--- common/config_file.h
+++ common/config_file.h
@@ -6,86 +6,20 @@
 // Reads an config file, looking for variable assignments.
 // TODO(hubbe): Read config files from serialflash.
 struct ConfigFile {
-  void skipwhite(FileReader* f) {
-    while (f->Peek() == ' ' || f->Peek() == '\t')
-      f->Read();
-  }
-  void skipline(FileReader* f) {
-    while (f->Available() && f->Read() != '\n');
-  }
-
-  int64_t readIntValue(FileReader* f) {
-    int64_t ret = 0;
-    int64_t sign = 1;
-    if (f->Peek() == '-') {
-      sign = -1;
-      f->Read();
-    }
-    while (f->Available()) {
-      int c = toLower(f->Peek());
-      if (c >= '0' && c <= '9') {
-        ret = (c - '0') + 10 * ret;
-        f->Read();
-      } else {
-        return ret * sign;
-      }
-    }
-    return ret * sign;
-  }
-
-  float readFloatValue(FileReader* f) {
-    float ret = 0.0;
-    float sign = 1.0;
-    float mult = 1.0;
-    if (f->Peek() == '-') {
-      sign = -1.0;
-      f->Read();
-    }
-    while (f->Available()) {
-      int c = toLower(f->Peek());
-      if (c >= '0' && c <= '9') {
-        if (mult == 1.0) {
-          ret = (c - '0') + 10 * ret;
-        } else {
-          ret += (c - '0') * mult;
-          mult /= 10.0;
-        }
-        f->Read();
-      } else if (c == '.') {
-        if (mult != 1.0) return ret * sign;
-        // Time to read decimals.
-        mult /= 10.0;
-        f->Read();
-      } else {
-        return ret * sign;
-      }
-    }
-    return ret * sign;
-  }
-
   void Read(FileReader* f) {
     SetVariable("=", 0.0);  // This resets all variables.
     if (!f || !f->IsOpen()) return;
-    for (; f->Available(); skipline(f)) {
+    for (; f->Available(); f->skipline()) {
       char variable[33];
       variable[0] = 0;
-      skipwhite(f);
+      f->skipwhite();
       if (f->Peek() == '#') continue;
-      for (int i = 0; i < 32; i++) {
-        int c = toLower(f->Peek());
-        if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')) {
-          f->Read();
-          variable[i] = c;
-          variable[i+1] = 0;
-        } else {
-          break;
-        }
-      }
-      skipwhite(f);
+      f->readVariable(variable);
+      f->skipwhite();
       if (f->Peek() != '=') continue;
       f->Read();
-      skipwhite(f);
-      float v = readFloatValue(f);
+      f->skipwhite();
+      float v = f->readFloatValue();
 #if 0
       STDOUT.print(variable);
       STDOUT.print(" = ");
--- common/file_reader.h
+++ common/file_reader.h
@@ -44,6 +44,7 @@ public:
     pos_ += to_copy;
     return to_copy;
   }
+  int write(const uint8_t* dest, size_t bytes) { return 0; }
   size_t available() {
     return size_ - pos_;
   }
@@ -97,6 +98,17 @@ public:
 #endif    
     return false;
   }
+  bool Create(const char* filename) {
+    Close();
+#ifdef ENABLE_SD
+    sd_file_ = LSFS::OpenForWrite(filename);
+    if (sd_file_) {
+      type_ = TYPE_SD;
+      return true;
+    }
+#endif    
+    return false;
+  }
   bool OpenMem(const uint8_t* data, uint32_t length) {
     Close();
     type_ = TYPE_MEM;
@@ -133,6 +145,12 @@ public:
       return -1;
     }
   }
+  int Write(const uint8_t* dest, int bytes) {
+    RUN_ALL(write(dest, bytes))
+    return 0;
+  }
+  int Write(uint8_t c) { return Write(&c, 1); }
+  int Write(const char *str) { return Write((uint8_t*)str, strlen(str)); }
   void Seek(uint32_t n) {
     RUN_ALL_VOID(seek(n))
   }
@@ -180,6 +198,137 @@ public:
   }
   void Skip(int n) { Seek(Tell() + n); }
   void Rewind() { Seek(0); }
+
+  void skipwhite() {
+    while (Peek() == ' ' || Peek() == '\t') Read();
+  }
+
+  // Skip rest of line.
+  void skipline() {
+    while (Available() && Read() != '\n');
+  }
+
+  int64_t readIntValue() {
+    int64_t ret = 0;
+    int64_t sign = 1;
+    if (Peek() == '-') {
+      sign = -1;
+      Read();
+    }
+    while (Available()) {
+      int c = toLower(Peek());
+      if (c >= '0' && c <= '9') {
+        ret = (c - '0') + 10 * ret;
+        Read();
+      } else {
+        return ret * sign;
+      }
+    }
+    return ret * sign;
+  }
+
+  float readFloatValue() {
+    float ret = 0.0;
+    float sign = 1.0;
+    float mult = 1.0;
+    if (Peek() == '-') {
+      sign = -1.0;
+      Read();
+    }
+    while (Available()) {
+      int c = toLower(Peek());
+      if (c >= '0' && c <= '9') {
+        if (mult == 1.0) {
+          ret = (c - '0') + 10 * ret;
+        } else {
+          ret += (c - '0') * mult;
+          mult /= 10.0;
+        }
+        Read();
+      } else if (c == '.') {
+        if (mult != 1.0) return ret * sign;
+        // Time to read decimals.
+        mult /= 10.0;
+        Read();
+      } else {
+        return ret * sign;
+      }
+    }
+    return ret * sign;
+  }
+
+  bool readVariable(char variable[33]) {
+    variable[0] = 0;
+    skipwhite();
+    for (int i = 0; i < 32; i++) {
+      int c = toLower(Peek());
+      if ((c >= 'a' && c <= 'z') || (c >= '0' && c <= '9')) {
+	Read();
+	variable[i] = c;
+	variable[i+1] = 0;
+      } else {
+	break;
+      }
+    }
+    skipwhite();
+    return true;
+  }
+
+  // LSPtr<> ?
+  char* readString() {
+    int len = 0;
+    int space = 16;
+    char* ret = (char*)malloc(space);
+    while (Available()) {
+      int c = Read();
+      switch (c) {
+	case '\n':
+	  ret[len] = 0;
+	  return ret;
+	case '\\':
+	  switch (c = Read()) {
+	    case '\\': ret[len++] = '\\'; break;
+	    case '\n': ret[len++] = '\n'; break;
+	    case '\t': ret[len++] = '\t'; break;
+	    default: ret[len++] = c; break;
+	  }
+	  break;
+	default:
+	  ret[len++] = c;
+      }
+      if (len == space) {
+	int new_space = space * 3/2 + 8;
+	char* tmp = (char*) realloc(ret, new_space);
+	if (!tmp) {
+	  STDOUT.println("Line too long.");
+	  free(tmp);
+	  return nullptr;
+	}
+	space = new_space;
+	ret = tmp;
+      }
+    }
+    ret[len] = 0;
+    return ret;
+  }
+
+  void write_key_value(const char* key,
+		       const char* value) {
+    Write(key);
+    Write('=');
+    for (;*value; value++) {
+      switch (*value) {
+	case 0:
+	  Write('\n');
+	  return;
+	case '\n': Write("\\n"); break;
+	case '\t': Write("\\t"); break;
+	case '\\': Write("\\\\"); break;
+	default:
+	  Write(*value);
+      }
+    }
+  }
 
 private:
   enum {
