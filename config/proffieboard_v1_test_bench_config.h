160a14e11ab1aa4a1f36d1f0da3a22affbc5ccb0
Joshua Stewardson <Joshua.Stewardson@meredith.com>
2019-03-11 14:10:21 -0500

    Small typo correction sensetive/sensitive.

--- config/proffieboard_v1_test_bench_config.h
+++ config/proffieboard_v1_test_bench_config.h
@@ -28,7 +28,7 @@
 // both of them to bladePin and drive them in parallel.
 const unsigned int maxLedsPerStrip = 144;
 
-// This defines how sensetive the clash detection is.
+// This defines how sensitive the clash detection is.
 #define CLASH_THRESHOLD_G 1.0
 
 // Feature defines, these let you turn off large blocks of code
