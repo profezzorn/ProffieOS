2a680aed9a85ecd850f8278a226aa1fb754aa1ff
profezzorn <hubbe@hubbe.net>
2019-05-12 11:44:16 -0700

    make RGBw possible on LED stars

--- blades/drive_logic.h
+++ blades/drive_logic.h
@@ -3,6 +3,34 @@
 
 #include "led_interface.h"
 
+template<typename T> struct ToVoid { typedef void Type; };
+
+template<class LED, typename Enable=void>
+struct ColorSelector {
+  static int Select(Color16 c) {
+    return c.select(Color16(Color8(LED::Red, LED::Green, LED::Blue)));
+  }
+};
+
+// This allows for engergy-efficient RGBW setups
+template<class LED>
+struct ColorSelector<LED, typename ToVoid<typename LED::SUBTRACT>::Type>  {
+  static int Select(Color16 c) {
+    ColorSelector<typename LED::SUBTRACT> subtract_selector;
+    int white = subtract_selector.Select(c);
+    return Color16(c.r - white, c.g - white, c.b - white).select(Color16(Color8(LED::Red, LED::Green, LED::Blue)));
+  }
+};
+
+// This allows for an easy way to override color selection (but not the overdrive logic)
+template<class LED>
+struct ColorSelector<LED, typename ToVoid<typename LED::CustomColorSelector>::Type> {
+  static int Select(Color16 c) {
+    typename LED::CustomColorSelector selector;
+    return selector.Select(c);
+  }
+};
+
 // This code turns down the PWM duty cycle when the battery voltage
 // is too high for the LED. We use a simple affine model to approximate
 // the amp/volt curve of the LED. This model requires that we know
@@ -12,7 +40,7 @@
 // given the current battery voltage, if the amps is higher than
 // the maximum then we adjust the PWM rate so that the average amps
 // equals the maximum amps.
-template<class LED>
+template<class LED, class Enable=void>
 class DriveLogic : public LEDInterface {
 public:
   float PWMMultiplier() {
@@ -21,6 +49,7 @@ public:
     float di = LED::MaxAmps - LED::P2Amps;
     float delta = dv / di;
     float amps = (V - LED::MaxVolts + LED::MaxAmps * delta) / (delta + LED::R);
+#ifndef NOTEST
     if (monitor.ShouldPrintMultiple(Monitoring::MonitorPWM)) {
       STDOUT.print("AMPS = ");
       STDOUT.print(amps);
@@ -29,19 +58,32 @@ public:
       STDOUT.print(" PWM = ");
       STDOUT.println(100.0 * LED::MaxAmps / amps);
     }
+#endif
     if (amps <= LED::MaxAmps) {
       return 1.0f;
     }
 
     return LED::MaxAmps / amps;
   }
-  int PWM(Color16 c) override {
-    return c.select(Color16(Color8(LED::Red, LED::Green, LED::Blue))) * PWMMultiplier();
-  }
+
   int PWM_overdrive(Color16 c) override {
-    return c.select(Color16(Color8(LED::Red, LED::Green, LED::Blue)));
+    return ColorSelector<LED>::Select(c);
+  }
+  int PWM(Color16 c) override {
+    return PWM_overdrive(c) * PWMMultiplier();
   }
 };
+
+// This allows LED class to override the entire drive logic.
+template<class LED>
+class DriveLogic<LED, typename LED::CustomDriveLogic> : public LEDInterface {
+public:
+  int PWM_overdrive(Color16 c) override { return drive_.PWM_overdrive(c); }
+  int PWM(Color16 c) override { return drive_.PWM(c); }
+private:
+  typename LED::CustomDriveLogic drive_;
+};
+
 
 template<class LED>
 class LEDInterface* LEDPtr() {
---
+++ blades/tests.cpp
@@ -0,0 +1,85 @@
+#include <vector>
+#include <stdint.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+#define NOTEST
+
+// cruft
+template<class A, class B>
+constexpr auto min(A&& a, B&& b) -> decltype(a < b ? std::forward<A>(a) : std::forward<B>(b)) {
+  return a < b ? std::forward<A>(a) : std::forward<B>(b);
+}
+template<class A, class B>
+constexpr auto max(A&& a, B&& b) -> decltype(a < b ? std::forward<A>(a) : std::forward<B>(b)) {
+  return a >= b ? std::forward<A>(a) : std::forward<B>(b);
+}
+float fract(float x) { return x - floor(x); }
+
+uint32_t micros_ = 0;
+uint32_t micros() { return micros_; }
+uint32_t millis() { return micros_ / 1000; }
+int32_t clampi32(int32_t x, int32_t a, int32_t b) {
+  if (x < a) return a;
+  if (x > b) return b;
+  return x;
+}
+
+struct BM {
+  float battery() { return 3.7; }
+};
+
+BM battery_monitor;
+
+#include "../common/monitoring.h"
+#include "../common/color.h"
+#include "drive_logic.h"
+#include "leds.h"
+
+#define EXPECT_EQ(X,Y) do {					\
+  auto x = (X);							\
+  auto y = (Y);							\
+  if (x != y) {							\
+    std::cerr << #X << " != " << #Y << " on line " << __LINE__ << std::endl; \
+    std::cerr << #X << " = " << x  << std::endl;				\
+    std:: cerr << #Y << " = " << y << std::endl;				\
+    exit(1);							\
+  }								\
+} while(0)
+
+struct CreeTest {
+  static constexpr float MaxAmps = 1.0;
+  static constexpr float MaxVolts = 3.4;
+  static constexpr float P2Amps = 0.35;
+  static constexpr float P2Volts = 3.1;
+  static constexpr float R = 0.24;
+  static const int Red = 0;
+  static const int Green = 0;
+  static const int Blue = 255;
+  typedef CreeXPE2White SUBTRACT;
+//  typedef int CustomColorSelector;
+};
+
+
+void test_color_selector() {
+  ColorSelector<CreeXPE2White> s1;
+  EXPECT_EQ(0xFFFF, s1.Select(Color16(0xFFFF,0xFFFF,0xFFFF)));
+  EXPECT_EQ(20, s1.Select(Color16(20,0xFFFF,0xFFFF)));
+  EXPECT_EQ(20, s1.Select(Color16(200,20,0xFFFF)));
+  EXPECT_EQ(20, s1.Select(Color16(100, 200,20)));
+  ColorSelector<CreeXPE2Blue> s2;
+  EXPECT_EQ(0xFFFF, s2.Select(Color16(0xFFFF,0xFFFF,0xFFFF)));
+  EXPECT_EQ(0x0000, s2.Select(Color16(0xFFFF,0xFFFF,0x0000)));
+  EXPECT_EQ(0xFFFF, s2.Select(Color16(0xFFFF,0x0000,0xFFFF)));
+  ColorSelector<CreeTest> s3;
+  EXPECT_EQ(0x0000, s3.Select(Color16(0xFFFF,0xFFFF,0xFFFF)));
+  EXPECT_EQ(0x0000, s3.Select(Color16(0xFFFF,0xFFFF,0x0000)));
+  EXPECT_EQ(0xFFFF, s3.Select(Color16(0xFFFF,0x0000,0xFFFF)));
+}
+
+
+int main() {
+  test_color_selector();
+}
