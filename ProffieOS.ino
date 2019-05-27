18fd61ec877ce47b9099025bfa7f28009451723a
profezzorn <hubbe@hubbe.net>
2019-05-26 20:42:52 -0700

    move out smooth swing config file

--- ProffieOS.ino
+++ ProffieOS.ino
@@ -405,33 +405,7 @@ void SetupStandardAudio() {
 
 HybridFont hybrid_font;
 
-class SmoothSwingConfigFile : public ConfigFile {
-public:
-  void SetVariable(const char* variable, float v) override {
-    CONFIG_VARIABLE(Version, 1);
-    CONFIG_VARIABLE(SwingSensitivity, 450.0f);
-    CONFIG_VARIABLE(MaximumHumDucking, 75.0f);
-    CONFIG_VARIABLE(SwingSharpness, 1.75f);
-    CONFIG_VARIABLE(SwingStrengthThreshold, 20.0f);
-    CONFIG_VARIABLE(Transition1Degrees, 45.0f);
-    CONFIG_VARIABLE(Transition2Degrees, 160.0f);
-    CONFIG_VARIABLE(MaxSwingVolume, 3.0f);
-    CONFIG_VARIABLE(AccentSwingSpeedThreshold, 0.0f);
-  };
-
-  int  Version;
-  float SwingSensitivity;
-  float MaximumHumDucking;
-  float SwingSharpness;
-  float SwingStrengthThreshold;
-  float Transition1Degrees;
-  float Transition2Degrees;
-  float MaxSwingVolume;
-  float AccentSwingSpeedThreshold;
-};
-
-SmoothSwingConfigFile smooth_swing_config;
-
+#include "sound/smooth_swing_config.h"
 #include "sound/looped_swing_wrapper.h"
 #include "sound/smooth_swing_v2.h"
 
