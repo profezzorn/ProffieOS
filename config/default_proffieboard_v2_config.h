a36d32fb845e90d8fa2b5bbb4bc00caf3bfc44fd
profezzorn <hubbe@hubbe.net>
2019-06-04 23:47:59 -0700

    pbv2 updates

---
+++ config/default_proffieboard_v2_config.h
@@ -0,0 +1,81 @@
+// This is a sample configuration file.
+// This saber has:
+//   o Proffieboard V2 hardware.
+//   o Two buttons
+//   o An XP-E2 RGB LED star on LED 4-5-6
+//   o A neopixel blade on LED 1-2-3 and neopixel data 1.
+// If you have a saber similar to this one, make a copy and use the copy.
+// This is also the default configuration file. Pre-programmed boards will
+// use this configuration file.
+
+#ifdef CONFIG_TOP
+#include "proffieboard_v2_config.h"
+#define NUM_BLADES 2
+#define NUM_BUTTONS 2
+#define VOLUME 1000
+const unsigned int maxLedsPerStrip = 144;
+#define CLASH_THRESHOLD_G 1.0
+#define ENABLE_AUDIO
+#define ENABLE_MOTION
+#define ENABLE_WS2811
+#define ENABLE_SD
+#endif
+
+#ifdef CONFIG_PRESETS
+Preset presets[] = {
+  { "TeensySF", "tracks/venus.wav",
+    StyleNormalPtr<CYAN, WHITE, 300, 800>(),
+    StyleNormalPtr<CYAN, WHITE, 300, 800>(), "cyan"},
+  { "SmthJedi", "tracks/mars.wav",
+    StylePtr<InOutSparkTip<EASYBLADE(BLUE, WHITE), 300, 800> >(),
+    StylePtr<InOutSparkTip<EASYBLADE(BLUE, WHITE), 300, 800> >(), "blue"},
+  { "SmthGrey", "tracks/mercury.wav",
+    StyleFirePtr<RED, YELLOW, 0>(),
+    StyleFirePtr<RED, YELLOW, 1>(), "fire"},
+  { "SmthFuzz", "tracks/uranus.wav",
+    StyleNormalPtr<RED, WHITE, 300, 800>(),
+    StyleNormalPtr<RED, WHITE, 300, 800>(), "red"},
+  { "RgueCmdr", "tracks/venus.wav",
+    StyleFirePtr<BLUE, CYAN, 0>(),
+    StyleFirePtr<BLUE, CYAN, 1>(), "blue fire"},
+  { "TthCrstl", "tracks/mars.wav",
+    StylePtr<InOutHelper<EASYBLADE(OnSpark<GREEN>, WHITE), 300, 800> >(),
+    StylePtr<InOutHelper<EASYBLADE(OnSpark<GREEN>, WHITE), 300, 800> >(), "green"},
+  { "TeensySF", "tracks/mercury.wav",
+    StyleNormalPtr<WHITE, RED, 300, 800, RED>(),
+    StyleNormalPtr<WHITE, RED, 300, 800, RED>(), "white"},
+  { "SmthJedi", "tracks/uranus.wav",
+    StyleNormalPtr<AudioFlicker<YELLOW, WHITE>, BLUE, 300, 800>(),
+    StyleNormalPtr<AudioFlicker<YELLOW, WHITE>, BLUE, 300, 800>(), "yellow"},
+  { "SmthGrey", "tracks/venus.wav",
+    StylePtr<InOutSparkTip<EASYBLADE(MAGENTA, WHITE), 300, 800> >(),
+    StylePtr<InOutSparkTip<EASYBLADE(MAGENTA, WHITE), 300, 800> >(), "magenta"},
+  { "SmthFuzz", "tracks/mars.wav",
+    StyleNormalPtr<Gradient<RED, BLUE>, Gradient<CYAN, YELLOW>, 300, 800>(),
+    StyleNormalPtr<Gradient<RED, BLUE>, Gradient<CYAN, YELLOW>, 300, 800>(), "gradient"},
+  { "RgueCmdr", "tracks/mercury.wav",
+    StyleRainbowPtr<300, 800>(),
+    StyleRainbowPtr<300, 800>(), "rainbow"},
+  { "TthCrstl", "tracks/uranus.wav",
+    StyleStrobePtr<WHITE, Rainbow, 15, 300, 800>(),
+    StyleStrobePtr<WHITE, Rainbow, 15, 300, 800>(), "strobe"},
+  { "TeensySF", "tracks/venus.wav",
+    &style_pov,
+    StyleNormalPtr<BLACK, BLACK, 300, 800>(), "POV"},
+  { "SmthJedi", "tracks/mars.wav",
+    &style_charging,
+    StyleNormalPtr<BLACK, BLACK, 300, 800>(), "Battery\nLevel"}
+};
+BladeConfig blades[] = {
+ { 0, WS2811BladePtr<144, WS2811_ACTUALLY_800kHz | WS2811_GRB>(),
+     SimpleBladePtr<CreeXPE2WhiteTemplate<550>,
+                    CreeXPE2BlueTemplate<240>,
+                    CreeXPE2BlueTemplate<240>, NoLED, bladePowerPin4, bladePowerPin5, bladePowerPin6, -1>(),
+     CONFIGARRAY(presets) },
+  };
+#endif
+
+#ifdef CONFIG_BUTTONS
+Button PowerButton(BUTTON_POWER, powerButtonPin, "pow");
+Button AuxButton(BUTTON_AUX, auxPin, "aux");
+#endif
