33a9f98be391656f37f5bf1340c4f17f09d7523a
SA-22C <45020724+SA-22C@users.noreply.github.com>
2019-06-07 13:02:01 -0600

    Add files via upload
    updated startswing with parameter in saber_base.h

--- common/saber_base.h
+++ common/saber_base.h
@@ -74,7 +74,7 @@ public:
   // 1.0 = kDefaultVolume
   // This is really just for sound fonts.
   virtual void SetHumVolume(float volume) {}
-  virtual void StartSwing() {}
+  virtual void StartSwing(bool* swingType_) {}
   virtual float SetSwingVolume(float swing_strength, float mixhum) {
     return mixhum;
   }
