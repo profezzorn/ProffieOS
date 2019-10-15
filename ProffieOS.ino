e9c84c295b36aebaf93cf8da99e32d35445768f5
profezzorn <hubbe@hubbe.net>
2019-10-15 00:59:38 -0700

    use my config file again

--- ProffieOS.ino
+++ ProffieOS.ino
@@ -29,9 +29,8 @@
 // #define CONFIG_FILE "config/owk_v2_config.h"
 // #define CONFIG_FILE "config/test_bench_config.h"
 // #define CONFIG_FILE "config/toy_saber_config.h"
-// #define CONFIG_FILE "config/proffieboard_v1_test_bench_config.h"
+#define CONFIG_FILE "config/proffieboard_v1_test_bench_config.h"
 // #define CONFIG_FILE "config/td_proffieboard_config.h"
-#define CONFIG_FILE "config/testconfig.h"
 
 
 #ifdef CONFIG_FILE_TEST
