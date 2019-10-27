b02ada423a2ebcf52906b6471f308d46c394eb03
profezzorn <hubbe@hubbe.net>
2019-10-27 15:11:39 -0700

    reset owk volume

--- config/owk_v2_config.h
+++ config/owk_v2_config.h
@@ -23,8 +23,7 @@
 // #define DUAL_POWER_BUTTONS
 
 // Volume, useful range is about 0-2000.
-// #define VOLUME 1800
-#define VOLUME 100
+#define VOLUME 1800
 
 // If you have two 144 LED/m strips in your blade, connect
 // both of them to bladePin and drive them in parallel.
