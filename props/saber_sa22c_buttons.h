7370c25577e745775a0219c615139920227faadd
SA-22C <matthew.mcgeary@gmail.com>
2019-11-06 23:21:19 -0600

    Update saber_sa22c_buttons.h

--- props/saber_sa22c_buttons.h
+++ props/saber_sa22c_buttons.h
@@ -1,48 +1,48 @@
-//sa22c props file, includes the following changes
+// sa22c props file, includes the following changes
 //
-//New DEFINE: NO_LOCKUP_HOLD
-//reverts to lockup being triggered only by clash + pow or aux
-//Button configs:
+// New #define NO_LOCKUP_HOLD
+// reverts to lockup being triggered only by clash + pow or aux
+// Button configs:
 //
-//1 Button:
-//Activate Muted - fast double click while OFF
-//Activate - short click while OFF
-//Play/Stop Music - hold and release while ON
-//Turn off blade - hold and wait till blade is off while ON
-//Next Preset - hold and release while OFF
-//Prev Preset - hold and wait while OFF
-//Lockup - hold + hit clash while ON
-//Drag - hold + hit clash while ON pointing the blade tip down
-//Blaster Blocks - short click while ON
-//Force Effects - hold + twist the hilt while ON (while pointing up)
-//Color Change mode - hold + twist the hilt while ON (pointing down)
+// 1 Button:
+// Activate Muted - fast double click while OFF
+// Activate - short click while OFF
+// Play/Stop Music - hold and release while ON
+// Turn off blade - hold and wait till blade is off while ON
+// Next Preset - hold and release while OFF
+// Prev Preset - hold and wait while OFF
+// Lockup - hold + hit clash while ON
+// Drag - hold + hit clash while ON pointing the blade tip down
+// Blaster Blocks - short click while ON
+// Force Effects - hold + twist the hilt while ON (while pointing up)
+// Color Change mode - hold + twist the hilt while ON (pointing down)
 //
-//2 Button: 
-//POWER
-//Activate Muted - fast double click while OFF
-//Activate - short click while OFF
-//Play/Stop Music - hold and release while OFF
-//Turn off blade - hold and wait till blade is off while ON
-//Force Effects - double click while ON
-//Volume UP - short click while OFF and in VOLUME MENU
-//Prev Preset - hold and wait while OFF
-//Color Change mode - hold + toggle AUX while ON
-//AUX
-//Blaster blocks - short click while ON
-//Next Preset - short click while OFF
-//Lockup - hold while ON
-//Drag - hold while ON pointing the blade tip down
-//Enter VOLUME MENU - long click while OFF
-//Volume down - short click while OFF and in VOLUME MENU
-//Battery level - hold while off
+// 2 Button:
+// POWER
+// Activate Muted - fast double click while OFF
+// Activate - short click while OFF
+// Play/Stop Music - hold and release while OFF
+// Turn off blade - hold and wait till blade is off while ON
+// Force Effects - double click while ON
+// Volume UP - short click while OFF and in VOLUME MENU
+// Prev Preset - hold and wait while OFF
+// Color Change mode - hold + toggle AUX while ON
+// AUX
+// Blaster blocks - short click while ON
+// Next Preset - short click while OFF
+// Lockup - hold while ON
+// Drag - hold while ON pointing the blade tip down
+// Enter VOLUME MENU - long click while OFF
+// Volume down - short click while OFF and in VOLUME MENU
+// Battery level - hold while off
 //
-//3 Button: Same as two button except for VOLUME MENU
-//AUX
-//Volume UP - short click while OFF and in VOLUME MENU
-//AUX2
-//Lockup - hold while ON
-//Previous Preset - short click while OFF
-//Volume DOWN - short click while OFF and in VOLUME MENU
+// 3 Button: Same as two button except for VOLUME MENU
+// AUX
+// Volume UP - short click while OFF and in VOLUME MENU
+// AUX2
+// Lockup - hold while ON
+// Previous Preset - short click while OFF
+// Volume DOWN - short click while OFF and in VOLUME MENU
 
 #ifndef PROPS_SABER_SA22C_BUTTONS_H
 #define PROPS_SABER_SA22C_BUTTONS_H
@@ -58,6 +58,32 @@ class SaberSA22CButtons : public PropBase {
 public:
   SaberSA22CButtons() : PropBase() {}
   const char* name() override { return "SaberSA22CButtons"; }
+
+  void ChangeVolume(bool up) {
+    if (up) {
+      STDOUT.println("Volume up");
+      if (dynamic_mixer.get_volume() < VOLUME) {
+        dynamic_mixer.set_volume(dynamic_mixer.get_volume() + VOLUME * 0.10);
+        beeper.Beep(0.5, 2000);
+        STDOUT.print("Current Volume: ");
+        STDOUT.println(dynamic_mixer.get_volume());
+      }
+      else {
+        beeper.Beep(0.5, 3000);
+      }
+    } else {
+      STDOUT.println("Volume Down");
+      if (dynamic_mixer.get_volume() > (0.10 * VOLUME)) {
+        dynamic_mixer.set_volume(dynamic_mixer.get_volume() - VOLUME * 0.10);
+        beeper.Beep(0.5, 2000);
+        STDOUT.print("Current Volume: ");
+        STDOUT.println(dynamic_mixer.get_volume());
+      }
+      else{
+        beeper.Beep(0.5, 1000);
+      }
+    }
+  }
 
   bool Event2(enum BUTTON button, EVENT event, uint32_t modifiers) override {
     switch (EVENTID(button, event, modifiers)) {
@@ -79,21 +105,11 @@ public:
       case EVENTID(BUTTON_AUX2, EVENT_LATCH_ON, MODE_OFF):
       case EVENTID(BUTTON_POWER, EVENT_CLICK_SHORT, MODE_OFF):
  #if NUM_BUTTONS == 2
-        if (MODE_VOLUME){
-          STDOUT.println("Volume up");
-          if (dynamic_mixer.get_volume() < VOLUME){
-            current_volume_ += VOLUME * 0.10;
-            dynamic_mixer.set_volume(current_volume_);
-            beeper.Beep(0.5, 2000);
-            STDOUT.print("Current Volume: ");
-            STDOUT.println(dynamic_mixer.get_volume());
-          }
-          else{
-           beeper.Beep(0.5, 3000);
-          }
+        if (mode_volume_){
+          ChangeVolume(true);
         }
 #endif
-        if (!MODE_VOLUME){
+        if (!mode_volume_) {
           aux_on_ = false;
           On();
         }
@@ -101,42 +117,21 @@ public:
 
       case EVENTID(BUTTON_AUX, EVENT_CLICK_SHORT, MODE_OFF):
 #if NUM_BUTTONS == 2
-        if (MODE_VOLUME){
-          STDOUT.println("Volume Down");
-          if (dynamic_mixer.get_volume() > (0.10 * VOLUME)){
-            current_volume_ = dynamic_mixer.get_volume();
-            current_volume_ -= (VOLUME * 0.10) ;
-            dynamic_mixer.set_volume(current_volume_);
-            beeper.Beep(0.5, 2000);
-            STDOUT.print("Current Volume: ");
-            STDOUT.println(dynamic_mixer.get_volume());
-          }
-          else{
-            beeper.Beep(0.5, 1000);
-          }
+        if (mode_volume_) {
+          ChangeVolume(false);
         }
 #endif
 #if NUM_BUTTONS == 3
-        if (MODE_VOLUME){
-          STDOUT.println("Volume up");
-          if (dynamic_mixer.get_volume() < VOLUME){
-            current_volume_ += VOLUME * 0.10;
-            dynamic_mixer.set_volume(current_volume_);
-            beeper.Beep(0.5, 2000);
-            STDOUT.print("Current Volume: ");
-            STDOUT.println(dynamic_mixer.get_volume());
-          }
-          else{
-            beeper.Beep(0.5, 3000);
-          }
+        if (mode_volume_) {
+          ChangeVolume(true);
         }
 #endif
-        if (!MODE_VOLUME) {
+        if (!mode_volume_) {
 #ifdef DUAL_POWER_BUTTONS
           aux_on_ = true;
           On();
 #else
-      if (!MODE_VOLUME) {
+      if (!mode_volume_) {
         next_preset();
 	  }
 
@@ -153,10 +148,10 @@ public:
       case EVENTID(BUTTON_NONE, EVENT_TWIST, MODE_ON | BUTTON_POWER):
 #ifndef DISABLE_COLOR_CHANGE
         if (accel_.x < -0.15) {
-				  ToggleColorChangeMode();
-		} else {
-			  SaberBase::DoForce();
-		}
+          ToggleColorChangeMode();
+        } else {
+          SaberBase::DoForce();
+        }
 #else
         SaberBase::DoForce();
 #endif
@@ -175,7 +170,7 @@ public:
           }
         }
 #if NUM_BUTTONS > 1
-        else{
+        else {
           SaberBase::DoForce();
         }
 #endif
@@ -194,63 +189,63 @@ public:
 #endif
           Off();
         }
-		return true;
+        return true;
 #endif
 
 #ifndef DISABLE_COLOR_CHANGE
 #ifdef NO_LOCKUP_HOLD
     case EVENTID(BUTTON_POWER, EVENT_CLICK_SHORT, MODE_ON | BUTTON_AUX):
       ToggleColorChangeMode();
-	    break;
-#else
+      break;
+#else //NO_LOCKUP_HOLD
     case EVENTID(BUTTON_AUX, EVENT_CLICK_SHORT, MODE_ON | BUTTON_POWER):
       ToggleColorChangeMode();
-	    break;    
-#endif
-#endif
+      break;
+#endif //NO_LOCKUP_HOLD
+#endif //DISABLE_COLOR_CHANGE
 #if NUM_BUTTONS > 1
       case EVENTID(BUTTON_POWER, EVENT_HELD_MEDIUM, MODE_ON):
       case EVENTID(BUTTON_POWER, EVENT_LATCH_OFF, MODE_ON):
       case EVENTID(BUTTON_AUX, EVENT_LATCH_OFF, MODE_ON):
       case EVENTID(BUTTON_AUX2, EVENT_LATCH_OFF, MODE_ON):
+#endif
 #if NUM_BUTTONS == 0
       case EVENTID(BUTTON_NONE, EVENT_TWIST, MODE_ON):
-#endif
 #endif
 #if NUM_BUTTONS != 1
         if (!SaberBase::Lockup()) {
 #ifndef DISABLE_COLOR_CHANGE
-	        if (SaberBase::GetColorChangeMode() != SaberBase::COLOR_CHANGE_MODE_NONE) {
-	          // Just exit color change mode.
-	          // Don't turn saber off.
-	          ToggleColorChangeMode();
-	          return true;
-	        }
+          if (SaberBase::GetColorChangeMode() != SaberBase::COLOR_CHANGE_MODE_NONE) {
+            // Just exit color change mode.
+            // Don't turn saber off.
+            ToggleColorChangeMode();
+            return true;
+          }
 #endif
           Off();
         }
 #endif
         return true;
 
-	  case EVENTID(BUTTON_NONE, EVENT_CLASH, MODE_ON | BUTTON_AUX):
+    case EVENTID(BUTTON_NONE, EVENT_CLASH, MODE_ON | BUTTON_AUX):
     case EVENTID(BUTTON_NONE, EVENT_CLASH, MODE_ON | BUTTON_POWER):
 #ifndef NO_LOCKUP_HOLD
 	  case EVENTID(BUTTON_AUX, EVENT_HELD, MODE_ON):
 #endif
-		if (!SaberBase::Lockup()) {
-		  if (pointing_down_) {
-			SaberBase::SetLockup(SaberBase::LOCKUP_DRAG);
-		  } else {
-			SaberBase::SetLockup(SaberBase::LOCKUP_NORMAL);
-		  }
-		  SaberBase::DoBeginLockup();
-	  return true;
-		}
+      if (!SaberBase::Lockup()) {
+        if (pointing_down_) {
+          SaberBase::SetLockup(SaberBase::LOCKUP_DRAG);
+        } else {
+          SaberBase::SetLockup(SaberBase::LOCKUP_NORMAL);
+        }
+        SaberBase::DoBeginLockup();
+        return true;
+      }
 
         // Off functions
       case EVENTID(BUTTON_POWER, EVENT_CLICK_LONG, MODE_OFF):
 #if NUM_BUTTONS == 1
-	  if (!MODE_VOLUME);
+	  if (!mode_volume_);
         next_preset();
 
         return true;
@@ -261,14 +256,13 @@ public:
 	return true;
 
       case EVENTID(BUTTON_AUX, EVENT_CLICK_LONG, MODE_OFF):
-      current_volume_ = dynamic_mixer.get_volume();
-      if (MODE_VOLUME){
-        MODE_VOLUME = false;
+      if (mode_volume_){
+        mode_volume_ = false;
         beeper.Beep(0.5, 3000);
         STDOUT.println("Exit Volume Menu");
       }
       else{
-        MODE_VOLUME = true;
+        mode_volume_ = true;
         beeper.Beep(0.5, 3000);
         STDOUT.println("Enter Volume Menu");
       }
@@ -279,7 +273,7 @@ public:
 	return true;
 
       case EVENTID(BUTTON_NONE, EVENT_CLASH, MODE_OFF | BUTTON_POWER):
-	    if (!MODE_VOLUME) {
+	    if (!mode_volume_) {
           next_preset();
 		}
 
@@ -288,7 +282,7 @@ public:
 #if NUM_BUTTONS < 3
       case EVENTID(BUTTON_POWER, EVENT_HELD_LONG, MODE_OFF):
 #endif
-        if (!MODE_VOLUME) {
+        if (!mode_volume_) {
           previous_preset();
 		}
 	return true;
@@ -303,30 +297,19 @@ public:
       case EVENTID(BUTTON_AUX2, EVENT_CLICK_SHORT, MODE_OFF):
       STDOUT.println(NUM_BUTTONS);
 #if NUM_BUTTONS == 3
-        if (MODE_VOLUME){
-          STDOUT.println("Volume Down");
-          if (dynamic_mixer.get_volume() > (0.10 * VOLUME)){
-            current_volume_ = dynamic_mixer.get_volume();
-            current_volume_ -= (VOLUME * 0.10) ;
-            dynamic_mixer.set_volume(current_volume_);
-            beeper.Beep(0.5, 2000);
-            STDOUT.print("Current Volume: ");
-            STDOUT.println(dynamic_mixer.get_volume());
-          }
-          else{
-            beeper.Beep(0.5, 1000);
-          }
+        if (mode_volume_) {
+          ChangeVolume(false);
         }
 
 #endif
 
 #ifdef DUAL_POWER_BUTTONS
-        if (!MODE_VOLUME) {
+        if (!mode_volume_) {
 			next_preset();
 
 		}
 #else
-        if (!MODE_VOLUME) {
+        if (!mode_volume_) {
 			previous_preset();
 
 		}
@@ -349,9 +332,7 @@ public:
 private:
   bool aux_on_ = true;
   bool pointing_down_ = false;
-  bool MODE_VOLUME = false;
-  int32_t current_volume_ = dynamic_mixer.get_volume();
+  bool mode_volume_ = false;
 };
 
 #endif
-
