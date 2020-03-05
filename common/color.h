d83c67c0eed6782736a708a2a04a52dc4079fab3
profezzorn <hubbe@hubbe.net>
2020-03-04 20:32:43 -0800

    compiles

--- common/color.h
+++ common/color.h
@@ -255,6 +255,8 @@ public:
 };
 
 struct OverDriveColor {
+  OverDriveColor() {}
+  OverDriveColor(const Color16 &c_, bool o) : c(c_), overdrive(o) {}
   Color16 c;
   bool overdrive;
   
@@ -263,5 +265,107 @@ struct OverDriveColor {
     c.printTo(p);
   }
 };
+
+// SIMPLE
+// Opaque.fade -> Opaque or Alpha?
+// Alpha.fade -> Alpha
+// PMAlpha.fade -> PMAlpha?
+// Opaque mix Opaque -> Opaque
+// Opaque mix Alpha -> PmAlpha
+// Opaque mix PmAlpha -> PmAlpha
+// Alpha mix PmAlpha -> PmAlpha
+// Opaque paste Opaque -> Opaque
+// Opaque paste Alpha -> Opaque
+// Opaque paste PmAlpha -> Opaque
+// Alpha paste Alpha -> PmAlpha
+// Alpha paste PmAlpha -> PmAlpha
+// Alpha paste PmAlpha -> PmAlpha
+
+// mix: A, B  A * (1-x) + B * x
+// paint B over A: B + A * (1 - B.a)
+
+struct Color32 {
+  Color32(uint32_t r_, uint32_t g_, uint32_t b_) : r(r_), g(g_), b(b_) {}
+  Color16 operator>>(int shift) {
+    return Color16(r >> shift, g >> shift, b >> shift);
+  }
+  Color32 operator+(const Color32& other) { return Color32(r + other.r, g + other.g, b + other.b); }
+  uint32_t r, g, b;
+};
+
+Color32 operator*(const Color16& c, uint16_t x) {
+  return Color32(c.r * x, c.g * x, c.b * x);
+}
+Color16 operator+(const Color16& a, const Color16 &b) {
+  return Color16(a.r + b.r, a.g + b.g, a.b + b.b);
+}
+
+// Unmultiplied RGBA, used as a temporary and makes optimization easier.
+struct RGBA_um {
+  RGBA_um(Color16 c_, bool od, uint16_t a) : c(c_), overdrive(od), alpha(a) {}
+  Color16 c;
+  bool overdrive;
+  uint16_t alpha;
+
+  void printTo(Print& p) {
+    if (overdrive) p.write('!');
+    c.printTo(p);
+    p.write(',');
+    p.print(alpha);
+  }
+};
+
+// Premultiplied ALPHA
+struct RGBA {
+  RGBA(Color16 c_, bool od, uint16_t a) : c(c_), overdrive(od), alpha(a) {}
+  RGBA(const RGBA_um& rgba) : c(rgba.c * rgba.alpha >> 15), overdrive(rgba.overdrive), alpha(rgba.alpha) {}
+  RGBA(const OverDriveColor& o) : c(o.c), overdrive(o.overdrive), alpha(32768) {}
+  Color16 c;
+  bool overdrive;
+  uint16_t alpha;
+  void printTo(Print& p) {
+    if (overdrive) p.write('!');
+    c.printTo(p);
+    p.write('*');
+    p.print(alpha);
+  }
+};
+
+RGBA_um operator*(const RGBA_um& a, uint16_t x) {
+  return RGBA_um(a.c, a.overdrive, a.alpha * x >> 15);
+}
+
+RGBA operator*(const RGBA& a, uint16_t x) {
+  return RGBA(a.c * x >> 15, a.overdrive, a.alpha * x >> 15);
+}
+
+OverDriveColor MixColors(OverDriveColor a, OverDriveColor b, int x, int shift) {
+  return OverDriveColor( (a.c * ((1 << shift) - x) + b.c * x) >> shift,
+			 x > (1 << (shift - 1)) ? b.overdrive : a.overdrive);
+}
+RGBA MixColors(RGBA a, RGBA b, int x, int shift) {
+  return RGBA( (a.c * ((1 << shift) - x) + b.c * x) >> shift,
+	       x > (1 << (shift - 1)) ? b.overdrive : a.overdrive,
+	       (a.alpha * ((1 << shift) - x) + b.alpha * x) >> shift);
+}
+
+// Paint over operators
+OverDriveColor operator<<(const OverDriveColor& base, const RGBA& over) {
+  uint16_t ac = 32768 - over.alpha;
+  return OverDriveColor((base.c * ac >> 15) + over.c,
+			over.alpha > 16384 ? over.overdrive : base.overdrive);
+}
+RGBA operator<<(const RGBA_um& base, const RGBA& over) {
+  uint16_t ac = 32768 - over.alpha;
+  return RGBA((base.c * (base.alpha * ac >> 15) >> 15) + over.c,
+	      over.alpha > 16384 ? over.overdrive : base.overdrive,
+	      (base.alpha * ac >> 15) + over.alpha);
+}
+RGBA operator<<(const RGBA& base, const RGBA& over) {
+  uint16_t ac = 32768 - over.alpha;
+  return RGBA((base.c * ac >> 15) + over.c,
+	      over.alpha > 16384 ? over.overdrive : base.overdrive,
+	      (base.alpha * ac >> 15) + over.alpha);
+}
 
 #endif
