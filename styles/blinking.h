07a74698654d5a2309058c783d56026aa54b4bc7
profezzorn <hubbe@hubbe.net>
2020-03-05 00:31:10 -0800

    blinking -> layer + function

--- styles/blinking.h
+++ styles/blinking.h
@@ -17,7 +17,8 @@
 // second half. If BLINK_PROMILLE is smaller, we get less A and more B.
 // If BLINK_PROMILLE is 0, we get all B.
 // If BLINK_PROMILLE is 1000 we get all A.
-// 
+//
+#if 0
 template<class COLOR1,
   class COLOR2,
   typename BLINK_MILLIS,
@@ -59,11 +60,22 @@ private:
   bool on_;
 };
 
+#else
+
+#include "../functions/blinking.h"
+
+template<class COLOR2, typename BLINK_MILLIS, typename BLINK_PROMILLE>
+  using BlinkingL = AlphaL<COLOR2, BlinkingF<BLINK_MILLIS, BLINK_PROMILLE>>;
+
+template<class COLOR1, class COLOR2, typename BLINK_MILLIS, typename BLINK_PROMILLE>
+  using BlinkingX = Layers<COLOR1, BlinkingL<COLOR2, BLINK_MILLIS, BLINK_PROMILLE>>;
+
 template<class COLOR1,
   class COLOR2,
   int BLINK_MILLIS,
   int BLINK_PROMILLE = 500>
   using Blinking = BlinkingX<COLOR1, COLOR2, Int<BLINK_MILLIS>, Int<BLINK_PROMILLE> >;
 
+#endif
 
 #endif
