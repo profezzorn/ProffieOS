a01cd492c26be7ef6dcaeac55b61f751ddb9ee1b
profezzorn <hubbe@hubbe.net>
2020-03-06 17:45:51 -0800

    make RotateColors<> handle transparent colors

--- styles/rotate_color.h
+++ styles/rotate_color.h
@@ -17,14 +17,15 @@ public:
     return RunStyle(&color_, blade);
   }
 
-  OverDriveColor getColor(int led) {
-    OverDriveColor c = color_.getColor(led);
+private:
+  COLOR color_;
+  ROTATION rotation_;
+public:
+  auto getColor(int led) -> decltype(color_.getColor(led)) {
+    auto c = color_.getColor(led);
     c.c = c.c.rotate((rotation_.getInteger(led) & 0x7fff) * 3);
     return c;
   }
-
-  COLOR color_;
-  ROTATION rotation_;
 };
 
 template<int rotation, class COLOR> using RotateColors = RotateColorsX<Int<rotation>, COLOR>;
