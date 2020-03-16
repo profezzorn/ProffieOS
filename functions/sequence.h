72c1655723b412e6d23238e28b3b6a83054256aa
profezzorn <hubbe@hubbe.net>
2020-03-15 23:26:43 -0700

    fix SequenceF

--- functions/sequence.h
+++ functions/sequence.h
@@ -8,12 +8,12 @@ public:
     static uint16_t sequence_[] = { sequence... };
     uint32_t now = millis();
     uint32_t bit = (now / millis_per_bit) % std::min<size_t>(bits, sizeof...(sequence) * 16);
-    on_ = !!((sequence_[bit >> 4] >> ((~bit) & 0xf)) & 1);
+    value_ = 32768 * ((sequence_[bit >> 4] >> ((~bit) & 0xf)) & 1);
   }
-  int getColor(int led) { return on_ * 32768; }
+  int getInteger(int led) { return value_; }
   
 private:
-  bool on_;
+  int value_;
 };
 
 #endif
