c96adaadde8d5d7bc56a0723581ba168d579b3bd
profezzorn <hubbe@hubbe.net>
2020-03-25 10:40:12 -0700

    IsLessThan cleanup

--- functions/islessthan.h
+++ functions/islessthan.h
@@ -3,7 +3,7 @@
 
 // Usage: IsLessThan<F, V>
 // Returns 0 or 32768 based on V
-// If F < V returns 0, if F >= V returns 32768 
+// If F < V returns 32768, if F >= V returns 0 
 // F, V: INTEGER
 // return value: INTEGER
 
@@ -15,21 +15,14 @@ public:
   void run(BladeBase* blade) {
     f_.run(blade);
     v_.run(blade);
-    int f = f_.getInteger(0);
-    int v = v_.getInteger(0);
-    if(f >= v) {
-    value_ = 0;
-    } else {
-    value_ = 32768;
-    }
   }
-  int getInteger(int led) { return value_; }
+  int getInteger(int led) {
+    return (f_.getInteger(led) < v_.getInteger(0)) << 15;
+  }
 
 private:
   F f_;
   V v_;
-  int value_;
-  
 };
 
 #endif
