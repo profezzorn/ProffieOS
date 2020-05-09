0154194e8b4cd0f6ccd735a9a2d3c792e5dde3fe
SA-22C <45020724+SA-22C@users.noreply.github.com>
2020-05-09 14:34:34 -0600

    Add files via upload

--- props/saber_shtok_buttons.h
+++ props/saber_shtok_buttons.h
@@ -8,9 +8,13 @@
 // Lockup - hold + hit clash while ON
 // Drag - hold + hit clash while ON pointing the blade tip down
 // Blaster Blocks - short click while ON
-// Force Effects - twist the hilt + press and hold the button while ON (while pointing up)
-// Enter Color Change mode - twist the hilt + press and hold the button while ON (pointing down)
+// Force Effects - twist the hilt + press and hold the button while ON (while pointing blade up)
+// Enter Color Change mode - twist the hilt + press and hold the button while ON (pointing blade down)
 // Confirm selected color in Color Change mode - hold the button until confirmation sound
+// Melt - hold the button + stab while ON
+// Lightning Block - fast double click + hold the button while ON
+// Enter Multi-Block mode - swing + hold the button for 1 second and release while ON (now swing the saber, blaster blocks will trigger automatically)
+// Exit Multi-Block mode - short click while ON
 //
 // 2 Buttons:
 // Activate Muted - fast double click Activation button while OFF
@@ -25,6 +29,10 @@
 // Force Effects - short click Activation button while ON
 // Enter Color Change mode - hold AUX and click Activation button while ON
 // Confirm selected color in Color Change mode - hold the button until confirmation sound
+// Melt - hold AUX (or Activation) button + perform stab action while ON
+// Lightning Block - hold Activation button + short click AUX button while ON
+// Enter Multi-Block mode - swing + hold the button for 1 second and release while ON (now swing the saber, blaster blocks will trigger automatically)
+// Exit Multi-Block mode - short click AUX button while ON
 
 #ifndef PROPS_SABER_SHTOK_BUTTONS_H
 #define PROPS_SABER_SHTOK_BUTTONS_H
@@ -34,12 +42,24 @@
 #undef PROP_TYPE
 #define PROP_TYPE SaberShtokButtons
 
-// The Saber class implements the basic states and actions
-// for the saber.
+// The Saber class implements the basic states and actions for the saber.
 class SaberShtokButtons : public PropBase {
 public:
-  SaberShtokButtons() : PropBase() {}
+SaberShtokButtons() : PropBase() {}
   const char* name() override { return "SaberShtokButtons"; }
+
+// EVENT_SWING
+  bool swinging_ = false;
+  void Loop() override {
+    PropBase::Loop();
+    if (!swinging_ && fusor.swing_speed() > 250) {
+      swinging_ = true;
+      Event(BUTTON_NONE, EVENT_SWING);
+    }
+    if (swinging_ && fusor.swing_speed() < 100) {
+      swinging_ = false;
+    }
+  }
 
   bool Event2(enum BUTTON button, EVENT event, uint32_t modifiers) override {
     switch (EVENTID(button, event, modifiers)) {
@@ -50,7 +70,7 @@ public:
         } else {
           pointing_down_ = false;
         }
-      return true;
+        return true;
 
 // 1-button code
 
@@ -63,7 +83,7 @@ public:
       case EVENTID(BUTTON_POWER, EVENT_CLICK_SHORT, MODE_OFF):
         aux_on_ = false;
         On();
-	return true;
+        return true;
 
 // Next Preset
       case EVENTID(BUTTON_POWER, EVENT_CLICK_LONG, MODE_OFF):
@@ -73,7 +93,7 @@ public:
 #else
         next_preset();
 #endif
-	return true;
+        return true;
 
 // Previous Preset
       case EVENTID(BUTTON_POWER, EVENT_HELD_LONG, MODE_OFF):
@@ -83,20 +103,20 @@ public:
 #else
         previous_preset();
 #endif
-	return true;
+        return true;
 
 // Activate Muted
-      case EVENTID(BUTTON_POWER, EVENT_DOUBLE_PRESSED, MODE_ON):
-	if (millis() - activated_ < 500) {
-	  if (SetMute(true)) {
-	    unmute_on_deactivation_ = true;
-	  }
-	}
-	return true;
+      case EVENTID(BUTTON_POWER, EVENT_DOUBLE_CLICK, MODE_ON):
+        if (millis() - activated_ < 500) {
+          if (SetMute(true)) {
+            unmute_on_deactivation_ = true;
+          }
+        }
+        return true;
 
-// Turn Blade OFF	
+// Turn Blade OFF
       case EVENTID(BUTTON_POWER, EVENT_HELD_LONG, MODE_ON):
-    if (!SaberBase::Lockup()) {
+        if (!SaberBase::Lockup()) {
 #ifndef DISABLE_COLOR_CHANGE
           if (SaberBase::GetColorChangeMode() != SaberBase::COLOR_CHANGE_MODE_NONE) {
             // Just exit color change mode.
@@ -105,8 +125,9 @@ public:
             return true;
           }
 #endif
-      Off();
-    }
+          Off();
+        }
+        swing_blast_ = false;
         return true;
 
 // Force and Color Change mode
@@ -120,12 +141,25 @@ public:
 #else
         SaberBase::DoForce();
 #endif
-	return true;
+        return true;
 
 // Blaster Deflection
       case EVENTID(BUTTON_POWER, EVENT_CLICK_SHORT, MODE_ON):
+        swing_blast_ = false;
         SaberBase::DoBlast();
-	return true;
+        return true;
+
+// Multi-Blaster Deflection mode
+      case EVENTID(BUTTON_NONE, EVENT_SWING, MODE_ON | BUTTON_POWER):
+        swing_blast_ = true;
+        hybrid_font.SB_Blast();
+        return true;
+
+      case EVENTID(BUTTON_NONE, EVENT_SWING, MODE_ON):
+        if (swing_blast_) {
+          SaberBase::DoBlast();
+        }
+        return true;
 
 // Lockup
       case EVENTID(BUTTON_NONE, EVENT_CLASH, MODE_ON | BUTTON_POWER):
@@ -136,19 +170,39 @@ public:
           } else {
             SaberBase::SetLockup(SaberBase::LOCKUP_NORMAL);
           }
+          swing_blast_ = false;
           SaberBase::DoBeginLockup();
-	  return true;
+          return true;
+        }
+        break;
+
+// Lightning Block
+      case EVENTID(BUTTON_POWER, EVENT_SECOND_HELD, MODE_ON):
+        SaberBase::SetLockup(SaberBase::LOCKUP_LIGHTNING_BLOCK);
+        swing_blast_ = false;
+        SaberBase::DoBeginLockup();
+        return true;
+
+// Melt
+      case EVENTID(BUTTON_NONE, EVENT_STAB, MODE_ON | BUTTON_POWER):
+      case EVENTID(BUTTON_NONE, EVENT_STAB, MODE_ON | BUTTON_AUX):
+        if (!SaberBase::Lockup()) {
+          SaberBase::SetLockup(SaberBase::LOCKUP_MELT);
+          swing_blast_ = false;
+          SaberBase::DoBeginLockup();
+          return true;
         }
         break;
 
 // Start or Stop Track
       case EVENTID(BUTTON_POWER, EVENT_CLICK_LONG, MODE_ON):
         StartOrStopTrack();
-	return true;
+        return true;
 
       case EVENTID(BUTTON_POWER, EVENT_PRESSED, MODE_OFF):
         SaberBase::RequestMotion();
-	return true;
+        return true;
+
 
 // 2-button code
 
@@ -161,7 +215,7 @@ public:
       case EVENTID(BUTTON_POWER, EVENT_CLICK_SHORT, MODE_OFF):
         aux_on_ = false;
         On();
-	return true;
+        return true;
 
 // Next Preset
       case EVENTID(BUTTON_AUX, EVENT_CLICK_SHORT, MODE_OFF):
@@ -171,22 +225,22 @@ public:
 #else
         next_preset();
 #endif
-	return true;
+        return true;
 
 // Activate Muted
-      case EVENTID(BUTTON_POWER, EVENT_DOUBLE_PRESSED, MODE_ON):
-	if (millis() - activated_ < 500) {
-	  if (SetMute(true)) {
-	    unmute_on_deactivation_ = true;
-	  }
-	}
-	return true;
+      case EVENTID(BUTTON_POWER, EVENT_DOUBLE_CLICK, MODE_ON):
+        if (millis() - activated_ < 500) {
+          if (SetMute(true)) {
+            unmute_on_deactivation_ = true;
+          }
+        }
+        return true;
 
-// Turn Blade OFF	
+// Turn Blade OFF
       case EVENTID(BUTTON_POWER, EVENT_HELD_LONG, MODE_ON):
       case EVENTID(BUTTON_AUX, EVENT_LATCH_OFF, MODE_ON):
       case EVENTID(BUTTON_AUX2, EVENT_LATCH_OFF, MODE_ON):
-    if (!SaberBase::Lockup()) {
+        if (!SaberBase::Lockup()) {
 #ifndef DISABLE_COLOR_CHANGE
           if (SaberBase::GetColorChangeMode() != SaberBase::COLOR_CHANGE_MODE_NONE) {
             // Just exit color change mode.
@@ -195,38 +249,70 @@ public:
             return true;
           }
 #endif
-      Off();
-    }
+          Off();
+        }
+        swing_blast_ = false;
         return true;
 
 // Force
       case EVENTID(BUTTON_POWER, EVENT_CLICK_SHORT, MODE_ON):
         SaberBase::DoForce();
-	return true;
+        return true;
 
 // Color Change mode
 #ifndef DISABLE_COLOR_CHANGE
       case EVENTID(BUTTON_POWER, EVENT_CLICK_SHORT, MODE_ON | BUTTON_AUX):
-	ToggleColorChangeMode();
-	break;
+        ToggleColorChangeMode();
+        break;
 #endif
 
 // Blaster Deflection
       case EVENTID(BUTTON_AUX, EVENT_CLICK_SHORT, MODE_ON):
+        swing_blast_ = false;
         SaberBase::DoBlast();
-	return true;
+        return true;
+
+// Multi-Blaster Deflection mode
+      case EVENTID(BUTTON_NONE, EVENT_SWING, MODE_ON | BUTTON_POWER):
+        swing_blast_ = true;
+        hybrid_font.SB_Blast();
+        return true;
+
+      case EVENTID(BUTTON_NONE, EVENT_SWING, MODE_ON):
+        if (swing_blast_) {
+          SaberBase::DoBlast();
+        }
+        return true;
 
 // Lockup
       case EVENTID(BUTTON_AUX, EVENT_HELD, MODE_ON):
-      case EVENTID(BUTTON_NONE, EVENT_CLASH, MODE_ON | BUTTON_AUX):
         if (!SaberBase::Lockup()) {
           if (pointing_down_) {
             SaberBase::SetLockup(SaberBase::LOCKUP_DRAG);
           } else {
             SaberBase::SetLockup(SaberBase::LOCKUP_NORMAL);
           }
+          swing_blast_ = false;
           SaberBase::DoBeginLockup();
-	  return true;
+          return true;
+        }
+        break;
+
+// Lightning Block
+      case EVENTID(BUTTON_AUX, EVENT_CLICK_SHORT, MODE_ON | BUTTON_POWER):
+        SaberBase::SetLockup(SaberBase::LOCKUP_LIGHTNING_BLOCK);
+        swing_blast_ = false;
+        SaberBase::DoBeginLockup();
+        return true;
+
+// Melt
+      case EVENTID(BUTTON_NONE, EVENT_STAB, MODE_ON | BUTTON_POWER):
+      case EVENTID(BUTTON_NONE, EVENT_STAB, MODE_ON | BUTTON_AUX):
+        if (!SaberBase::Lockup()) {
+          SaberBase::SetLockup(SaberBase::LOCKUP_MELT);
+          swing_blast_ = false;
+          SaberBase::DoBeginLockup();
+          return true;
         }
         break;
 
@@ -234,21 +320,21 @@ public:
       case EVENTID(BUTTON_POWER, EVENT_CLICK_LONG, MODE_OFF):
       case EVENTID(BUTTON_POWER, EVENT_CLICK_LONG, MODE_ON):
         StartOrStopTrack();
-	return true;
+        return true;
 
       case EVENTID(BUTTON_POWER, EVENT_PRESSED, MODE_OFF):
         SaberBase::RequestMotion();
-	return true;
+        return true;
 
       case EVENTID(BUTTON_NONE, EVENT_CLASH, MODE_OFF | BUTTON_POWER):
         next_preset();
-	return true;
+        return true;
 
 
 // Previous Preset
       case EVENTID(BUTTON_POWER, EVENT_CLICK_SHORT, MODE_OFF | BUTTON_AUX):
         previous_preset();
-	return true;
+        return true;
 
 #else
 #error only 1 and 2 buttons are supported by this mod
@@ -278,22 +364,25 @@ public:
 #else
         previous_preset();
 #endif
-	return true;
+        return true;
 
 // Events that needs to be handled regardless of what other buttons are pressed.
       case EVENTID(BUTTON_POWER, EVENT_RELEASED, MODE_ANY_BUTTON | MODE_ON):
       case EVENTID(BUTTON_AUX, EVENT_RELEASED, MODE_ANY_BUTTON | MODE_ON):
-	if (SaberBase::Lockup()) {
-	  SaberBase::DoEndLockup();
-	  SaberBase::SetLockup(SaberBase::LOCKUP_NONE);
-	  return true;
-	}
+        if (SaberBase::Lockup()) {
+          SaberBase::DoEndLockup();
+          SaberBase::SetLockup(SaberBase::LOCKUP_NONE);
+          swing_blast_ = false;
+          return true;
+        }
     }
     return false;
   }
+
 private:
   bool aux_on_ = true;
   bool pointing_down_ = false;
+  bool swing_blast_ = false;
 };
 
 #endif
