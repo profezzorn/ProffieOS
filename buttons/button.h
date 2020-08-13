a9ad6b9abc90192356de56366c3fe3681a8f4df6
Rob van de Pijpekamp <info@artandtech.com>
2020-08-13 20:57:11 +0200

    modified:   button.h
    Added option for pull up or pull down connection for buttons

--- buttons/button.h
+++ buttons/button.h
@@ -5,17 +5,30 @@
 
 class Button : public ButtonBase {
 public:
-  Button(enum BUTTON button, int pin, const char* name) : ButtonBase(name, button), pin_(pin) {
-    pinMode(pin, INPUT_PULLUP);
+  Button(enum BUTTON button, int pin, const char* name, bool pullup = true) : ButtonBase(name, button), pin_(pin) {
+	pullup_ = pullup;  
+    if (pullup){
+		pinMode(pin, INPUT_PULLUP);
 #ifdef ENABLE_SNOOZE
-    snooze_digital.pinMode(pin, INPUT_PULLUP, RISING);
+		snooze_digital.pinMode(pin, INPUT_PULLUP, RISING);
 #endif
+    }else{
+		pinMode(pin, INPUT_PULLDOWN);
+#ifdef ENABLE_SNOOZE
+		snooze_digital.pinMode(pin, INPUT_PULLDOWN, FALLING);
+#endif
+	}
   }
 protected:
   bool Read() override {
-    return digitalRead(pin_) == LOW;
+	if (pullup_){  
+		return digitalRead(pin_) == LOW;
+	}else{
+		return digitalRead(pin_) == HIGH;
+	}
   }
   uint8_t pin_;
+  bool pullup_;
 };
 
 #endif
