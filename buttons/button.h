246ce84686aff4eda38b78edccf5e0adee346fd9
Rob van de Pijpekamp <info@art-and-tech.com>
2020-08-15 16:22:36 +0200

    modified:   button.h
        added ButtonPullDown class for pulldown connected switches

--- buttons/button.h
+++ buttons/button.h
@@ -3,32 +3,65 @@
 
 #include "button_base.h"
 
+// For all setups following the wiring diagram (V2) described on https://fredrik.hubbe.net/lightsaber/v5/
+// 
+// momentary buttons are expected to be connected to gnd and 1 of the Proffieboard/Teensy input pins
+//
+// Usage example, put this in the button config section: 
+// Button PowerButton(BUTTON_POWER, powerButtonPin, "pow");  
+//
+
 class Button : public ButtonBase {
 public:
-  Button(enum BUTTON button, int pin, const char* name, bool pullup = true) : ButtonBase(name, button), pin_(pin) {
-	pullup_ = pullup;  
-    if (pullup){
-		pinMode(pin, INPUT_PULLUP);
+
+  Button(enum BUTTON button, int pin, const char* name) : ButtonBase(name, button), pin_(pin) {
+    pinMode(pin, INPUT_PULLUP);
 #ifdef ENABLE_SNOOZE
-		snooze_digital.pinMode(pin, INPUT_PULLUP, RISING);
+    snooze_digital.pinMode(pin, INPUT_PULLUP, RISING);
 #endif
-    }else{
-		pinMode(pin, INPUT_PULLDOWN);
+  }
+
+protected:
+  uint8_t pin_;
+
+  bool Read() override {
+    return digitalRead(pin_) == LOW;
+  }
+};
+
+
+// For special setups were you want to hook up a momentary button to VCC(3.3V) or Vbat
+// and the other end to the inputs of the Proffieboard / Teensy 
+// 
+// For Proffieboards all inputs can handle Vbat, except for Data3 / blade3pin
+//
+// This class will add some additional bytes to your code, 
+// (224 bytes, for a 4 button setup with 2 button pullup, and 2 buttons pulldown)
+// However, if all of your used switches are made ButtonPullDown 
+// it will save you a couple of bytes compared to the original Button class 
+// (8 bytes for 4 button setup)
+//
+// Usage example, put this in the button config section: 
+// ButtonPullDown PowerButton(BUTTON_POWER, powerButtonPin, "pow");  
+//
+
+class ButtonPullDown : public ButtonBase{
+public:
+  ButtonPullDown(enum BUTTON button, int pin, const char* name) : ButtonBase(name, button), pin_(pin) {
+    pinMode(pin, INPUT_PULLDOWN);
 #ifdef ENABLE_SNOOZE
-		snooze_digital.pinMode(pin, INPUT_PULLDOWN, FALLING);
+    snooze_digital.pinMode(pin, INPUT_PULLDOWN, FALLING);
 #endif
-	}
   }
 protected:
-  bool Read() override {
-	if (pullup_){  
-		return digitalRead(pin_) == LOW;
-	}else{
-		return digitalRead(pin_) == HIGH;
-	}
-  }
   uint8_t pin_;
-  bool pullup_;
+
+  bool Read() override {
+	 return digitalRead(pin_) == HIGH;
+  }
+   
 };
 
 #endif
+
+
