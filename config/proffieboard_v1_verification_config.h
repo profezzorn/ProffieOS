35f58a694e556fc94356e3c98481ff1eabe29635
profezzorn <hubbe@hubbe.net>
2020-10-01 23:13:08 -0700

    more disambiguation

--- config/proffieboard_v1_verification_config.h
+++ config/proffieboard_v1_verification_config.h
@@ -59,7 +59,10 @@ Preset presets[] = {
     StyleRainbowPtr<300, 800>(),
     StyleRainbowPtr<300, 800>(), "rainbow"},
   { "TthCrstl", "tracks/uranus.wav",
-    StyleStrobePtr<WHITE, Rainbow, 15, 300, 800>(),
+    StylePtr<Layers<
+      Green,
+      TransitionEffectL<TrConcat<TrWipeIn<300>,AlphaL<Rgb<128,0,0>,SmoothStep<Int<20000>,Int<2000>>>,TrJoin<TrSmoothFade<900>,TrWipe<700>>>,EFFECT_STAB>,
+      InOutTrL<TrWipeSparkTip<White,250>,TrWipeInX<Int<700>>>>>(),
     StyleStrobePtr<WHITE, Rainbow, 15, 300, 800>(), "strobe"},
   { "TeensySF", "tracks/venus.wav",
     &style_pov,
