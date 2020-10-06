e15526df42e2181209f6b5f9fcea5f130f7dc9d1
fdarosa2663 <fernando_darosa@hotmail.com>
2020-10-06 10:58:28 -0400

    Update saber_fett263_buttons.h
    new controls, defines and features

--- props/saber_fett263_buttons.h
+++ props/saber_fett263_buttons.h
@@ -1,37 +1,48 @@
 // Fett263 Buttons with Gesture Controls and Battle Mode with Smart Lockup
 //
 // Includes Gesture Controls, "Battle Mode" with "Smart Lockup", "Multi-Blast" Mode (to enable Swing Blast control)
-// "Multi-Phase" Mode and SA22C volume menu
+// "Multi-Phase" Mode, "On Demand Power Save", "On Demand Battery Level", "Fast On" Ignition (no preon) and SA22C volume menu
+//
 // Optional Defines
 //
-// FETT263_BATTLE_MODE
-// To enable Battle Mode, Swing On and Twist Off gestures enabled automatically
+// FETT263_BATTLE_MODE_ALWAYS_ON
+// Battle Mode is always on, normal controls deactivated
+// This will disable traditional Clash and Stab effects 
 //
 // FETT263_LOCKUP_DELAY 200
 // This is the "delay" in millis to determine Clash vs Lockup
 //
+// FETT263_BM_DISABLE_OFF_BUTTON
+// During Battle Mode Power Button Retraction is disabled
+//
 // FETT263_SWING_ON
-// To enable Swing On Ignition control (automatically enters Battle Mode)
+// To enable Swing On Ignition control (automatically enters Battle Mode, uses Fast On)
+//
+// FETT263_SWING_ON_NO_BM
+// To enable Swing On Ignition control but not activate Battle Mode
+//
+// FETT263_SWING_ON_SPEED 250
+// Adjust Swing Speed required for Ignition 250 ~ 500 recommended
 //
 // FETT263_TWIST_OFF
 // To enable Twist Off Retraction control
 //
 // FETT263_STAB_ON
-// To enable Stab On Ignition control (automatically enters Battle Mode)
+// To enable Stab On Ignition control (automatically enters Battle Mode, uses Fast On)
 //
-// FETT263_TWIST_ON
-// To enable Twist On Ignition control (automatically enters Battle Mode)
+// FETT263_STAB_ON_NO_BM
+// To enable Stab On Ignition control but not activate Battle Mode
+//
+// FETT263_GESTURE_PREON
+// Disables Fast On ignition on gesture so Preon is used
 //
 // FETT263_MULTI_PHASE
 // This will enable "live" preset change to create a "Multi-Phase" saber effect
 // with preset changes on the fly while blade is ignited.
 //
 // 2 Button Controls (PWR and AUX):
-// "Battle Mode" - Swing while saber is OFF to ignite in Battle Mode
-// or hold AUX and Swing while blade is ON to toggle mode
-//
-// Ignite (ON) - click PWR while OFF or Swing Saber while OFF
-//
+// "Battle Mode" - hold AUX and Swing while blade is ON to toggle mode ON/OFF
+// Ignite (ON) - click PWR while OFF (Swing On and Stab On available with defines)
 // Muted Ignition (ON) - double click PWR while OFF
 // Retract (OFF) - click PWR while ON (not swinging or in Battle Mode)
 // Twist Off (OFF) if in Battle Mode
@@ -41,21 +52,35 @@
 // Clash - clash blade while ON
 // Stab - thrust forward and clash blade while ON
 // Lockup - hold AUX and clash while ON
-// Drag - hold AUX and clash pointing down while ON
+// Drag - hold AUX and stab pointing down while ON
 // Lightning Block - hold PWR and click AUX while ON
 // Melt - hold PWR (or AUX) and thrust forward and clash while ON
 // Force - hold and release PWR while ON
-// Color Change - hold AUX and click PWR while ON to enter CCWheel,
+// Power Save - hold Aux and click PWR while ON (pointing up) to use Power Save (requires style)
+// Color Change - hold AUX and click PWR while ON (parallel or down) to enter CCWheel,
 // turn hilt to rotate through colors, click PWR to select/exit
 // if using COLOR_CHANGE_DIRECT each button press advances one Color at a time
-// Next Preset - click AUX while OFF
+//
+// Next Preset - click AUX while OFF (parallel or up)
+// Previous Preset - click Aux while OFF (point down)
 // MULTI_PHASE Next Preset - hold AUX and TWIST while ON (use define to enable)
-// Previous Preset - hold AUX and click PWR while OFF
 // MULTI_PHASE Previous Preset - hold PWR and TWIST while ON (use define to enable)
+// Battery Level - hold AUX and click PWR while OFF (requires style)
 // Enter SA22C Volume Menu - hold and release AUX while OFF
 // Volume Up (10% increment, 100% max) - click PWR while in Volume Menu while OFF
 // Volume Down (10% increment) - click AUX while in Volume Menu while OFF
 // Exit Volume Menu - hold and release AUX while in Volume Menu while OFF
+//
+// Custom sounds supported:
+// On Demand Power Save - dim.wav
+// On Demand Battery Level - battery.wav
+// Battle Mode On (on toggle) - bmbegin.wav
+// Battle Mode Off (on toggle) - bmend.wav
+// Enter Volume Menu - vmbegin.wav
+// Exit Volume Menu - vmend.wav
+// Fast On (optional) - faston.wav
+// Multi-Blast Mode On - blstbgn.wav
+// Multi-Blast Mode Off - blstend.wav
 
 #ifndef PROPS_SABER_FETT263_BUTTONS_H
 #define PROPS_SABER_FETT263_BUTTONS_H
@@ -64,8 +89,20 @@
 #define MOTION_TIMEOUT 60 * 15 * 1000
 #endif
 
+#ifndef FETT263_SWING_ON_SPEED
+#define FETT263_SWING_ON_SPEED 250
+#endif
+
 #ifndef FETT263_LOCKUP_DELAY
 #define FETT263_LOCKUP_DELAY 200
+#endif
+
+#if defined(FETT263_SWING_ON) && defined(FETT263_SWING_ON_NO_BM)
+#error You cannot define both FETT263_SWING_ON and FETT263_SWING_ON_NO_BM
+#endif
+
+#if defined(FETT263_STAB_ON) && defined(FETT263_STAB_ON_NO_BM)
+#error You cannot define both FETT263_STAB_ON and FETT263_STAB_ON_NO_BM
 #endif
 
 #include "prop_base.h"
@@ -73,6 +110,15 @@
 
 #undef PROP_TYPE
 #define PROP_TYPE SaberFett263Buttons
+
+EFFECT(dim);
+EFFECT(battery);
+EFFECT(bmbegin);
+EFFECT(bmend);
+EFFECT(vmbegin);
+EFFECT(vmend);
+EFFECT(faston);
+EFFECT(blstbgn);
 
 // The Saber class implements the basic states and actions
 // for the saber.
@@ -108,14 +154,31 @@ SaberFett263Buttons() : PropBase() {}
       // Swing On gesture control this portion allows fine tuning of speed needed to ignite
       if (millis() - saber_off_time_ < MOTION_TIMEOUT) {
         SaberBase::RequestMotion();
-        // Edit '250' value in line below to change swing on sensitivity when off
-        // 250 ~ 400 work best in testing
-        if (!swinging_ && fusor.swing_speed() > 250) {
+        if (!swinging_ && fusor.swing_speed() > FETT263_SWING_ON_SPEED) {
           swinging_ = true;
           Event(BUTTON_NONE, EVENT_SWING);
         }
       }
     }
+  }
+
+// Fast On Gesture Ignition
+  virtual void FastOn() {
+    if (IsOn()) return;
+    if (current_style() && current_style()->NoOnOff())
+      return;
+    activated_ = millis();
+    STDOUT.println("Ignition.");
+    MountSDCard();
+    EnableAmplifier();
+    SaberBase::RequestMotion();
+    // Avoid clashes a little bit while turning on.
+    // It might be a "clicky" power button...
+    IgnoreClash(500);
+    SaberBase::TurnOn();
+    // Optional effects
+    SaberBase::DoEffect(EFFECT_FAST_ON, 0);
+    hybrid_font.PlayCommon(&SFX_faston);
   }
 
   // SA22C Volume Menu
@@ -223,10 +286,15 @@ SaberFett263Buttons() : PropBase() {}
           return true;
         }
 #endif
-        if (!swinging_ && !battle_mode_) {
+        if (!swinging_) {
           swing_blast_ = false;
-          Off();
+#ifndef FETT263_BM_DISABLE_OFF_BUTTON
+	  Off();
           saber_off_time_ = millis();
+#endif
+#ifndef FETT263_BATTLE_MODE_ALWAYS_ON
+	  battle_mode_ = false;
+#endif
         }
         return true;
 
@@ -244,30 +312,35 @@ SaberFett263Buttons() : PropBase() {}
 
       case EVENTID(BUTTON_AUX, EVENT_CLICK_LONG, MODE_ON):
         swing_blast_ = true;
-        hybrid_font.SB_Effect(EFFECT_BLAST, 0);
+        hybrid_font.PlayCommon(&SFX_blstbgn);
         return true;
 
       case EVENTID(BUTTON_NONE, EVENT_SWING, MODE_ON):
         if (swing_blast_) {
-	  hybrid_font.SB_Effect(EFFECT_BLAST, 0);
+	  SaberBase::DoBlast();
         }
         return true;
 
-#ifndef DISABLE_COLOR_CHANGE
+
       case EVENTID(BUTTON_POWER, EVENT_CLICK_SHORT, MODE_ON | BUTTON_AUX):
-        ToggleColorChangeMode();
-        return true;
+        if (fusor.angle1() >  M_PI / 3) {
+          SaberBase::DoEffect(EFFECT_POWERSAVE, 0);
+        } else {
+#ifndef DISABLE_COLOR_CHANGE
+          ToggleColorChangeMode();
 #endif
+#ifdef DISABLE_COLOR_CHANGE
+	  SaberBase::DoEffect(EFFECT_POWERSAVE, 0);
+#endif
+	}
+        return true;
+
 
         // Lockup
       case EVENTID(BUTTON_NONE, EVENT_CLASH, MODE_ON | BUTTON_POWER):
       case EVENTID(BUTTON_NONE, EVENT_CLASH, MODE_ON | BUTTON_AUX):
         if (!SaberBase::Lockup()) {
-          if (pointing_down_) {
-            SaberBase::SetLockup(SaberBase::LOCKUP_DRAG);
-          } else {
-            SaberBase::SetLockup(SaberBase::LOCKUP_NORMAL);
-          }
+          SaberBase::SetLockup(SaberBase::LOCKUP_NORMAL);
           swing_blast_ = false;
           SaberBase::DoBeginLockup();
           return true;
@@ -283,7 +356,11 @@ SaberFett263Buttons() : PropBase() {}
       case EVENTID(BUTTON_NONE, EVENT_STAB, MODE_ON | BUTTON_POWER):
       case EVENTID(BUTTON_NONE, EVENT_STAB, MODE_ON | BUTTON_AUX):
         if (!SaberBase::Lockup()) {
-          SaberBase::SetLockup(SaberBase::LOCKUP_MELT);
+          if (fusor.angle1() < - M_PI / 4) {
+            SaberBase::SetLockup(SaberBase::LOCKUP_DRAG);
+          } else {
+            SaberBase::SetLockup(SaberBase::LOCKUP_MELT);
+          }
           swing_blast_ = false;
           SaberBase::DoBeginLockup();
           return true;
@@ -302,43 +379,44 @@ SaberFett263Buttons() : PropBase() {}
       case EVENTID(BUTTON_AUX, EVENT_CLICK_LONG, MODE_OFF):
         if (mode_volume_) {
           mode_volume_ = false;
-          hybrid_font.SB_Change(EXIT_COLOR_CHANGE);
+          hybrid_font.PlayCommon(&SFX_vmend);
           // beeper.Beep(0.5, 3000);
           STDOUT.println("Exit Volume Menu");
         } else {
           mode_volume_ = true;
-          hybrid_font.SB_Change(ENTER_COLOR_CHANGE);
+          hybrid_font.PlayCommon(&SFX_vmbegin);
           // beeper.Beep(0.5, 3000);
           STDOUT.println("Enter Volume Menu");
         }
         return true;
 
       case EVENTID(BUTTON_NONE, EVENT_CLASH, MODE_OFF | BUTTON_POWER):
-        next_preset();
+        if (fusor.angle1() < - M_PI / 4) {
+          previous_preset();
+        } else {
+          next_preset();
+        }
         return true;
 
-      case EVENTID(BUTTON_POWER, EVENT_CLICK_SHORT, MODE_OFF | BUTTON_AUX):
-        previous_preset();
+      case EVENTID(BUTTON_NONE, EVENT_TWIST, MODE_OFF):
+        last_twist_ = millis();
+        SaberBase::DoEffect(EFFECT_BATTERY, 0);
         return true;
 
-       // Gesture Controls
        // Battle Mode
 
-#ifdef FETT263_BATTLE_MODE
-#define FETT263_SWING_ON
-#define FETT263_TWIST_OFF
+#ifndef FETT263_BATTLE_MODE_ALWAYS_ON
        case EVENTID(BUTTON_NONE, EVENT_SWING, MODE_ON | BUTTON_AUX):
          if (!battle_mode_) {
            battle_mode_ = true;
-           // Force sound plays when entering Battle Mode
-	   hybrid_font.SB_Effect(EFFECT_FORCE, 0);
+           hybrid_font.PlayCommon(&SFX_bmbegin);
          } else {
            battle_mode_ = false;
-           // Exit Color Change sound plays when exiting Battle Mode
-           hybrid_font.SB_Change(EXIT_COLOR_CHANGE);
+           hybrid_font.PlayCommon(&SFX_bmend);;
          }
          return true;
-
+#endif
+		    
       // Auto Lockup Mode
       case EVENTID(BUTTON_NONE, EVENT_CLASH, MODE_ON):
         if (!battle_mode_) return false;
@@ -365,14 +443,22 @@ SaberFett263Buttons() : PropBase() {}
           SaberBase::DoBeginLockup();
         }
         return true;
-#endif
 
+      // Optional Gesture Controls
+		    
 #ifdef FETT263_SWING_ON
       case EVENTID(BUTTON_NONE, EVENT_SWING, MODE_OFF):
         // Due to motion chip startup on boot creating false ignition we delay Swing On at boot for 3000ms
         if (millis() > 3000) {
-          On();
-          battle_mode_ = true;
+#ifndef FETT263_GESTURE_PREON
+        FastOn();
+#endif
+#ifdef FETT263_GESTURE_PREON
+        On();
+#endif
+#ifndef FETT263_STAB_ON_NO_BM
+	battle_mode_ = true;
+#endif
         }
         return true;
 #endif
@@ -384,27 +470,24 @@ SaberFett263Buttons() : PropBase() {}
           Off();
           last_twist_ = millis();
           saber_off_time_ = millis();
+#ifndef FETT263_BATTLE_MODE_ALWAYS_ON
+	  battle_mode_ = false;
+#endif
         }
         return true;
 #endif
-
-      // Optional Gestures use defines to enable
 
 #ifdef FETT263_STAB_ON
       case EVENTID(BUTTON_NONE, EVENT_STAB, MODE_OFF):
-        On();
-        battle_mode_ = true;
-        return true;
+#ifndef FETT263_GESTURE_PREON
+        FastOn();
 #endif
-
-#ifdef FETT263_TWIST_ON
-      case EVENTID(BUTTON_NONE, EVENT_TWIST, MODE_OFF):
-        // Delay twist events to prevent false trigger from over twisting
-        if (millis() - last_twist_ > 3000) {
-          On();
-          last_twist_ = millis();
-          battle_mode_ = true;
-        }
+#ifdef FETT263_GESTURE_PREON
+        On();
+#endif
+#ifndef FETT263_STAB_ON_NO_BM
+        battle_mode_ = true;
+#endif
         return true;
 #endif
 
