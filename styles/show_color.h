eacbbb65d391b8fb90204943dd88050554dbd3a9
profezzorn <hubbe@hubbe.net>
2021-01-26 21:51:11 -0800

    single blade show color style

--- styles/show_color.h
+++ styles/show_color.h
@@ -1,6 +1,8 @@
 #ifndef STYLES_SHOW_COLOR_H
 #define STYLES_SHOW_COLOR_H
 
+#include "../common/preset.h"
+
 class ShowColorStyle : public BladeStyle {
   bool NoOnOff() override { return true; }
   bool Charging() override { return false; }
@@ -15,6 +17,12 @@ class ShowColorStyle : public BladeStyle {
   }
 
   void SetColor(Color16 color) { color_ = color; }
+private:
+  Color16 color_;
+};
+
+class ShowColorAllBlades : public ShowColorStyle {
+public:
   void Start() {
 #define SHOW_COLOR_STYLE_START(N)				\
     style##N##_ = current_config->blade##N->UnSetStyle();	\
@@ -28,9 +36,36 @@ class ShowColorStyle : public BladeStyle {
     ONCEPERBLADE(SHOW_COLOR_STYLE_STOP);
   }
 private:
-  Color16 color_;
 #define SHOW_COLOR_STYLE_DEFINE(N) BladeStyle* style##N##_;
   ONCEPERBLADE(SHOW_COLOR_STYLE_DEFINE)
+};
+
+class ShowColorSingleBlade : public ShowColorStyle {
+public:
+  void Start(int blade) {
+#define SHOW_COLOR_STYLE_START2(N)				\
+    case N:							\
+      style_ = current_config->blade##N->UnSetStyle();	        \
+      current_config->blade##N->SetStyle(this);			\
+      break;
+
+    switch (blade) {
+      ONCEPERBLADE(SHOW_COLOR_STYLE_START2)
+    }
+  }
+  void Stop(int blade) {
+#define SHOW_COLOR_STYLE_STOP2(N)			\
+    case N:						\
+      current_config->blade##N->UnSetStyle();		\
+      current_config->blade##N->SetStyle(style_);	\
+      break;
+
+    switch (blade) {
+      ONCEPERBLADE(SHOW_COLOR_STYLE_STOP2)
+    }
+  }
+private:
+  BladeStyle* style_;
 };
 
 #endif  // STYLES_SHOW_COLOR_H
