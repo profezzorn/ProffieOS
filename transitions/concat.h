f222ad7fdef660a0a3952f272041d0665ce15c89
profezzorn <hubbe@hubbe.net>
2021-02-10 19:58:29 -0800

    revert trconcat optimization because it is not compatible with IntArg/RgbArg, use #define OPTIMIZE_TRCONCAT to use the optimized version

--- transitions/concat.h
+++ transitions/concat.h
@@ -13,6 +13,11 @@ template<class ... REST> class TrConcat  {};
 
 template<class A> class TrConcat<A> : public A {};
 
+#ifdef OPTIMIZE_TRCONCAT
+
+// this TRCONCAT can save some RAM, but it's not compatible with
+// IntArg/RgbArg
+
 template<class A, class INTERMEDIATE, class... B>
 class TrConcat<A, INTERMEDIATE, B...> {
 public:
@@ -68,5 +73,44 @@ public:
     }
   }
 };
+
+#else // OPTIMIZE_TRCONCAT
+
+template<class A, class INTERMEDIATE, class... B>
+class TrConcat<A, INTERMEDIATE, B...> {
+public:
+  void begin() { a_.begin(); run_a_ = true; }
+  bool done() { return !run_a_ && b_.done(); }
+  void run(BladeBase* blade) {
+    intermediate_.run(blade);
+    if (run_a_) {
+      a_.run(blade);
+      if (!a_.done()) return;
+      run_a_ = false;
+      b_.begin();
+    }
+    b_.run(blade);
+  }
+  
+private:
+  bool run_a_ = false;
+  A a_;
+  TrConcat<B...> b_;
+  INTERMEDIATE intermediate_;
+public:  
+  template<class X, class Y>
+    auto getColor(const X& a, const Y& b, int led) -> decltype(MixColors(a_.getColor(a, intermediate_.getColor(led), led),
+								 	 b_.getColor(intermediate_.getColor(led), b, led), 1,1)) {
+    if (done()) return b;
+    auto intermediate = intermediate_.getColor(led);
+    if (run_a_) {
+      return a_.getColor(a, intermediate, led);
+    } else {
+      return b_.getColor(intermediate, b, led);
+    }
+  }
+};
+
+#endif
 
 #endif
