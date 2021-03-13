65bb2cd667a01385c18cdbddca7bbeda8cd8b909
fdarosa2663 <fernando_darosa@hotmail.com>
2021-03-13 18:23:30 -0500

    Create sum.h

---
+++ functions/sum.h
@@ -0,0 +1,29 @@
+#ifndef FUNCTIONS_SUM_H
+#define FUNCTIONS_SUM_H
+
+// Usage: SUM<A, B, ...>
+// Adds A + B...
+// A, B: INTEGER
+// return value: INTEGER
+
+template<class ... REST> class Sum {};
+
+template<class X> class Sum<X> : public X {};
+
+template<class A, class... B>
+class Sum<A, B...> {
+public:
+  void run(BladeBase* blade) {
+    a_.run(blade);
+    b_.run(blade);
+  }
+  int getInteger(int led) {
+    return a_.getInteger(led) + b_.getInteger(led);
+  }
+
+private:
+  A a_;
+  Sum<B...> b_;
+};
+
+#endif
