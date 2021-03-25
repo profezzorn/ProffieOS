30cef89f678c31aec6868f4b72be954fd6f1fb8e
fdarosa2663 <fernando_darosa@hotmail.com>
2021-03-24 20:49:50 -0400

    Create center_wipe.h

---
+++ transitions/center_wipe.h
@@ -0,0 +1,88 @@
+#ifndef TRANSITIONS_CENTER_WIPE_H
+#define TRANSITIONS_CENTER_WIPE_H
+
+#include "wave.h"
+#include "../functions/mult.h"
+#include "../functions/sum.h"
+
+// Usage: TrCenterWipeX<POSITION_FUNCTION, MILLIS_FUNCTION>
+// or: TrCenterWipeX<POSITION, MILLIS>
+// POSITION_FUNCTION & MILLIS_FUNCTION: FUNCTION
+// POSITION: Int
+// MILLIS: a number
+// return value: TRANSITION
+// In the beginning entire blade is color A, then color B 
+// starts at the POSTION and extends up and down the blade
+// in the specified number of milliseconds.
+template<class MILLIS, class POSITION = Int<16384>>
+class TrCenterWipeX : public TransitionBaseX<MILLIS> {
+public:
+  void run(BladeBase* blade) {
+    pos_.run(blade);
+    TransitionBaseX<MILLIS>::run(blade);
+    center_ = (pos_.getInteger(0) * blade->num_leds()) >> 7;
+    fade_top_ = this->update(256 * blade->num_leds() - center_);
+    fade_bottom_ = this->update(center_);
+    top_ = clampi32(center_ + fade_top_, center_, 256 * blade->num_leds());
+    bottom_ = clampi32(center_ - fade_bottom_, 0, center_);
+  }
+
+  template<class A, class B>
+  auto getColor(const A& a, const B& b, int led) -> decltype(MixColors(a,b,1,1)) {
+        int mix = (Range(bottom_, top_) & Range(led << 8, (led << 8) + 256)).size();
+    return MixColors(a, b, mix, 8);
+  }
+private:
+  POSITION pos_;
+  uint32_t fade_top_;
+  uint32_t fade_bottom_;
+  uint32_t center_;
+  uint32_t top_;
+  uint32_t bottom_;
+};
+
+template<int MILLIS, int POSITION = 16384> using TrCenterWipe = TrCenterWipeX<Int<MILLIS>, Int<POSITION>>;
+template<class COLOR, class MILLIS, class POSITION = Int<16384>> using TrCenterWipeSparkX = TrJoin<TrCenterWipeX<MILLIS, POSITION>,TrWaveX<COLOR, Sum<MILLIS, MILLIS, MILLIS, MILLIS>, Int<200>, Sum<MILLIS, MILLIS>, POSITION>>;
+template<class COLOR, int MILLIS, int POSITION = 16384> using TrCenterWipeSpark = TrJoin<TrCenterWipeX<Int<MILLIS>, Int<POSITION>>,TrWaveX<COLOR, Sum<Int<MILLIS>, Int<MILLIS>, Int<MILLIS>, Int<MILLIS>>, Int<200>, Sum<Int<MILLIS>, Int<MILLIS>>, Int<POSITION>>>;
+
+// Usage: TrCenterWipeInX<POSITION_FUNCTION, MILLIS_FUNCTION>
+// or: TrCenterWipeInX<POSITION, MILLIS>
+// POSITION_FUNCTION & MILLIS_FUNCTION: FUNCTION
+// POSITION: Int
+// MILLIS: a number
+// return value: TRANSITION
+// In the beginning entire blade is color A, then color B 
+// starts at the ends and moves toward POSITION
+// in the specified number of milliseconds.
+template<class MILLIS, class POSITION = Int<16384>>
+class TrCenterWipeInX : public TransitionBaseX<MILLIS> {
+public:
+  void run(BladeBase* blade) {
+    pos_.run(blade);
+    TransitionBaseX<MILLIS>::run(blade);
+    center_ = (pos_.getInteger(0) * blade->num_leds()) >> 7;
+    fade_top_ = this->update(256 * blade->num_leds() - center_);
+    fade_bottom_ = this->update(center_);
+    top_ = clampi32((256 * blade->num_leds()) - fade_top_, center_, 256 * blade->num_leds());
+    bottom_ = clampi32(0 + fade_bottom_, 0, center_);
+  }
+
+  template<class A, class B>
+  auto getColor(const A& a, const B& b, int led) -> decltype(MixColors(a,b,1,1)) {
+        int mix = (Range(bottom_, top_) & Range(led << 8, (led << 8) + 256)).size();
+    return MixColors(b, a, mix, 8);
+  }
+private:
+  POSITION pos_;
+  uint32_t fade_top_;
+  uint32_t fade_bottom_;
+  uint32_t center_;
+  uint32_t top_;
+  uint32_t bottom_;
+};
+
+template<int MILLIS, int POSITION = 16384> using TrCenterWipeIn = TrCenterWipeInX<Int<MILLIS>, Int<POSITION>>;
+template<class COLOR, class MILLIS, class POSITION = Int<16384>> using TrCenterWipeInSparkX = TrJoin<TrCenterWipeInX<MILLIS, POSITION>,TrJoin<TrWaveX<COLOR, MILLIS, Int<200>, Sum<MILLIS, MILLIS>, Int<0>>,TrWaveX<COLOR, MILLIS, Int<200>, Sum<MILLIS, MILLIS>, Int<32768>>>>;
+template<class COLOR, int MILLIS, int POSITION = 16384> using TrCenterWipeInSpark = TrJoin<TrCenterWipeInX<Int<MILLIS>, Int<POSITION>>,TrJoin<TrWaveX<COLOR, Int<MILLIS>, Int<200>, Sum<Int<MILLIS>, Int<MILLIS>>, Int<0>>,TrWaveX<COLOR, Int<MILLIS>, Int<200>, Sum<Int<MILLIS>, Int<MILLIS>>, Int<32768>>>>;
+
+#endif
