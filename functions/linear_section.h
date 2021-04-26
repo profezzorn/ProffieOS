bb7a81ecafb25136094da0b407045e32ec6d4d12
fdarosa2663 <fernando_darosa@hotmail.com>
2021-04-26 10:32:34 -0400

    Update linear_section.h

--- functions/linear_section.h
+++ functions/linear_section.h
@@ -5,7 +5,7 @@
 
 // Usage: LinearSectionF<POSITION, FRACTION>
 // POSITION: FUNCTION position on the blade, 0-32768
-// FRACTION: FUNCTION how much of the blade to light up, 0 = none, 32768 = all of it
+// FRACTION: FUNCTION how much of the blade to light up, 0 = none
 // return value: FUNCTION
 // creates a "block" of pixels at POSITION taking up FRACTION of blade
 
@@ -16,36 +16,25 @@ public:
   FunctionRunResult run(BladeBase* base) {
     pos_.run(base);
     FunctionRunResult ret = RunFunction(&fraction_, base);
-    
-    num_leds_ = base->num_leds();
+    num_leds = base->num_leds();
     int fraction = fraction_.getInteger(0);
-    if (fraction == 32768) {
-      start_ = 0;
-      end_ = num_leds_ * 32768;
-    } else if (fraction == 0) {
-      start_ = 0;
-      end_ = 0;
-    } else {
-      int pos = pos_.getInteger(0);
-      start_ = clampi32((pos - fraction / 2) * num_leds_, 0, 32768 * num_leds_);
-      end_ = clampi32((pos + fraction / 2) * num_leds_, 0, 32768 * num_leds_);
-    }
-    num_leds_ *= 32768;
+    int pos = pos_.getInteger(0);
+    start = clampi32((pos - fraction / 2) * num_leds, 0, 32768 * num_leds);
+    end = clampi32((pos + fraction / 2) * num_leds, 0, 32768 * num_leds);
+    range_ = Range(start, end);
     return ret;
   }
   int getInteger(int led) {
     Range led_range(led * 32768, led * 32768 + 32768);
-     int black_mix = 0;
-      black_mix = (Range(start_, end_) & led_range).size();
-    return black_mix;
+    return (Range(range_) & led_range).size();
   }
 private:
   POSITION pos_;
   FRACTION fraction_;
-  
-  uint32_t start_;
-  uint32_t end_;
-  uint32_t num_leds_;
+  Range range_;
+  uint32_t start;
+  uint32_t end;
+  uint32_t num_leds;
 };
 
 #endif
