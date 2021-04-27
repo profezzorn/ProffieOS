cf78ea85e4255ee02a1310d2ad21122650347238
fdarosa2663 <fernando_darosa@hotmail.com>
2021-04-27 09:56:53 -0400

    Create TrSparkX

--- transitions/wave.h
+++ transitions/wave.h
@@ -57,4 +57,51 @@ public:
   }
 };
    
+template<class COLOR,
+         class SPARK_SIZE = Int<100>,
+         class SPARK_MS = Int<400>,
+         class SPARK_CENTER = Int<16384>>
+class TrSparkX : public TransitionBaseX<SPARK_MS> {
+public:
+
+  void run(BladeBase *blade) {
+    spark_size_.run(blade);
+    spark_center_.run(blade);
+    spark_ms_.run(blade);
+    color_.run(blade);
+    if (this->restart()) {
+      center_ = spark_center_.getInteger(0);
+      size_ = spark_size_.getInteger(0);
+    }
+    TransitionBaseX<SPARK_MS>::run(blade);
+    mix_ = this->update(32768);
+    num_leds_ = blade->num_leds();
+    offset_ = (millis() - this->start_millis()) * 32768 / spark_ms_.getInteger(0);
+  }
+private:
+  SPARK_CENTER spark_center_;
+  int center_;
+  
+  SPARK_SIZE spark_size_;
+  int size_;
+
+  int mix_;
+  int num_leds_;
+  int offset_;
+  SPARK_MS spark_ms_;
+  COLOR color_;
+public:
+  template<class A, class B>
+  auto getColor(const A& a, const B& b, int led) -> decltype(MixColors(a,color_.getColor(led),1,1)) {
+    int dist = std::abs(center_ - led * 32768 / num_leds_);
+    int N = std::abs(dist - offset_) * size_ >> 15;
+    int mix;
+    if (N <= 32) {
+      mix = blast_hump[N] * mix_ >> 8;
+    } else {
+      mix = 0;
+    }
+    return MixColors(a, color_.getColor(led), mix, 15);
+  }
+};
 #endif
