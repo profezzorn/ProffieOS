e9ac6949f877aafed89d7241e614f33f9c75a082
fdarosa2663 <fernando_darosa@hotmail.com>
2021-05-10 09:44:25 -0400

    Update hold_peak.h

--- functions/hold_peak.h
+++ functions/hold_peak.h
@@ -1,45 +1,43 @@
 #ifndef FUNCTIONS_HOLD_PEAK_H
 #define FUNCTIONS_HOLD_PEAK_H
 
-// Usage: HoldPeakF<F, hold_time_, SPEED>
-// Holds Peak value of F for hold_time_ in millis.
+// Usage: HoldPeakF<F, HOLD_MILLIS, SPEED>
+// Holds Peak value of F for HOLD_MILLIS.
 // then transitions down over SPEED to current F
-// F, SPEED: FUNCTION
-// hold_time_ MILLIS
+// F, HOLD_MILLIS and SPEED: FUNCTION
 // return value: FUNCTION, same for all LEDs
 
 class BladeBase;
 
-template<class F, int hold_time_, class SPEED>
-
+template<class F, class HOLD_MILLIS, class SPEED>
 class HoldPeakF {
 public:
   void run(BladeBase* blade) {
     f_.run(blade);
-    current_ = f_.getInteger(0);
+    current = f_.getInteger(0);
+    hold_time_millis.run(blade);
+    hold_millis = hold_time_millis.getInteger(0);
     speed_.run(blade);
     uint32_t now = micros();
-    uint64_t delta = now - last_micros_;
-    last_micros_ = now;
-    if (current_ > peak_ && millis() > 2000) { // the millis is for SwingSpeed since boot causes spike
-      peak_ = current_;
-      hold_millis_ = millis();
-      value_ = peak_;
+    uint64_t delta = now - last_micros;
+    last_micros = now;
+    if (current > value_) {
+      value_ = current;
+      last_peak = millis();
     }
-    if (peak_ > current_ && millis() - hold_millis_ > hold_time_) {
+    if (value_ > current && millis() - last_peak > hold_millis) {
       if (delta > 1000000) delta = 1;
       delta *= speed_.getInteger(0);
       delta /= 1000000;
       int target = f_.getInteger(0);
-      if (delta > abs(peak_ - target)) {
-        peak_ = target;
-      } else if (peak_ < target) {
-        peak_ += delta;
+      if (delta > abs(value_ - target)) {
+        value_ = target;
+      } else if (value_ < target) {
+        value_ += delta;
       } else {
-        peak_ -= delta;
+        value_ -= delta;
       }
     }
-    value_ = peak_;
   }
 
   int getInteger(int led) {
@@ -48,12 +46,13 @@ public:
 
 private:
   F f_;
+  HOLD_MILLIS hold_time_millis;
   SPEED speed_;
-  int value_;
-  int peak_;
-  int current_;
-  uint32_t last_micros_ = 0;
-  uint32_t hold_millis_ = millis();
+  int value_ = 0;
+  int current;
+  uint32_t last_micros = 0;
+  uint32_t hold_millis;
+  uin32_t last_peak = millis();
 };
 
 #endif
