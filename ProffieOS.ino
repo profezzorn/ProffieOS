beafff01d3af9b9dddacb679565fc6d536849e2c
profezzorn <hubbe@hubbe.net>
2021-05-23 01:00:29 -0700

    revert common

--- ProffieOS.ino
+++ ProffieOS.ino
@@ -991,9 +991,6 @@ class Commands : public CommandParser {
       STDOUT.print("Global loops / second: ");
       global_loop_counter.Print();
       STDOUT.println("");
-      STDOUT.print("High frequency loops / second: ");
-      hf_loop_counter.Print();
-      STDOUT.println("");
       SaberBase::DoTop(total_cycles);
       Looper::LoopTop(total_cycles);
       DumpProfileLocations(total_cycles);
