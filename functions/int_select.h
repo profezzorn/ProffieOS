1bceac9851849b29da63b3445b184754b9569b27
fdarosa2663 <fernando_darosa@hotmail.com>
2021-06-16 15:58:57 -0400

    Add IntSelect<>

---
+++ functions/int_select.h
@@ -0,0 +1,36 @@
+#ifndef FUNCTIONS_INT_SELECT_H
+#define FUNCTIONS_INT_SELECT_H
+
+// Usage: IntSelect<SELECTION, N>
+// SELECTION: FUNCTION
+// Returns SELECTION of N
+// N: numbers
+// return value: INTEGER
+
+template<class F, int... N>
+class IntSelect {
+public:
+  FunctionRunResult run(BladeBase* blade) {
+    FunctionRunResult frr = RunFunction(&f_, blade);
+    int f = f_.getInteger(0);
+    while(f < 0) f += sizeof...(N) << 8;
+    f = f % sizeof...(N);
+    const static int values[] = { N ... };
+    value_ = values[f];
+    if (frr != FunctionRunResult::UNKNOWN) {
+      switch (value_) {
+        case 0: return FunctionRunResult::ZERO_UNTIL_IGNITION;
+        case 32768: return FunctionRunResult::ONE_UNTIL_IGNITION;
+        default: break;
+      }
+    }
+    return FunctionRunResult::UNKNOWN;
+  }
+  int getInteger(int led) { return value_; }
+
+private:
+	F f_;
+  int value_;
+};
+
+#endif
