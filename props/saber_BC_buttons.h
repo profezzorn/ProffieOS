c5ed135926a54bf19f3401d2a6a4b27e71e74e5c
NoSloppy <53964195+NoSloppy@users.noreply.github.com>
2021-08-26 01:19:53 -0400

    consolidated if condition

--- props/saber_BC_buttons.h
+++ props/saber_BC_buttons.h
@@ -850,12 +850,10 @@ public:
     case EVENTID(BUTTON_NONE, EVENT_STAB, MODE_ON):
       clash_impact_millis_ = millis();
       swing_blast_ = false;
-      if (!SaberBase::Lockup()) {
-        if (!swinging_) {
+      if (!SaberBase::Lockup() && !swinging_) {
           SaberBase::SetLockup(SaberBase::LOCKUP_MELT);
           auto_melt_on_ = true;
           SaberBase::DoBeginLockup();
-        }
       }
       return true;
 
