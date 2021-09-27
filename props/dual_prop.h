573e1d28c86c50125dd3f68377ab84996903463d
NoSloppy <53964195+NoSloppy@users.noreply.github.com>
2021-09-26 20:49:46 -0400

    Update dual_prop.h.  Some Additional "how-to" comments. (#253)

--- props/dual_prop.h
+++ props/dual_prop.h
@@ -1,14 +1,30 @@
 #ifndef PROPS_DUAL_PROP_H
 #define PROPS_DUAL_PROP_H
 
-// How to use:
-// #ifdef CONFIG_PROP
-// #include "../props/dual_prop.h"
-// #include "../props/saber_sa22c_buttons.h"
-// #include "../props/blaster.h"
-// #undef PROP_TYPE
-// #define PROP_TYPE DualProp<SaberSA22CButtons, Blaster>
-// #endif
+/*
+dual_prop.h allows for 2 discrete prop files to be used, 
+alternating on a latched switch (Blade Detect).
+This is useful when you want a saber to 
+toggle to a blaster for example, 
+and you want the buttons to take on different behaviors.
+
+How to use:
+#ifdef CONFIG_PROP
+#include "../props/dual_prop.h"
+#include "../props/saber_sa22c_buttons.h"
+#include "../props/blaster.h"
+#undef PROP_TYPE
+#define PROP_TYPE DualProp<SaberSA22CButtons, Blaster>
+#endif
+** Note the prop file SaberSA22CButtons here would change
+to the file name of the prop you choose.
+
+Then setup your config file for 2 states:
+Bladein preset bank, and a BladeOut preset bank.
+Give the BladeConfig 2 sets of descriptions, and
+#define BLADE_DETECT_PIN properly.
+*/
+
 
 #ifndef DUAL_PROP_CONDITION
 #define DUAL_PROP_CONDITION blade_detected_
