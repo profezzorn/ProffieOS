a954afb87b72af29514eefc2e9a0c038889fb8ff
NoSloppy <53964195+NoSloppy@users.noreply.github.com>
2021-09-28 05:10:01 -0700

    Update usage comments

--- functions/wavlen.h
+++ functions/wavlen.h
@@ -1,13 +1,17 @@
 #ifndef FUNCTIONS_WAVELEN_H
 #define FUNCTIONS_WAVELEN_H
 
-// Usage: WavLen<EFFECT>
+// Usage: WavLen<>
+// Or: WavLen<EFFECT>
 // EFFECT: effect type
 // return value: INTEGER
 //
-// Example: TrFadeX<WavLen<EFFECT_RETRACTION>>
 // WavLen (length of wav file) takes the duration of a wav file sound
-// and can be used to replace time integer arguments in a blade style. 
+// and can be used to replace time integer arguments in a blade style.
+// Example: TrFadeX<WavLen<EFFECT_RETRACTION>>
+// When used as WavLen<> inside a TransitionEffectL whose EFFECT is already specified, 
+// then it will automatically use the right effect.
+// Example: TransitionEffectL<TrConcat<TrWipex<WavLen<>>,White,TrWipeX<WavLen<>>>,EFFECT_BLAST>
 
 template<EffectType T = EFFECT_NONE>
 class WavLen {
