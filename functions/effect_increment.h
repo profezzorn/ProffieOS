f8633ea1bd0d6d10d4a4d9d62d17ae10b6a3215a
fdarosa2663 <fernando_darosa@hotmail.com>
2021-10-11 15:27:13 -0400

    Create effect_increment.h

---
+++ functions/effect_increment.h
@@ -0,0 +1,30 @@
+#ifndef FUNCTIONS_EFFECT_INCREMENT_H
+#define FUNCTIONS_EFFECT_INCREMENT_H
+
+// Usage: EffectIncrement<EFFECT, MAX, I>
+// Increases by value I (up to MAX) each time EFFECT is triggered
+// if greater than MAX returns 0
+// I, MAX = numbers
+// return value: INTEGER
+
+template<BladeEffectType EFFECT, class MAX = Int<32768>, class I = Int<1>>
+class EffectIncrement {
+public:
+  void run(BladeBase* blade) {
+    int i = i_.getInteger(0);
+    int max = max_.getInteger(0);
+    if (effect_.Detect(blade)) {
+      value_ += i;
+    }
+    value_ %= max;
+  }
+  int getInteger(int led) { return value_; }
+
+private:
+  OneshotEffectDetector<EFFECT> effect_;
+  I i_;
+  MAX max_;
+  int value_;
+};
+
+#endif
