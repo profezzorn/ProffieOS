ddb02c368fafa6eede01cdfe20983a8f45aa372e
fdarosa2663 <fernando_darosa@hotmail.com>
2021-10-12 08:05:51 -0400

    updates per comments

--- functions/effect_increment.h
+++ functions/effect_increment.h
@@ -11,12 +11,13 @@ template<BladeEffectType EFFECT, class MAX = Int<32768>, class I = Int<1>>
 class EffectIncrement {
 public:
   void run(BladeBase* blade) {
+    i_.run(blade);
+    max_.run(blade);
     int i = i_.getInteger(0);
-    int max = max_.getInteger(0);
     if (effect_.Detect(blade)) {
       value_ += i;
     }
-    value_ %= max;
+    value_ %= max_.getInteger(0);
   }
   int getInteger(int led) { return value_; }
 
