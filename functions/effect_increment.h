1d87db78916f9972f55ca4e99977501a0bd3fd9f
fdarosa2663 <fernando_darosa@hotmail.com>
2021-10-13 13:57:23 -0400

    renamed EffectIncrementF<>

--- functions/effect_increment.h
+++ functions/effect_increment.h
@@ -1,22 +1,20 @@
-#ifndef FUNCTIONS_EFFECT_INCREMENT_H
-#define FUNCTIONS_EFFECT_INCREMENT_H
+#ifndef FUNCTIONS_EFFECT_INCREMENT_F_H
+#define FUNCTIONS_EFFECT_INCREMENT_F_H
 
-// Usage: EffectIncrement<EFFECT, MAX, I>
+// Usage: EffectIncrementF<EFFECT, MAX, I>
 // Increases by value I (up to MAX) each time EFFECT is triggered
 // if greater than MAX returns 0
 // I, MAX = numbers
 // return value: INTEGER
 
 template<BladeEffectType EFFECT, class MAX = Int<32768>, class I = Int<1>>
-class EffectIncrement {
+class EffectIncrementF {
 public:
   void run(BladeBase* blade) {
     i_.run(blade);
     max_.run(blade);
     int i = i_.getInteger(0);
-    if (effect_.Detect(blade)) {
-      value_ += i;
-    }
+    if (effect_.Detect(blade)) value_ += i;
     value_ %= max_.getInteger(0);
   }
   int getInteger(int led) { return value_; }
