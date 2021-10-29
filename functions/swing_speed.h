4d8fc387a4ecd700564e99136d69fd850e16cc0b
fdarosa2663 <fernando_darosa@hotmail.com>
2021-10-29 15:08:28 -0400

    add SwingAcceleration<>

--- functions/swing_speed.h
+++ functions/swing_speed.h
@@ -26,4 +26,27 @@ using SwingSpeedX = SingleValueAdapter<SwingSpeedSVF<MAX>>;
 template<int MAX>
 using SwingSpeed = SwingSpeedX<Int<MAX>>;
 
+// Usage: SwingAcceleration<MAX>
+// Returns 0-32768 based on swing acceleration
+// MAX defaults to 100
+// returned value: INTEGER
+template<class MAX = Int<100>>
+class SwingAccelerationSVF {
+ public:
+  void run(BladeBase* blade) { max_.run(blade); }
+ int calculate(BladeBase* blade) {
+   max_.run(blade);
+   float v = fusor.rss() / max_.calculate(blade);
+   return clampi32(v * 32768, 0, 32768);
+  }
+private:
+  PONUA SVFWrapper<MAX> max_;
+};
+
+template<class MAX = Int<100>>
+using SwingAccelerationX = SingleValueAdapter<SwingAccelerationSVF<MAX>>;
+
+template<int MAX = 100>
+using SwingAcceleration = SwingAccelerationX<Int<MAX>>;
+
 #endif
