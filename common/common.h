1a1b1762af3334c841680a91d5df0fa226e12ebd
profezzorn <hubbe@hubbe.net>
2021-11-08 20:12:04 -0800

    fix compilation on teensy

--- common/common.h
+++ common/common.h
@@ -1,6 +1,9 @@
 #ifndef COMMON_COMMON_H
 #define COMMON_COMMON_H
 
+// For static_assert, and other things.
+#include <type_traits>
+
 // No unique address saves some RAM.
 // but it's not available in older compilers.
 #if __has_cpp_attribute(no_unique_address)
