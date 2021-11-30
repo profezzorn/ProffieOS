b18cb5725ad3b1d86354772a007d0eff0e7b9914
NoSloppy <53964195+NoSloppy@users.noreply.github.com>
2021-11-29 19:19:54 -0500

    "Too long' message only if Teensyboard.

--- sound/effect.h
+++ sound/effect.h
@@ -423,14 +423,24 @@ class Effect {
       }
 
 #ifdef ENABLE_AUDIO
+#if VERSION_MAJOR < 4
       else if (strlen(dir) > 8) { // TODO: Check individual path segments
+        STDOUT << "\nFont directory not found\n";
+        STDOUT << " and name exceeds 8 characters: " << dir << "\n";
         talkie.Say(talkie_font_directory_15, 15);
         talkie.Say(talkie_too_long_15, 15);
       } else if (strlen(dir)) {
-	STDOUT.println(" Font directory not found.");
+        STDOUT << "\nFont directory not found: " << dir << "\n";
+        talkie.Say(talkie_font_directory_15, 15);
+        talkie.Say(talkie_not_found_15, 15);
+      } 
+#else
+      else if (strlen(dir)) {
+        STDOUT << "\nFont directory not found: " << dir << "\n";
         talkie.Say(talkie_font_directory_15, 15);
         talkie.Say(talkie_not_found_15, 15);
       }
+#endif   // VERSION_MAJOR < 4
 #endif   // ENABLE_AUDIO
 #endif   // ENABLE_SD
       STDOUT.println(" done");
