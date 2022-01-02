a17f78bec777fc57f094091e0e4c014a7979731b
Fredrik Hubinette <hubbe@hubbe.net>
2022-01-01 17:18:17 -0800

    remove upper limit on TimeSinceEffect and make it an SVF

--- functions/time_since_effect.h
+++ functions/time_since_effect.h
@@ -11,10 +11,13 @@
 // When used as TimeSinceEffect<> inside a TransitionEffectL whose EFFECT is already specified, 
 // then it will automatically use the right effect.
 
+#include "svf.h"
+
 template<EffectType T = EFFECT_NONE>
-class TimeSinceEffect {
+class TimeSinceEffectSVF {
 public:
-  void run(BladeBase* blade) {
+  void run(BladeBase* blade) {}
+  int calculate(BladeBase* blade) {
     BladeEffect* effect;
     if (T == EFFECT_NONE) {
       effect = last_detected_blade_effect;
@@ -22,12 +25,22 @@ public:
       OneshotEffectDetector<T> detector;
       effect = detector.Detect(blade);
     }
-    if (effect) value_ = (micros() - effect->start_micros) / 1000; // ms
+    if (effect) {
+      last_occurance_us_ = effect->start_micros;
+    }
+    uint32_t now = micros();
+    uint32_t ret = now - last_occurance_us_;
+    if (ret > 1000000000) {
+      // Prevent weird wraparounds.
+      last_occurance_us_ = now - 1000000000UL;
+    } 
+    return ret / 1000; // return ms
   }
-  int getInteger() { return value_; }
-  int getInteger(int led) { return value_; }
 private:
-  int value_ = 0;
+  uint32_t last_occurance_us_ = 0;
 };
+
+template<EffectType T = EFFECT_NONE>
+using TimeSinceEffect = SingleValueAdapter<TimeSinceEffectSVF<T>>;
 
 #endif
