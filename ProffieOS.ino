f4663869bcbd712df2d3d8015ffb19121f872907
NoSloppy <53964195+NoSloppy@users.noreply.github.com>
2022-01-05 22:48:54 -0500

    copyright info standalone comment

--- ProffieOS.ino
+++ ProffieOS.ino
@@ -16,14 +16,14 @@
 
  You should have received a copy of the GNU General Public License
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-
--------------------------------------------------------------------
-You can have multiple configuration files, and specify which one
-to use here by removing the comment slashes at the beginning.
-**NOTE** Only ONE line should be left uncommented at a time!
-Add the slashes to any that you are not uploading.
 */
+
+/*-------------------------------------------------------------------
+-  You can have multiple configuration files, and specify which one -
+-  to use here by removing the comment slashes at the beginning.    -
+-  **NOTE** Only ONE line should be left uncommented at a time!     -
+-  Add the slashes to any that you are not uploading.               -
+*/-------------------------------------------------------------------
 
 // #define CONFIG_FILE "config/YOUR_CONFIG_FILE_NAME_HERE.h"
 
