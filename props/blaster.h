dfbf5a3fd7432d1c9c6e540217fc7d456e090628
NoSloppy <53964195+NoSloppy@users.noreply.github.com>
2022-02-05 03:06:05 -0500

    working now.

--- props/blaster.h
+++ props/blaster.h
@@ -278,7 +278,6 @@ public:
     PollNextAction();
     if (auto_firing_) {
       if (&auto_player_) {
-        STDOUT << "*************************** &auto_player_ works, but Why is it half speed!!???\n";
         STDOUT << millis() - auto_time_;
         if (millis() - auto_time_ > 1000 * auto_player_->length()) {
           shots_fired_++;
