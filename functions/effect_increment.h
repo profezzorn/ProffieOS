92a47595732154e71af9d6cc2be2ab96a73ed4c2
NoSloppy <53964195+NoSloppy@users.noreply.github.com>
2022-02-08 22:56:48 -0500

    usage clarification

--- functions/effect_increment.h
+++ functions/effect_increment.h
@@ -62,7 +62,8 @@ class SingleValueAdapter<IncrementWithResetSVF<PULSE, RESET_PULSE, MAX, I>> : pu
 
 // Usage: EffectIncrementF<EFFECT, MAX, I>
 // Increases by value I (up to MAX) each time EFFECT is triggered
-// if greater than MAX returns 0
+// If current value + I - MAX, it returns 0.
+// If adding I exceeds MAX, the function returns 0 + any remainder in excesss of MAX 
 // I, MAX = numbers
 // return value: INTEGER
 template<BladeEffectType EFFECT, class MAX = Int<32768>, class I = Int<1>>
