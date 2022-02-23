23cb46969620e7b25684263cb86b3c0e796090c0
fdarosa2663 <fernando_darosa@hotmail.com>
2022-02-23 14:18:58 -0500

    Add control comment for "revert" in Color Change mode (#408)

--- props/saber_fett263_buttons.h
+++ props/saber_fett263_buttons.h
@@ -97,6 +97,7 @@ Standard Controls While Blade is ON
         *if COLOR_CHANGE_DIRECT is defined then each click will change color instead of turn
       Otherwise ColorWheel is used per style set up.
     Click PWR to save
+    Click AUX to revert
     NEW! Color Zoom* = Hold PWR, Release to Save
       *For Color List or ColorWheel you can Hold PWR down to zoom in color for easier selection
        Release PWR to save
