05a7094c99db371c697baf42819d72eb3604e912
fdarosa2663 <fernando_darosa@hotmail.com>
2022-05-03 18:18:58 -0400

    AUDIO_CLASH_SUPPRESSION_LEVEL

--- props/prop_base.h
+++ props/prop_base.h
@@ -17,9 +17,9 @@
 #undef SAVE_VOLUME
 #endif
 
-#ifndef AUDIO_CLASH_OFFSET
+#ifndef AUDIO_CLASH_SUPPRESSION_LEVEL
 // Account for Audio Volume in Clash Detection (range 1 ~ 50)
-#define AUDIO_CLASH_OFFSET 1
+#define AUDIO_CLASH_SUPPRESSION_LEVEL 1
 #endif
 
 class SaveGlobalStateFile : public ConfigFile {
@@ -728,7 +728,7 @@ public:
     }
     // If we're spinning the saber or if loud sounds are playing, 
     // require a stronger acceleration to activate the clash.
-    if (v > (CLASH_THRESHOLD_G + fusor.gyro().len() / 200.0) + (dynamic_mixer.audio_volume() * (AUDIO_CLASH_OFFSET * 0.000001))) {    
+    if (v > (CLASH_THRESHOLD_G + fusor.gyro().len() / 200.0) + (dynamic_mixer.audio_volume() * (AUDIO_CLASH_SUPPRESSION_LEVEL * 0.000001))) {    
       if ( (accel_ - fusor.down()).len2() > (accel - fusor.down()).len2() ) {
         diff = -diff;
       }
