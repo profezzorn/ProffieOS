3d01be9420d5fbb2f055bcbdb7b1fe616eebabeb
fdarosa2663 <fernando_darosa@hotmail.com>
2022-09-11 19:58:16 -0400

    Add OS7 EFFECTS

--- common/saber_base.h
+++ common/saber_base.h
@@ -38,6 +38,11 @@ extern SaberBase* saberbases;
     DEFINE_EFFECT(POWERSAVE)                    \
     DEFINE_EFFECT(BATTERY_LEVEL)                \
     DEFINE_EFFECT(FAST_ON)                      \
+    DEFINE_EFFECT(SECONDARY_IGNITION)		\
+    DEFINE_EFFECT(SECONDARY_RETRACTION)		\
+    DEFINE_EFFECT(QUOTE)			\
+    DEFINE_EFFECT(ALT_SOUND)			\
+    DEFINE_EFFECT(TRANSITION_SOUND)		\
     /* Blaster effects */                       \
     DEFINE_EFFECT(STUN)				\
     DEFINE_EFFECT(FIRE)				\
@@ -52,6 +57,17 @@ extern SaberBase* saberbases;
     DEFINE_EFFECT(UNJAM)			\
     DEFINE_EFFECT(PLI_ON)			\
     DEFINE_EFFECT(PLI_OFF)                      \
+    /* Mini game effects */                     \
+    DEFINE_EFFECT(GAME_START)                   \
+    DEFINE_EFFECT(GAME_ACTION1)                 \
+    DEFINE_EFFECT(GAME_ACTION2)                 \
+    DEFINE_EFFECT(GAME_CHOICE)                  \
+    DEFINE_EFFECT(GAME_RESPONSE1)               \
+    DEFINE_EFFECT(GAME_RESPONSE2)               \
+    DEFINE_EFFECT(GAME_RESULT1)                 \
+    DEFINE_EFFECT(GAME_RESULT2)                 \
+    DEFINE_EFFECT(GAME_WIN)                     \
+    DEFINE_EFFECT(GAME_LOSE)                    \
     /* user-definable effects */                \
     DEFINE_EFFECT(USER1)			\
     DEFINE_EFFECT(USER2)			\
