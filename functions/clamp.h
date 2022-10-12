af4daf07c25a7e2718b11020a726340387f32891
fdarosa2663 <fernando_darosa@hotmail.com>
2022-10-12 14:58:37 -0400

    Rename ClampF, Add nonSVF

--- functions/clamp.h
+++ functions/clamp.h
@@ -4,13 +4,33 @@
 #include "int.h"
 #include "svf.h"
 
-// Usage: Clamp<F, MIN, MAX>
+// Usage: ClampF<F, MIN, MAX>
 // Clamps value between MIN and MAX
 // F, A, B: INTEGER
 // return value: INTEGER
 
 template<class F, class MIN = Int<0>, class MAX = Int<32768>>
-class ClampXSVF {
+class ClampFBase {
+public:
+  void run(BladeBase* blade) {
+    f_.run(blade);
+    min_.run(blade);
+    max_.run(blade);
+  }
+  int getInteger(int led) {
+    int min = min_.getInteger(led);
+    int max = max_.getInteger(led);
+    return clampi32(f_.getInteger(led), min, max);
+  }
+
+private:
+  PONUA F f_;
+  PONUA MIN min_;
+  PONUA MAX max_;
+};
+
+template<class F, class MIN = Int<0>, class MAX = Int<32768>>
+class ClampFXSVF {
 public:
   void run(BladeBase* blade) {
     f_.run(blade);
@@ -29,9 +49,9 @@ private:
 };
 
 template<class F, class MIN = Int<0>, class MAX = Int<32768>>
-using ClampX = SingleValueAdapter<ClampXSVF<F, MIN, MAX>>;
+using ClampFX = SingleValueAdapter<ClampFXSVF<F, MIN, MAX>>;
 
 template<class F, int MIN = 0, int MAX = 32768>
-using Clamp = ClampX<F, Int<MIN>, Int<MAX>>;
+using ClampF = ClampFX<F, Int<MIN>, Int<MAX>>;
 
 #endif
