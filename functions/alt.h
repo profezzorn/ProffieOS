c01b26b73fde8649522f4e0fd208ed1d71376cd8
fdarosa2663 <fernando_darosa@hotmail.com>
2022-10-19 16:44:14 -0400

    Update alt.h

--- functions/alt.h
+++ functions/alt.h
@@ -1,14 +1,18 @@
 #ifndef FUNCTIONS_ALTF_H
 #define FUNCTIONS_ALTF_H
 
+#include "svf.h"
+
 // Usage: AltF
 // return value: INTEGER
 // Returns current_alternative for use in ColorSelect<>, TrSelect<> or IntSelect<>
 
-class AltF {
+class AltSVF {
  public:
     void run(BladeBase* blade) {}
     int getInteger(int led) { return current_alternative; }
 };
+
+using AltF = SingleValueAdapter<AltSVF>;
 
 #endif
