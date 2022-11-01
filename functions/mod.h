30f169072952d4e5d2cebdc0c97ede52e7687872
fdarosa2663 <fernando_darosa@hotmail.com>
2022-11-01 08:11:29 -0400

    change to int

--- functions/mod.h
+++ functions/mod.h
@@ -13,7 +13,7 @@ public:
     max_.run(blade);
   }
   int getInteger(int led) {
-    int32_t max = max_.getInteger(led);
+    int max = max_.getInteger(led);
     if (max == 0) {
       return 0;
     } else {
@@ -40,7 +40,7 @@ public:
     max_.run(blade);
   }
   int calculate(BladeBase* blade) { 
-    int32_t max = max_.calculate(blade);
+    int max = max_.calculate(blade);
     if (max == 0) {
       return 0;
     } else {
