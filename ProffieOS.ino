da32971ae605f99791f440048ea259b059ee07b0
profezzorn <hubbe@hubbe.net>
2023-01-18 02:38:56 -0600

    add TrLoop & TrLoopUntil

--- ProffieOS.ino
+++ ProffieOS.ino
@@ -536,6 +536,7 @@ struct is_same_type<T, T> { static const bool value = true; };
 #include "transitions/sequence.h"
 #include "transitions/blink.h"
 #include "transitions/doeffect.h"
+#include "transitions/loop.h"
 
 #include "styles/legacy_styles.h"
 //responsive styles
