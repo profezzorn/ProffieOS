ee105b6155c2dc4cf2592659cab0b2a03a87f1b2
fdarosa2663 <fernando_darosa@hotmail.com>
2023-01-20 08:12:02 -0500

    DISABLE_TALKIE

--- ProffieOS.ino
+++ ProffieOS.ino
@@ -1478,8 +1478,12 @@ void setup() {
   SaberBase::DoBoot();
 #if defined(ENABLE_SD) && defined(ENABLE_AUDIO)
   if (!sd_card_found) {
+#if !defined(DISABLE_TALKIE)
     talkie.Say(talkie_sd_card_15, 15);
     talkie.Say(talkie_not_found_15, 15);
+#else
+    beeper.Beep(1.0, 1000);
+#endif
   }
 #endif // ENABLE_AUDIO && ENABLE_SD
 }
