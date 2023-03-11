f287a8de2107cc533dbebbb7a74c04660a14ab46
profezzorn <hubbe@hubbe.net>
2023-03-11 02:15:24 -0800

    WHEN_ON -> WHEN_OFF

--- transitions/doeffect.h
+++ transitions/doeffect.h
@@ -4,18 +4,22 @@
 #include "base.h"
 #include "../sound/hybrid_font.h"
 
-// Usage: TrDoEffectX<TRANSITION, EFFECT, WAVNUM, LOCATION>
-// or: TrDoEffect<TRANSITION, EFFECT, WAVNUM, LOCATION>
+// Usage: TrDoEffectX<TRANSITION, EFFECT, WAVNUM, LOCATION_LOCASS, WHEN_OFF>
+// or: TrDoEffect<TRANSITION, EFFECT, WAVNUM, LOCATION, WHEN_OFF>
 // TRANSITION: TRANSITION
 // EFFECT: effect type
 // WAVNUM, LOCATION: a number
+// LOCATION_CLASS: INTEGER
+// WHEN_OFF: true or false
 // return value: TRANSITION
 // Runs the specified TRANSITION and triggers EFFECT
 // Can specify WAV file to use for EFFECT with WAVNUM
 // NOTE: 0 is first wav file, -1 is random wav
-// LOCATION = -1 is random 
+// LOCATION = -1 is random
+// If WHEN_OFF is false, TrDoEffect will do nothing if the blade is off
+// and will also force the transition to stop when off.
 
-template<class TRANSITION, BladeEffectType EFFECT, class WAVNUM = Int<-1>, class LOCATION = Int<-1>, bool WHEN_ON=false>
+template<class TRANSITION, BladeEffectType EFFECT, class WAVNUM = Int<-1>, class LOCATION = Int<-1>, bool WHEN_OFF=false>
 class TrDoEffectX : public TRANSITION {
 public:
   void begin() {
@@ -30,8 +34,10 @@ public:
     float location = location_.calculate(blade);
     if (location == -1) location = random(32768) / 32768.0;
     if (begin_) {
-      int wavnum = wavnum_.calculate(blade);
-      SaberBase::DoEffect(EFFECT, location, wavnum);
+      if (blade->is_on()) {
+        int wavnum = wavnum_.calculate(blade);
+        SaberBase::DoEffect(EFFECT, location, wavnum);
+      }
       begin_ = false;
     }
     if (!done_) {
@@ -57,7 +63,7 @@ public:
 };
 
 template<class TRANSITION, BladeEffectType EFFECT, class WAVNUM, class LOCATION>
-class TrDoEffectX<TRANSITION, EFFECT, WAVNUM, LOCATION, false>  : public TRANSITION {
+class TrDoEffectX<TRANSITION, EFFECT, WAVNUM, LOCATION, true>  : public TRANSITION {
 public:
   void begin() {
     TRANSITION::begin();
@@ -82,6 +88,6 @@ public:
   bool begin_;
 };
 
-template<class TRANSITION, BladeEffectType EFFECT, int WAVNUM = -1, int LOCATION = -1, bool WHEN_ON = false> using TrDoEffect = TrDoEffectX<TRANSITION, EFFECT, Int<WAVNUM>, Int<LOCATION>, WHEN_ON>;
+template<class TRANSITION, BladeEffectType EFFECT, int WAVNUM = -1, int LOCATION = -1, bool WHEN_OFF = false> using TrDoEffect = TrDoEffectX<TRANSITION, EFFECT, Int<WAVNUM>, Int<LOCATION>, WHEN_OFF>;
 
 #endif
