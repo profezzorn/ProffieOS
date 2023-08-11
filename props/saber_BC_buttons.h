27e1b462590fe07d71d2e8a1e0a84e4a3ac9888d
NoSloppy <53964195+NoSloppy@users.noreply.github.com>
2023-08-11 11:09:49 -0400

    endif

--- props/saber_BC_buttons.h
+++ props/saber_BC_buttons.h
@@ -637,8 +637,8 @@ public:
 #endif
           last_twist_ = millis();
         }
-#endif  // BC_TWIST_ON
       return true;
+#endif  // BC_TWIST_ON
 
 #ifdef BC_TWIST_OFF
     case EVENTID(BUTTON_NONE, EVENT_TWIST, MODE_ON):
