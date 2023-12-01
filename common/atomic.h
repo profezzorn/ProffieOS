923821caa28a45c85832d9208182407d73e5b1da
profezzorn <hubbe@hubbe.net>
2023-11-30 21:13:38 -0600

    compare-and-swap

--- common/atomic.h
+++ common/atomic.h
@@ -41,6 +41,10 @@ public:
   }
   void operator+=(T value) { value_ += value; }
   void operator-=(T value) { value_ -= value; }
+
+  bool cas(T current, T new_value) {
+    return value_.compare_exchange_strong(current, new_value);
+  }
   
 private:
   std::atomic<T> value_;
@@ -77,6 +81,21 @@ public:
     __atomic_fetch_sub(&value_, value, __ATOMIC_RELAXED);
 #else
     value_ -= value;
+#endif    
+  }
+  
+  bool cas(T current, T new_value) {
+#ifdef ARDUINO_ARCH_STM32L4
+    return __atomic_compare_exchange_n(&value_, &current, new_value, false, __ATOMIC_RELAXED, __ATOMIC_RELAXED);
+#else    
+    bool ret = false;
+    noInterrupts();
+    if (value_ == current) {
+      value_ = new_value;
+      ret = true;
+    }
+    interrupts();
+    return ret;
 #endif    
   }
   
