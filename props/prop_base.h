cbbbb03e48b7f453eeac5f0d7f493546c2ce5543
NoSloppy <53964195+NoSloppy@users.noreply.github.com>
2023-12-08 23:57:56 -0500

    too much announcing

--- props/prop_base.h
+++ props/prop_base.h
@@ -625,7 +625,7 @@ public:
     size_t best_config = FindBestConfig(announce);
     PVLOG_STATUS << "blade = " << best_config << "\n";
     current_config = blades + best_config;
-    bool bladestatus = id(announce) < NO_BLADE; 
+    bool bladestatus = id() < NO_BLADE; 
 
 #define ACTIVATE(N) do {     \
     if (!current_config->blade##N) goto bad_blade;  \
