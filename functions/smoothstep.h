e37a5ed5acdcd3cbe02f0e9c46ff93a55b9a7038
profezzorn <hubbe@hubbe.net>
2023-12-31 08:54:09 +0000

    make smoothstep use less flash memory

--- functions/smoothstep.h
+++ functions/smoothstep.h
@@ -10,34 +10,37 @@
 // From there it has a smooth transition to 32768, which will be reached at 75% of
 // the blade. If WIDTH is negative, the transition will go the other way.
 
-template<class POS, class WIDTH>
-class SmoothStep {
+class SmoothStepBase {
 public:
-  void run(BladeBase* blade) {
-    pos_.run(blade);
-    width_.run(blade);
-    int width = width_.calculate(blade);
-    float mult;
-    if (width == 0) {
-      mult = 32768;
-    } else {
-      mult = 32768 * 32768.0 / width / blade->num_leds();
-    }
-    mult_ = mult;
-    location_ = blade->num_leds() * mult * (pos_.calculate(blade) - width/2) / 32768;
-  }
-  
   int getInteger(int led) {
     int x = led * mult_ - location_;
     if (x < 0) return 0;
     if (x > 32768) return 32768;
     return (((x * x) >> 14) * ((3<<14) - x)) >> 15;
   }
+protected:
+  int mult_;
+  int location_;
+};
+
+template<class POS, class WIDTH>
+class SmoothStep : public SmoothStepBase {
+public:
+  void run(BladeBase* blade) {
+    pos_.run(blade);
+    width_.run(blade);
+    int width = width_.calculate(blade);
+    if (width == 0) {
+      mult_ = 32768;
+      location_ = blade->num_leds() * pos_.calculate(blade);
+    } else {
+      mult_ = 32768 * 32768 / width / blade->num_leds();
+      location_ =  32768 * pos_.calculate(blade) / width - 16384;
+    }
+  }
   
   PONUA SVFWrapper<POS> pos_;
   PONUA SVFWrapper<WIDTH> width_;
-  int mult_;
-  int location_;
 };
   
 #endif
