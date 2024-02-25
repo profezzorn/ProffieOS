9caecfbc81c2707a9bf274a7c915402f00329dae
profezzorn <hubbe@hubbe.net>
2024-02-25 15:47:27 -0800

    more efficient used argument checker

--- common/arg_parser.h
+++ common/arg_parser.h
@@ -124,54 +124,93 @@ private:
   const char* str_;
 };
 
-class GetUsedArgsParser : public ArgParser {
+template<size_t SIZE>
+class BitSet {
 public:
-  GetUsedArgsParser(const char* data) : ArgParser(data) {
-    for (size_t i = 0; i < NELEM(used_args_); i++) {
-      used_args_[0] = 0;
+  BitSet() {
+    for (size_t i = 0; i < NELEM(bits_); i++) {
+      bits_[i] = 0;
     }
   }
+  bool operator[](size_t bit) { return !!(bits_[bit >> 5] & (1UL << (bit & 31))); }
+  bool get(size_t bit) { return !!(bits_[bit >> 5] & (1UL << (bit & 31))); }
+  void set(size_t bit) { bits_[bit >> 5] |= (1UL << (bit & 31)); }
+  void clear(size_t bit) { bits_[bit >> 5] &=~ (1UL << (bit & 31)); }
+  void clear() {
+    for (size_t i = 0; i < NELEM(bits_); i++) {
+      bits_[i] = 0;
+    }
+  }
+  size_t popcount() {
+    size_t ret = 0;
+    for (size_t i = 0; i < NELEM(bits_); i++) {
+      ret += __builtin_popcount(bits_[i]);
+    }
+    return ret;
+  }
+  size_t next(size_t bit) {
+    for (size_t i = 1; i <= SIZE; i++) {
+      size_t j = (bit + i) % SIZE;
+      if (get(j)) return j;
+    }
+    return bit;
+  }
+  size_t prev(size_t bit) {
+    for (size_t i = 1; i <= SIZE; i++) {
+      size_t j = (bit + SIZE - i) % SIZE;
+      if (get(j)) return j;
+    }
+    return bit;
+  }
+  size_t nth(int bit) {
+    for (size_t i = 0; i <= SIZE; i++) {
+      if (get(i)) {
+	if (bit-- <= 0) {
+	  return i;
+	}
+      }
+    }
+    return 0;
+  }
+  
+private:
+  uint32_t bits_[(SIZE+31)/32];
+};
+
+// 32 bytes
+struct ArgInfo {
+  BitSet<128> used_;
+  BitSet<128> color_;
+
+  int used() { return used_.popcount(); }
+  int used(int ARG) { return used_[ARG]; }
+  int next(int ARG) { return used_.next(ARG); }
+  int prev(int ARG) { return used_.prev(ARG); }
+  int nth(int ARG) { return used_.nth(ARG); }
+  int iscolor(int ARG) { return color_[ARG]; }
+};
+
+class GetUsedArgsParser : public ArgParser {
+public:
+  GetUsedArgsParser(const char* data) : ArgParser(data) {}
   const char* GetArg(int arg_num, const char* name, const char* default_value) override {
     int arg = arg_num + offset_;
-    used_args_[arg >> 5] |= 1UL << (arg & 31);
+    arginfo_.used_.set(arg);
+    if (strchr(default_value, ',')) arginfo_.color_.set(arg);
     return ArgParser::GetArg(arg_num, name, default_value);
   }
   void Shift(int words) override {
     ArgParser::Shift(words);
     offset_ += words;
   }
-  int used() {
-    int ret = 0;
-    for (size_t i = 0; i < NELEM(used_args_); i++) {
-      ret += __builtin_popcount(used_args_[i]);
-    }
-    return ret;
-  }
-  int next(int ARG) {
-    for (size_t j = 0; j <= NELEM(used_args_); j++) {
-      int arg = (ARG + j) % NELEM(used_args_);
-      if (used_args_[arg >> 5] & (1UL << (arg & 31))) return arg;
-    }
-  }
-  int prev(int ARG) {
-    for (size_t j = 0; j <= NELEM(used_args_); j++) {
-      int arg = ((NELEM(used_args_) * 64) + ARG - j) % (NELEM(used_args_) * 32);
-      if (used_args_[arg >> 5] & (1UL << (arg & 31))) return arg;
-    }
-  }
-  int nth(int ARG) {
-    for (size_t arg = 0; arg <= NELEM(used_args_); arg++) {
-      if (used_args_[arg >> 5] & (1UL << (arg & 31))) {
-	if (ARG-- <= 0) {
-	  return arg;
-	}
-      }
-    }
-    return 0;
-  }
+  int used() { return arginfo_.used(); }
+  int next(int ARG) { return arginfo_.next(ARG); }
+  int prev(int ARG) { return arginfo_.prev(ARG); }
+  int nth(int ARG) { return arginfo_.nth(ARG); }
+  ArgInfo& getArgInfo() { return arginfo_; }
 private:
   int offset_ = 0;
-  uint32_t used_args_[8];
+  ArgInfo arginfo_;
 };
 
 class GetMaxArgParser : public ArgParser {
