ff9197e792e02a9748d9e4b6f3080f63d827a9dc
NoSloppy <53964195+NoSloppy@users.noreply.github.com>
2024-03-06 03:43:36 -0500

    Make DelayTimer class and use with scroll presets

--- props/saber_BC_buttons.h
+++ props/saber_BC_buttons.h
@@ -392,6 +392,32 @@ EFFECT(push);       // for Force Push gesture
 EFFECT(quote);      // for playing quotes
 EFFECT(monosfx);    // for Monophonically played sounds (iceblade, seismic charge etc...)
 EFFECT(swap);       // for standalone triggering EffectSequence<>
+EFFECT(mute);      // Plays before mute ignition to avoid confusion.
+
+class DelayTimer {
+public:
+    DelayTimer() : triggered_(false), trigger_time_(0), duration_(0) {}
+
+    void trigger(uint32_t duration) {
+        triggered_ = true;
+        trigger_time_ = millis();
+        duration_ = duration;
+    }
+
+    bool timerCheck() {
+        if (!triggered_) return false;
+        if (millis() - trigger_time_ > duration_) {
+            triggered_ = false;  // Reset the timer flag
+            return true;  // Timer has elapsed
+        }
+        return false;  // Timer is still running
+    }
+
+private:
+    bool triggered_;
+    uint32_t trigger_time_;
+    uint32_t duration_;
+};
 
 // The Saber class implements the basic states and actions
 // for the saber.
@@ -465,11 +491,8 @@ public:
       }
 
     // Scroll Presets timer check - avoid beep/wav overlap
-    if (scroll_presets_timer_trigger_ && DelayTimer(scroll_presets_timer_trigger_, last_timer_time_, beep_seq_duration_ * 1000)) {
-        if (scroll_presets_) {
-            SaberBase::DoEffect(EFFECT_NEWFONT, 0);
-        }
-        scroll_presets_timer_trigger_ = false; // Reset the timer flag
+    if (scrollPresetsTimer.timerCheck() && scroll_presets_) {
+        SaberBase::DoEffect(EFFECT_NEWFONT, 0);
     }
   }  // Loop()
 
@@ -775,9 +798,7 @@ public:
       beeper.Beep(0.05, 2000);
       beeper.Silence(0.05);
       beeper.Beep(0.10, 3000);
-      beep_seq_duration_ = 0.35;  // .05 breathing room
-      last_timer_time_ = millis();
-      scroll_presets_timer_trigger_ = true;
+      scrollPresetsTimer.trigger(350);
     } else {
       PVLOG_NORMAL << "** Exit Scroll Presets\n";
       // beep on exit
@@ -1244,9 +1265,9 @@ public:
   }
 
 private:
-  float beep_seq_duration_ = 0.0;
+  DelayTimer scrollPresetsTimer;
+
   float current_menu_angle_ = 0.0;
-  bool scroll_presets_timer_trigger_ = false;
   bool mode_volume_ = false;
   bool auto_lockup_on_ = false;
   bool auto_melt_on_ = false;
@@ -1268,7 +1289,6 @@ private:
   uint32_t saber_off_time_ = millis();
   uint32_t beep_delay_ = millis();
   uint32_t volume_range_delay_ = millis();
-  uint32_t last_timer_time_ = 0;
 
 };
 
