51be384203de0a66c92190f2cf862a522761f32d
NoSloppy <53964195+NoSloppy@users.noreply.github.com>
2024-03-31 02:41:15 -0400

    adjust example to match ints

--- functions/blaster_mode.h
+++ functions/blaster_mode.h
@@ -10,8 +10,8 @@ int prop_GetBlasterMode();
 // 0 for MODE_STUN, 1 for MODE_KILL, 2 for MODE_AUTO
 // This function should only be used when BLASTER_SHOTS_UNTIL_EMPTY is defined,
 // indicating that the current prop is a Blaster.
-// Example usage in a style:
-// StylePtr<ColorSelect<BlasterModeF, TrInstant, Red, Green, Blue>>()
+// Example usage in a style for Blue Stun, Red Kill, and Green Auto:
+// StylePtr<ColorSelect<BlasterModeF, TrInstant, Blue, Red, Green>>()
 
 class BlasterModeSVF {
 public:
