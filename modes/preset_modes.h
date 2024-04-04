a3361b1c268c80ea351d7504302ee71ef039c424
profezzorn <hubbe@hubbe.net>
2024-04-04 15:52:42 -0700

    fix some more typos

--- modes/preset_modes.h
+++ modes/preset_modes.h
@@ -2,6 +2,7 @@
 #define MODE_PRESET_MODES
 
 #include "../common/bytearray.h"
+#include "menu_list.h"
 
 // Returns the position of the current preset.
 int GetPresetPosition();
@@ -12,12 +13,13 @@ int CopyPreset(int from_position);
 namespace mode {
 
 template<class SPEC, class ACTION_ENTRY>
-using ConfirmMenu = typename SPEC::template MenuListMode<
+class  ConfirmMenu : public MenuEntryMenu<SPEC,
   PopMenuEntry<typename SPEC::SoundLibrary::tCancel>,
   PopMenuEntry<typename SPEC::SoundLibrary::tCancel>,
   ACTION_ENTRY,
   PopMenuEntry<typename SPEC::SoundLibrary::tCancel>,
-  PopMenuEntry<typename SPEC::SoundLibrary::tCancel>>;
+  PopMenuEntry<typename SPEC::SoundLibrary::tCancel>
+> {};
 
 template<class SPEC, class CMD, class ARG, class SOUND>
 using ConfirmCommandMenuEntry =
@@ -28,16 +30,16 @@ int menu_selected_preset = -1;
 template<class SPEC>
 struct MovePresetUpEntry : public MenuEntry {
   void say(int entry) override {
-    getSL<SPEC>->SayMovePresetUp();
+    getSL<SPEC>()->SayMovePresetUp();
   }
   void select(int entry) override {
     menu_selected_preset = -1;
     int pos = GetPresetPosition();
     if (pos > 0) {
       MovePreset(pos - 1);
-      getSL<SPEC>->SaySelect();
+      getSL<SPEC>()->SaySelect();
     } else {
-      getSL<SPEC>->SayListEndBuzz();
+      getSL<SPEC>()->SayListEndBuzz();
     }
   }
 };
@@ -45,7 +47,7 @@ struct MovePresetUpEntry : public MenuEntry {
 template<class SPEC>
 struct MovePresetDownEntry : public MenuEntry {
   void say(int entry) override {
-    getSL<SPEC>->SayMovePresetDown();
+    getSL<SPEC>()->SayMovePresetDown();
   }
   void select(int entry) override {
     menu_selected_preset = -1;
@@ -53,9 +55,9 @@ struct MovePresetDownEntry : public MenuEntry {
     int num_presets = GetTotalPresets();
     if (pos + 1 < num_presets) {
       MovePreset(pos + 1);
-      getSL<SPEC>->SaySelect();
+      getSL<SPEC>()->SaySelect();
     } else {
-      getSL<SPEC>->SayListEndBuzz();
+      getSL<SPEC>()->SayListEndBuzz();
     }
   }
 };
@@ -80,18 +82,18 @@ struct MovePresetToBeginningEntry : public MenuEntry {
 template<class SPEC>
 struct SelectPresetEntry : public MenuEntry {
   void say(int entry) override {
-    getSL<SPEC>->SaySelectPreset();
+    getSL<SPEC>()->SaySelectPreset();
   }
   void select(int entry) override {
     menu_selected_preset = GetPresetPosition();
-    getSL<SPEC>->SaySelect();
+    getSL<SPEC>()->SaySelect();
   }
 };
   
 template<class SPEC>
 struct InsertSelectedPresetEntry : public MenuEntry {
   void say(int entry) override {
-    getSL<SPEC>->SaySelectPreset();
+    getSL<SPEC>()->SaySelectPreset();
   }
   void select(int entry) override {
     int pos = GetPresetPosition();
@@ -99,7 +101,7 @@ struct InsertSelectedPresetEntry : public MenuEntry {
     if (pos <= menu_selected_preset) {
       menu_selected_preset++;
     }
-    getSL<SPEC>->SayPresetInserted();
+    getSL<SPEC>()->SayPresetInserted();
   }
 };
 
@@ -107,27 +109,27 @@ struct InsertSelectedPresetEntry : public MenuEntry {
 template<class SPEC, int BLADE>
 struct EditBladeSubMenuEntry : public MenuEntry {
   void say(int entry) override {
-    getSL<SPEC>->SaySelectPreset();
+    getSL<SPEC>()->SaySelectPreset();
   }
   void select(int entry) override {
     menu_current_blade = BLADE;
-    pushMode<SPEC::EditStyleMenu>();
+    pushMode<typename SPEC::EditStyleMenu>();
   }
 };
 
 #define EDIT_BLADE_SUBMENU(N) EditBladeSubMenuEntry<SPEC, N>,
   
 template<class SPEC>
-using EditPresetMenu = typename SPEC::template MenuListMode<
+class EditPresetMenu : public MenuEntryMenu<SPEC,
   SubMenuEntry<typename SPEC::SelectFont, typename SPEC::SoundLibrary::tEditFont>,
   SubMenuEntry<typename SPEC::SelectTrack, typename SPEC::SoundLibrary::tEditTrack>,
   ONCEPERBLADE(EDIT_BLADE_SUBMENU)
-  SPEC::MovePresetUpEntry,
-  SPEC::MovePresetDownEntry,
-  SPEC::SelectPresetEntry,
-  SPEC::InsertSelectedPreset,
+  typename SPEC::MovePresetUpEntry,
+  typename SPEC::MovePresetDownEntry,
+  typename SPEC::SelectPresetEntry,
+  typename SPEC::InsertSelectedPresetEntry,
   ConfirmCommandMenuEntry<SPEC, STRTYPE("delete_preset"), STRTYPE(""), typename SPEC::SoundLibrary::tDeletePreset>
-  >;
+> {};
   
 }  // namespace mode
 
