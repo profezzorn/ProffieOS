fe39a879bf0baafa3a10102ae0e43fb9e516c2da
profezzorn <hubbe@hubbe.net>
2024-04-04 22:25:07 -0700

    move StringPiece to separate file

--- common/stdout.h
+++ common/stdout.h
@@ -154,53 +154,11 @@ int constexpr toLower(char x) {
   return (x >= 'A' && x <= 'Z') ? x - 'A' + 'a' : x;
 }
 
+#include "string_piece.h"
 
-struct StringPiece {
-  StringPiece(const char* s) : str(s), len(strlen(s)) {}
-  StringPiece(const char* s, size_t l) : str(s), len(l) {}
-  StringPiece(const char* begin, const char* end) : str(begin), len(end >= begin ? end - begin : 0) {}
-  StringPiece() : str(0), len(0) {}
-  int cmp(const StringPiece& other) const {
-    const char* a = str;
-    const char* b = other.str;
-    size_t l = 0;
-    while (true) {
-      if (l == len && l == other.len) return 0;
-      if (l == len) return -1;
-      if (l == other.len) return 1;
-      char A = toLower(*a);
-      char B = toLower(*b);
-      if (A != B) return A < B ? -1 : 1;
-      a++;
-      b++;
-      l++;
-    }
-  }
-  bool operator<(const StringPiece& other) const { return cmp(other) < 0; }
-  bool operator>(const StringPiece& other) const { return cmp(other) > 0; }
-  bool operator<=(const StringPiece& other) const { return cmp(other) <= 0; }
-  bool operator>=(const StringPiece& other) const { return cmp(other) <= 0; }
-  bool operator==(const StringPiece& other) const { return cmp(other) == 0; }
-  bool operator!=(const StringPiece& other) const { return cmp(other) != 0; }
-  char operator[](size_t x) const { return str[x]; }
-  explicit operator bool() const { return len != 0; }
-
-  void printTo(Print& p) {
-    p.write((const uint8_t*)str, len);
-  }
-
-  void paste(char* to) const { memcpy(to, str, len); }
-
-  bool contains(char c) const {
-    for (size_t i = 0; i < len; i++) {
-      if (str[i] == c) return true;
-    }
-    return false;
-  }
-
-  const char* str;
-  size_t len;
-};
+void StringPiece::printTo(Print& p) {
+  p.write((const uint8_t*)str, len);
+}
 
 template<size_t bufsize>
 struct Line {
