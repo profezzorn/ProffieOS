fcc3a99418cdd98785c306950c2ebeb816dd52e1
profezzorn <hubbe@hubbe.net>
2024-09-26 02:49:26 -0500

    create rotary base class

--- buttons/rotary.h
+++ buttons/rotary.h
@@ -47,18 +47,13 @@ void do_nothing(void* context) {}
 
 // This is generally meant for an EC11-type twist knob which
 // has four steps between each detent.
-template<int pin1, int pin2, int steps_per_detent>
-class RotaryClass : public Looper {
+template<int pin1, int pin2, typename DELTA = int8_t>
+class RotaryBase {
 public:
-  RotaryClass(RotaryReceiver* receiver) : receiver_(receiver) {}
-
   void Setup() override {
     pinMode(pin1, INPUT_PULLUP);
     pinMode(pin2, INPUT_PULLUP);
-    Setup1();
   }
-
-  const char* name() override { return "Rotary"; }
 
   static void detach_interrupt(int pin) {
 #ifdef PROFFIEBOARD_VERSION
@@ -90,20 +85,20 @@ public:
   static int Setup1() {
     detach_interrupt(pin2);
     if (digitalRead(pin1)) {
-      attachInterrupt(pin1, &RotaryClass::LO1, FALLING);
+      attachInterrupt(pin1, &RotaryBase::LO1, FALLING);
       return 1;
     } else {
-      attachInterrupt(pin1, &RotaryClass::HI1, RISING);
+      attachInterrupt(pin1, &RotaryBase::HI1, RISING);
       return -1;
     }
   }
   static int Setup2() {
     detach_interrupt(pin1);
     if (digitalRead(pin2)) {
-      attachInterrupt(pin2, &RotaryClass::LO2, FALLING);
+      attachInterrupt(pin2, &RotaryBase::LO2, FALLING);
       return 1;
     } else {
-      attachInterrupt(pin2, &RotaryClass::HI2, RISING);
+      attachInterrupt(pin2, &RotaryBase::HI2, RISING);
       return -1;
     }
   }
@@ -113,8 +108,27 @@ public:
   static void HI1() { adjustPhaseAndAdd(2, -Setup2()); }
   static void HI2() { adjustPhaseAndAdd(3, Setup1()); }
 
+protected:
+  static volatile DELTA delta_;
+};
+
+template<int pin1, int pin2, typename DELTA>
+volatile DELTA RotaryBase<pin1, pin2, DELTA>::delta_ = 0;
+
+
+// This is generally meant for an EC11-type twist knob which
+// has four steps between each detent.
+template<int pin1, int pin2, int steps_per_detent>
+class RotaryClass : public RotaryBase<pin1, pin2> {
+public:
+  RotaryClass(RotaryReceiver* receiver) : receiver_(receiver) {}
+
+  void Setup() override { RotaryBase<pin1, pin2>::Setup(); }
+
+  const char* name() override { return "Rotary"; }
+
   void Loop() override {
-    int delta = delta_;
+    int delta = this->delta_;
     while (delta - pos_ >= steps_per_detent) {
       receiver_->Update(1);
       pos_ += steps_per_detent;
@@ -127,12 +141,10 @@ public:
     }
   }
 private:
-  static volatile int8_t delta_;
   int8_t pos_ = 0;
   RotaryReceiver* receiver_;
 };
 
-template<int pin1, int pin2, int steps_per_detent> volatile int8_t RotaryClass<pin1, pin2, steps_per_detent>::delta_ = 0;
 template<int pin1, int pin2, int steps_per_detent = 4> using Rotary = RotaryClass<pin1, pin2, steps_per_detent>;
 
 #endif
