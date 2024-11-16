17fe59ccb3144703cc946893fe448ef537398bb7
profezzorn <hubbe@hubbe.net>
2024-11-16 02:36:12 -0600

    clang compatibility

--- common/cyclint.h
+++ common/cyclint.h
@@ -9,7 +9,9 @@ struct Cyclint {
   static const T NMASK = ((T)1) << (sizeof(T)*8 - 1);
   explicit Cyclint(T value) : v(value) {}
   Cyclint operator+(T o) { return Cyclint(v+o); }
+  Cyclint operator+(int o) { return Cyclint(v+o); }
   Cyclint operator-(T o) { return Cyclint(v-o); }
+  Cyclint operator-(int o) { return Cyclint(v-o); }
   void operator+=(T o) { v += o; }
   void operator-=(T o) { v -= o; }
   bool operator==(Cyclint o) const { return v == o.v; }
