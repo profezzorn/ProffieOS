99020b82d61574d0fcaf51e231f4f4d1ca6b8b79
Christopher Shannon <cs238@students.uwf.edu>
2025-01-15 23:38:57 -0500

    Implementing Feedback

--- props/prop_base.h
+++ props/prop_base.h
@@ -584,40 +584,38 @@ public:
 #ifndef SHARED_POWER_PINS
 #warning SHARED_POWER_PINS is recommended when using BLADE_ID_SCAN_MILLIS
 #endif
-#ifdef BLADE_ID_SCAN_TIMEOUT
-#define BLADE_ID_SCAN_TIMEOUT 60 * 10 * 1000
-#endif
     
     bool find_blade_again_pending_ = false;
     uint32_t last_scan_id_ = 0;
     bool ScanBladeIdNow() {
         uint32_t now = millis();
         
-        bool scan = true;
-        #ifdef BLADE_ID_STOP_SCAN_WHILE_IGNITED
+        bool scan = (now - last_scan_id_) > BLADE_ID_SCAN_MILLIS;
+        
+#ifdef BLADE_ID_STOP_SCAN_WHILE_IGNITED
         if (IsOn()) {
             scan = false;
         }
-        #endif
+#endif
         
+#ifdef BLADE_ID_SCAN_TIMEOUT
+#define BLADE_ID_SCAN_TIMEOUT 60 * 10 * 1000
+
         if ((now - blade_id_scan_start_) < BLADE_ID_SCAN_TIMEOUT) {
             scan = false;
         }
+#endif
         
-        if (scan == true) {
-            if (now - last_scan_id_ > BLADE_ID_SCAN_MILLIS) {
-                last_scan_id_ = now;
-                size_t best_config = FindBestConfig(PROFFIEOS_LOG_LEVEL >= 500);
-                if (current_config != blades + best_config) {
-                    // We can't call FindBladeAgain right away because
-                    // we're called from the blade. Wait until next loop() call.
-                    find_blade_again_pending_ = true;
-                }
-                return true;
+        if (scan) {
+            last_scan_id_ = now;
+            size_t best_config = FindBestConfig(PROFFIEOS_LOG_LEVEL >= 500);
+            if (current_config != blades + best_config) {
+                // We can't call FindBladeAgain right away because
+                // we're called from the blade. Wait until next loop() call.
+                find_blade_again_pending_ = true;
             }
-            return false;
-        }
-        else{
+            return true;
+        } else {
             return false;
         }
     }
