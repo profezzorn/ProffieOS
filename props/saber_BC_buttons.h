8232fc9944458920be9f68a9cf4d652279857b5f
NoSloppy <53964195+NoSloppy@users.noreply.github.com>
2025-01-22 21:43:42 -0500

    fix SpeakBladeID()

--- props/saber_BC_buttons.h
+++ props/saber_BC_buttons.h
@@ -1523,7 +1523,7 @@ public:
 
 #ifdef SPEAK_BLADE_ID
   void SpeakBladeID(float id) override {
-    if (&SFX_mnum) {
+    if (SFX_mnum) {
       sound_library_v2.SayBlade();
       sound_library_.SayNumber(id, SAY_WHOLE);
     } else {
