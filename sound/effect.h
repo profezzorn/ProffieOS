2b173da88a09ec4253f4a0ddc6fa5440e55e5265
NoSloppy <53964195+NoSloppy@users.noreply.github.com>
2025-01-25 04:35:53 -0500

    guard idle sound with define

--- sound/effect.h
+++ sound/effect.h
@@ -740,8 +740,9 @@ EFFECT2(lock, lock);
 EFFECT(swng);
 EFFECT(slsh);
 EFFECT(quote);
+#ifdef ENABLE_IDLE_SOUND
 EFFECT2(idle, idle);  // Plays when blade is off
-
+#endif
 // Looped swing fonts. (SmoothSwing V1/V2)
 EFFECT2(swingl, swingl);  // Looped swing, LOW
 EFFECT2(swingh, swingh);  // Looped swing, HIGH
