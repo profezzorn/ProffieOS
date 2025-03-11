69eab0f5c1b88e39f02357f517099aef1b33d761
NoSloppy <53964195+NoSloppy@users.noreply.github.com>
2025-03-11 05:14:45 -0400

    Breakout PollScanID(), make GetNoBladeLevelBefore()

--- props/prop_base.h
+++ props/prop_base.h
@@ -616,20 +616,25 @@ public:
         }
     }
 
+  virtual int GetNoBladeLevelBefore() {
+    int level = current_config->ohm / NO_BLADE;
+    return level;
+  }
+
   // Must be called from loop()
   void PollScanId() {
     if (find_blade_again_pending_) {
       find_blade_again_pending_ = false;
-      int noblade_level_before = current_config->ohm / NO_BLADE;
+      int noblade_level_before = GetNoBladeLevelBefore();
       FindBladeAgain();
       int noblade_level_after = current_config->ohm / NO_BLADE;
-
+      
       if (noblade_level_before < noblade_level_after) {
         SaberBase::DoBladeDetect(false);
-      } else if(noblade_level_before > noblade_level_after) {
+      } else if (noblade_level_before > noblade_level_after) {
         SaberBase::DoBladeDetect(true);
       } else {
-	SaberBase::DoNewFont();
+        SaberBase::DoNewFont();
       }
     }
   }
@@ -821,11 +826,11 @@ public:
     // If we're spinning the saber or if loud sounds are playing,
     // require a stronger acceleration to activate the clash.
     if (v > (CLASH_THRESHOLD_G * (1
-				  + fusor.gyro().len() / 500.0
+          + fusor.gyro().len() / 500.0
 #if defined(ENABLE_AUDIO) && defined(AUDIO_CLASH_SUPPRESSION_LEVEL)
-				  + dynamic_mixer.audio_volume() * (AUDIO_CLASH_SUPPRESSION_LEVEL * 1E-10) * dynamic_mixer.get_volume()
+          + dynamic_mixer.audio_volume() * (AUDIO_CLASH_SUPPRESSION_LEVEL * 1E-10) * dynamic_mixer.get_volume()
 #endif
-	       ))) {
+         ))) {
       if ( (accel_ - fusor.down()).len2() > (accel - fusor.down()).len2() ) {
         diff = -diff;
       }
@@ -1098,7 +1103,7 @@ public:
       if (current_style() && !current_style()->Charging()) {
         LowBatteryOff();
         if (millis() - last_beep_ > 15000) {  // (was 5000)
-	  STDOUT << "Low battery: " << battery_monitor.battery() << " volts\n";
+    STDOUT << "Low battery: " << battery_monitor.battery() << " volts\n";
           SaberBase::DoLowBatt();
           last_beep_ = millis();
         }
@@ -1293,10 +1298,10 @@ public:
     if (!strcmp(cmd, "on1")) {
       PRINT_CHECK_BLADE=true;
       if (SaberBase::BladeIsOn(2)) {
-	STDOUT << "faston!\n";
-	SaberBase::TurnOn(EffectLocation(0, ~BladeSet::fromBlade(2)));
+  STDOUT << "faston!\n";
+  SaberBase::TurnOn(EffectLocation(0, ~BladeSet::fromBlade(2)));
       } else {
-	On(EffectLocation(0, ~BladeSet::fromBlade(2)));
+  On(EffectLocation(0, ~BladeSet::fromBlade(2)));
       }
       PRINT_CHECK_BLADE=false;
       return true;
@@ -1304,10 +1309,10 @@ public:
     if (!strcmp(cmd, "on2")) {
       PRINT_CHECK_BLADE=true;
       if (SaberBase::BladeIsOn(1)) {
-	STDOUT << "faston!\n";
-	SaberBase::TurnOn(EffectLocation(0, ~BladeSet::fromBlade(1)));
+  STDOUT << "faston!\n";
+  SaberBase::TurnOn(EffectLocation(0, ~BladeSet::fromBlade(1)));
       } else {
-	On(EffectLocation(0, ~BladeSet::fromBlade(1)));
+  On(EffectLocation(0, ~BladeSet::fromBlade(1)));
       }
       PRINT_CHECK_BLADE=false;
       return true;
@@ -1315,11 +1320,11 @@ public:
     if (!strcmp(cmd, "off2")) {
       PRINT_CHECK_BLADE=true;
       if (SaberBase::BladeIsOn(1)) {
-	STDOUT << "Turning off SINGLE blade.\n";
-	SaberBase::TurnOff(OffType::OFF_NORMAL, EffectLocation(1000, ~~BladeSet::fromBlade(2)));
+  STDOUT << "Turning off SINGLE blade.\n";
+  SaberBase::TurnOff(OffType::OFF_NORMAL, EffectLocation(1000, ~~BladeSet::fromBlade(2)));
       } else {
-	STDOUT << "Turning off all blades.\n";
-	Off(OffType::OFF_NORMAL);
+  STDOUT << "Turning off all blades.\n";
+  Off(OffType::OFF_NORMAL);
       }
       PRINT_CHECK_BLADE=false;
       return true;
@@ -1327,12 +1332,12 @@ public:
     if (!strcmp(cmd, "off1")) {
       PRINT_CHECK_BLADE=true;
       if (SaberBase::BladeIsOn(2)) {
-	EffectLocation tmp = EffectLocation(1000, ~~BladeSet::fromBlade(1));
-	STDOUT << "Turning off SINGLE blade: " << tmp << "\n";
-	SaberBase::TurnOff(OffType::OFF_NORMAL, tmp);
+  EffectLocation tmp = EffectLocation(1000, ~~BladeSet::fromBlade(1));
+  STDOUT << "Turning off SINGLE blade: " << tmp << "\n";
+  SaberBase::TurnOff(OffType::OFF_NORMAL, tmp);
       } else {
-	STDOUT << "Turning off all blades.\n";
-	Off(OffType::OFF_NORMAL);
+  STDOUT << "Turning off all blades.\n";
+  Off(OffType::OFF_NORMAL);
       }
       PRINT_CHECK_BLADE=false;
       return true;
@@ -1743,7 +1748,7 @@ public:
     if (!strcmp(cmd, "change_preset") && arg) {
       int preset = strtol(arg, NULL, 0);
       if (preset != current_preset_.preset_num) {
-	SaveState(preset);
+  SaveState(preset);
         SetPreset(preset, true);
       }
       return true;
@@ -1779,26 +1784,26 @@ public:
     if (!strcmp(cmd, "list_fonts")) {
       LOCK_SD(true);
       for (LSFS::Iterator iter("/"); iter; ++iter) {
-	if (iter.name()[0] == '.') continue;
-	if (!strcmp(iter.name(), "common")) continue;
+  if (iter.name()[0] == '.') continue;
+  if (!strcmp(iter.name(), "common")) continue;
         if (!iter.isdir()) continue;
-	bool isfont = false;
-	for (LSFS::Iterator i2(iter); i2 && !isfont; ++i2) {
-	  if (i2.isdir()) {
-	    if (!strcasecmp("hum", i2.name())) isfont = true;
-	    if (!strcasecmp("alt000", i2.name())) isfont = true;
-	  } else {
-	    const char* tmp = i2.name();
-	    if (!startswith("hum", tmp)) continue;
-	    tmp += 3;
-	    if (startswith("m", tmp)) tmp++;
-	    while (*tmp >= '0' && *tmp <= '9') tmp++;
-	    if (!strcasecmp(".wav", tmp)) isfont = true;
-	  }
-	}
-	if (isfont) {
-	  STDOUT.println(iter.name());
-	}
+  bool isfont = false;
+  for (LSFS::Iterator i2(iter); i2 && !isfont; ++i2) {
+    if (i2.isdir()) {
+      if (!strcasecmp("hum", i2.name())) isfont = true;
+      if (!strcasecmp("alt000", i2.name())) isfont = true;
+    } else {
+      const char* tmp = i2.name();
+      if (!startswith("hum", tmp)) continue;
+      tmp += 3;
+      if (startswith("m", tmp)) tmp++;
+      while (*tmp >= '0' && *tmp <= '9') tmp++;
+      if (!strcasecmp(".wav", tmp)) isfont = true;
+    }
+  }
+  if (isfont) {
+    STDOUT.println(iter.name());
+  }
       }
       LOCK_SD(false);
       return true;
@@ -1813,7 +1818,7 @@ public:
     switch (event) {
       case EVENT_RELEASED:
         clash_pending_ = false;
-	[[gnu::fallthrough]];
+  [[gnu::fallthrough]];
       case EVENT_PRESSED:
         IgnoreClash(50); // ignore clashes to prevent buttons from causing clashes
       default:
