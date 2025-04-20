220ec2b640ee87a4aa0f98a2392522ad2d1224bd
NoSloppy <53964195+NoSloppy@users.noreply.github.com>
2025-04-20 16:37:42 -0700

    Update effect_increment.h (#796)

--- functions/effect_increment.h
+++ functions/effect_increment.h
@@ -47,9 +47,9 @@ using LockupPulseF = SingleValueAdapter<LockupPulseSVF<LOCKUP_TYPE>>;
 
 // Usage: IncrementWithReset<PULSE, RESET_PULSE, MAX, I>
 // PULSE: FUNCTION (pulse type) 
-// RESET_PULSEE: FUNCTION (pulse type) defaults to Int<0> (no reset)
+// RESET_PULSE: FUNCTION (pulse type) defaults to Int<0> (no reset)
 // MAX, I: FUNCTION
-// Starts at zero, increments by I each time the PULSE occurse.
+// Starts at zero, increments by I each time the PULSE occurs.
 // If it reaches MAX it stays there.
 // Resets back to zero when RESET_PULSE occurs.
 
