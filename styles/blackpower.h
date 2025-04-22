273b869f6f77e1996634e88bd0a9188779b4c6d2
Sabersense-CC <126567924+Sabersense-CC@users.noreply.github.com>
2025-04-22 15:40:53 +0100

    Create blackpower.h
    Used for Blade Plug Charging.

---
+++ styles/blackpower.h
@@ -0,0 +1,32 @@
+#ifndef STYLES_BLACKPOWER_H
+#define STYLES_BLACKPOWER_H
+
+#include "blade_style.h"
+
+// Usage:
+// Provides blade style that keeps pogo
+// pins live for Blade Plug Charging.
+// Use with ChargingStylePtr tp prevent
+// IDLE_OFF_TIME shutting down LED power.
+
+class BlackPower : public BladeStyle {
+public:
+    void run(BladeBase* base) override {
+    }
+
+    OverDriveColor getColor(int led) override {
+      return OverDriveColor(Color16(0, 0, 0));
+    }
+
+    bool NoOnOff() override {
+      return true;  // Pins stay live for blade plug charging.
+    }
+
+    bool IsHandled(HandledFeature effect) override {
+      return false;
+    }
+  };
+
+StyleFactoryImpl<BlackPower> black_power;
+
+#endif
