511a3a965c949a3dc9c218a11a1298ee4d06af2c
Olivier <olivierflying321@gmail.com>
2025-05-17 03:53:45 +0200

    Update button_base.h
    Just tabs & stray spaces clean-up & one comment spelling error.

--- buttons/button_base.h
+++ buttons/button_base.h
@@ -48,7 +48,7 @@ public:
 protected:
   // We send two events:
   // the event itself, and the event with a count, dependinging on how
-  // may double-presses there has been. The count starts at FIRST, that way
+  // many double-presses there has been. The count starts at FIRST, that way
   // the first event can be distinguished from the rest.
   bool Send(uint32_t event) {
     if (!prop.Event(button_, (EVENT)(event + (EVENT_SECOND_PRESSED - EVENT_FIRST_PRESSED) * press_count_))) {
@@ -66,23 +66,23 @@ protected:
       saved_event_ = event + (EVENT_SAVED_CLICK_SHORT - EVENT_CLICK_SHORT);
     }
   }
-  
+
   void Loop() override {
     STATE_MACHINE_BEGIN();
     while (true) {
       while (!DebouncedRead()) {
-	if (saved_event_ &&
-	    millis() - push_millis_ > BUTTON_DOUBLE_CLICK_TIMEOUT) {
-	  Send(saved_event_);
-	  saved_event_ = 0;;
-	}
-	YIELD();
+        if (saved_event_ &&
+          millis() - push_millis_ > BUTTON_DOUBLE_CLICK_TIMEOUT) {
+          Send(saved_event_);
+          saved_event_ = 0;;
+        }
+        YIELD();
       }
       saved_event_ = 0;
       if (millis() - push_millis_ < BUTTON_DOUBLE_CLICK_TIMEOUT) {
-	if (press_count_ < 4) press_count_++;
+        if (press_count_ < 4) press_count_++;
       } else {
-	press_count_ = 1;
+        press_count_ = 1;
       }
       Send(EVENT_PRESSED);
       push_millis_ = millis();
@@ -93,10 +93,10 @@ protected:
           while (DebouncedRead() && (current_modifiers & button_)) {
             if (millis() - push_millis_ > BUTTON_HELD_MEDIUM_TIMEOUT){
               Send(EVENT_HELD_MEDIUM);
-	      while (DebouncedRead() && (current_modifiers & button_)) {
+              while (DebouncedRead() && (current_modifiers & button_)) {
                 if (millis() - push_millis_ > BUTTON_HELD_LONG_TIMEOUT) {
                   Send(EVENT_HELD_LONG);
-		  while (DebouncedRead() && (current_modifiers & button_)) YIELD();
+                  while (DebouncedRead() && (current_modifiers & button_)) YIELD();
                 }
                 YIELD();
               }
@@ -113,7 +113,7 @@ protected:
         if (millis() - push_millis_ < BUTTON_SHORT_CLICK_TIMEOUT) {
           SendClick(EVENT_CLICK_SHORT);
         } else if (millis() - push_millis_ < BUTTON_LONG_CLICK_TIMEOUT) {
-	  // Long clicks cannot be "saved", so just emit immediately.
+          // Long clicks cannot be "saved", so just emit immediately.
           Send(EVENT_CLICK_LONG);
         }
       } else {
@@ -125,44 +125,44 @@ protected:
   }
 
   bool Parse(const char* cmd, const char* arg) override {
-#ifndef DISABLE_DIAGNOSTIC_COMMANDS    
+#ifndef DISABLE_DIAGNOSTIC_COMMANDS
     if (!strcmp(cmd, name_)) {
       EVENT e = EVENT_CLICK_SHORT;
       int cnt = 0;
       if (arg) {
-	if (strlen(arg) > 2) {
-	  bool ret;
-	  current_modifiers |= button_;
-	  const char *tmp = strchr(arg, ' ');
-	  if (tmp) {
-	    char cmd2[32];
-	    memcpy(cmd2, arg, tmp - arg);
-	    cmd2[tmp-arg] = 0;
-	    ret = CommandParser::DoParse(cmd2, tmp + 1);
-	  } else {
-	    ret = CommandParser::DoParse(arg, NULL);
-	  }
-	  current_modifiers &=~ button_;
-	  return ret;
-	}
-	switch (arg[0]) {
-	case 'p': e = EVENT_PRESSED; break;
-	case 'r': e = EVENT_RELEASED; break;
-	case 'h': e = EVENT_HELD; break;
-	case 'm': e = EVENT_HELD_MEDIUM; break;
-	case 'l': e = EVENT_HELD_LONG; break;
-	case 'S': e = EVENT_CLICK_SHORT; break;
-	case 's': e = EVENT_SAVED_CLICK_SHORT; break;
-	case 'L': e = EVENT_CLICK_LONG; break;
-	}
-	if (strlen(arg) > 1) {
-	  cnt = atoi(arg + 1);
-	}
+        if (strlen(arg) > 2) {
+          bool ret;
+          current_modifiers |= button_;
+          const char *tmp = strchr(arg, ' ');
+          if (tmp) {
+            char cmd2[32];
+            memcpy(cmd2, arg, tmp - arg);
+            cmd2[tmp-arg] = 0;
+            ret = CommandParser::DoParse(cmd2, tmp + 1);
+          } else {
+            ret = CommandParser::DoParse(arg, NULL);
+          }
+          current_modifiers &=~ button_;
+          return ret;
+        }
+        switch (arg[0]) {
+        case 'p': e = EVENT_PRESSED; break;
+        case 'r': e = EVENT_RELEASED; break;
+        case 'h': e = EVENT_HELD; break;
+        case 'm': e = EVENT_HELD_MEDIUM; break;
+        case 'l': e = EVENT_HELD_LONG; break;
+        case 'S': e = EVENT_CLICK_SHORT; break;
+        case 's': e = EVENT_SAVED_CLICK_SHORT; break;
+        case 'L': e = EVENT_CLICK_LONG; break;
+        }
+        if (strlen(arg) > 1) {
+          cnt = atoi(arg + 1);
+        }
       }
       prop.Event(button_, (EVENT)(e + (EVENT_SECOND_PRESSED - EVENT_FIRST_PRESSED) * cnt));
       return true;
     }
-#endif    
+#endif
     return false;
   }
 
