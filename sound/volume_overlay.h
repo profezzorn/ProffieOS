f32dc6036e4c288bcfe1255c08d674a3d46b255b
NoSloppy <53964195+NoSloppy@users.noreply.github.com>
2025-07-04 03:56:55 -0400

    add StartAndFade, with fractional stepping to allow fades slower than 1 unit/sample

--- sound/volume_overlay.h
+++ sound/volume_overlay.h
@@ -10,7 +10,7 @@ const uint32_t kDefaultSpeed = 500 * kMaxVolume / AUDIO_RATE;
 template<class T>
 class VolumeOverlay : public T {
 public:
-  VolumeOverlay() : volume_(kMaxVolume / 100), stop_when_zero_(false) {
+  VolumeOverlay() : volume_(kMaxVolume / 100), stop_when_zero_(false), use_fractional_fade_(false), fractional_speed_(0.0f), fractional_accum_(0.0f) {
     volume_.set(kDefaultVolume);
     volume_.set_target(kDefaultVolume);
     volume_.set_speed(kDefaultSpeed);
@@ -24,8 +24,8 @@ public:
         // Do nothing
       } else if (mult == 0) {
         if (stop_when_zero_.get()) {
-	  volume_.set_speed(kDefaultSpeed);
-	  T::StopFromReader();
+          volume_.set_speed(kDefaultSpeed);
+          T::StopFromReader();
         }
         for (int i = 0; i < elements; i++) data[i] = 0;
       } else {
@@ -37,7 +37,14 @@ public:
       for (int i = 0; i < elements; i++) {
         int32_t v = (data[i] * (int32_t)volume_.value()) >> kVolumeShift;
         data[i] = clamptoi16(v);
-        volume_.advance();
+        if (use_fractional_fade_) {
+          fractional_accum_ += fractional_speed_;
+          int steps = (int)fractional_accum_;
+          fractional_accum_ -= steps;
+          for (int s = 0; s < steps; s++) volume_.advance();
+        } else {
+          volume_.advance();
+        }
       }
     }
     return elements;
@@ -70,7 +77,11 @@ public:
     volume_.set_speed(speed);
   }
   void set_fade_time(float t) {
-    set_speed(std::max<int>(1, (int)(kMaxVolume / t / AUDIO_RATE)));
+    // Prevent zero or too-small fade times
+    if (t <= 0.01f) { t = 0.01f; }
+    fractional_accum_ = 0.0f;
+    fractional_speed_ = (float)kMaxVolume / (t * AUDIO_RATE);
+    use_fractional_fade_ = true;
   }
   float fade_speed() const {
     return (kMaxVolume / (float)volume_.speed_) / AUDIO_RATE;
@@ -84,6 +95,15 @@ public:
     stop_when_zero_.set(true);
   }
 
+  void StartAndFade() {
+    if (use_fractional_fade_) {
+      volume_.set_speed(1);
+    }
+    volume_.set(0);
+    volume_.set_target(kDefaultVolume);
+    stop_when_zero_.set(false);
+  }
+
   void ResetStopWhenZero() {
     stop_when_zero_.set(false);
   }
@@ -91,6 +111,9 @@ public:
 private:
   ClickAvoiderLin volume_;
   POAtomic<bool> stop_when_zero_;
+  bool use_fractional_fade_;
+  float fractional_speed_;
+  float fractional_accum_;
 };
 
 #endif
