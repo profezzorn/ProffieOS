43ed226cd795ddbdcbcc018d659b1323bde6dda2
jimsworld <jamesn_1991@hotmail.co.uk>
2025-08-29 14:01:13 +0100

    Committing changes based on latest reviews.

--- config/hev_config.h
+++ config/hev_config.h
@@ -2,30 +2,24 @@
 #include "proffieboard_v2_config.h"
 #define NUM_BLADES 1
 #define NUM_BUTTONS 2
-#define VOLUME 30
+#define VOLUME 50
 const unsigned int maxLedsPerStrip = 144;
 #define CLASH_THRESHOLD_G 3.0
-#define ENABLE_AUDIO
-#define ENABLE_MOTION
-#define ENABLE_WS2811
-#define ENABLE_SD
+//If not disabled, Armor Readout play back floats around current Armor value.
 #define DISABLE_NO_REPEAT_RANDOM
-#define HEV_RANDOM_EVENT_INTERVAL_MS 30000
-#define HEV_RANDOM_HAZARD_CHANCE 50
-#define HEV_HAZARD_DELAY_MS 6000
-#define HEV_HAZARD_DECREASE_MS 1000
-#define HEV_HAZARD_AFTER_REVIVE_MS 30000
-#define HEV_HEALTH_INCREASE_MS 100
-#define HEV_ARMOR_INCREASE_MS 100
 #endif
 
 #ifdef CONFIG_PROP
 #include "../props/hev.h"
 #endif
 
+// All HALF-LIFE sfx audio can be found in the game's "sound" directory.
+// Specifically, HEV Suit voice lines can be found within the "fvox" dir.
+// The music tracks can be found in the "media" dir in the root folder.
+
 #ifdef CONFIG_PRESETS
 Preset presets[] = {
-  { "_hev;common_hev", "common_hev/tracks/hl1_ost/10 Valve Theme [Extended].wav",
+  { "hev;common_hev", "common_hev/tracks/hl1_ost/10 Valve Theme [Extended].wav",
     StyleNormalPtr<Orange, Blue, 800, 300>(),
   }
 };
