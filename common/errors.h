3f4ea959ab03292ec4c64e00aa2828cf42f8cc81
Olivier <olivierflying321@gmail.com>
2025-09-04 19:09:10 +0200

    Update errors.h
    Addition of:
    - voice_pack_not_found
    - error_in_voice_pack_version
    
    Some cosmetic line-ups

--- common/errors.h
+++ common/errors.h
@@ -9,6 +9,8 @@ public:
   static void error_in_blade_array();
   static void error_in_font_directory();
   static void low_battery();
+  static void voice_pack_not_found();
+  static void error_in_voice_pack_version();
 };
 
 #endif // COMMON_ERROR_H_DECLARED
@@ -16,30 +18,35 @@ public:
 #ifdef PROFFIEOS_DEFINE_FUNCTION_STAGE
 
 #if defined(DISABLE_TALKIE) && defined(KEEP_MINIMUM_TALKIE_ONLY)
-#warning Your config file has DISABLE_TALKIE & KEEP_MINIMUM_TALKIE_ONLY in it. DISABLE_TALKIE has priority and no talkies will be included!
+#warning Your config file has both DISABLE_TALKIE & KEEP_MINIMUM_TALKIE_ONLY defined in it. DISABLE_TALKIE has priority and no talkie will be included!
 #endif
 
 #if !defined(DISABLE_TALKIE) && !defined(KEEP_MINIMUM_TALKIE_ONLY)
+#ifdef KEEP_REGULAR_TALKIE
+#error Your config file has KEEP_REGULAR_TALKIE defined. Please delete it from your config! For internal use only!
+#else
 #define KEEP_REGULAR_TALKIE
 #endif
 
 void ProffieOSErrors::sd_card_not_found() {
   SaberBase::DoEffect(EFFECT_SD_CARD_NOT_FOUND, 0);
 #ifdef ENABLE_AUDIO
+  // no need for a ".wav" file for this one
 #ifndef DISABLE_TALKIE
 #ifdef KEEP_MINIMUM_TALKIE_ONLY
-  talkie.Say(talkie_no_15, 15);
-  talkie.Say(talkie_sd_15, 15);
+  talkie.Say(spNO);
+  talkie.Say(spS);
+  talkie.Say(spD);
 #else // KEEP_MINIMUM_TALKIE_ONLY
   talkie.Say(talkie_sd_card_15, 15);
   talkie.Say(talkie_not_found_15, 15);
 #endif // KEEP_MINIMUM_TALKIE_ONLY
 #else // DISABLE_TALKIE
-  beeper.Beep(0.5, 261.63 * 2); // C4
-  beeper.Beep(0.5, 293.66 * 2); // D4
-  beeper.Beep(0.5, 261.63 * 2); // C4
-  beeper.Beep(0.5, 196.00 * 2); // G3
-  beeper.Beep(1.0, 130.81 * 2); // C3
+  beeper.Beep(0.5, 261.63 * 2); // C4 - S
+  beeper.Beep(0.5, 293.66 * 2); // D4 - D
+  beeper.Beep(0.5, 261.63 * 2); // C4 - card
+  beeper.Beep(0.5, 196.00 * 2); // G3 - not
+  beeper.Beep(1.0, 130.81 * 2); // C3 - found
 #endif // DISABLE_TALKIE
 #endif
 }
@@ -47,31 +54,40 @@ void ProffieOSErrors::sd_card_not_found() {
 void ProffieOSErrors::font_directory_not_found() {
   SaberBase::DoEffect(EFFECT_FONT_DIRECTORY_NOT_FOUND, 0);
 #ifdef ENABLE_AUDIO
+  // play the fonts "font directory not found" sound if it exists
+  if (SFX_erfntdnf) {
+    hybrid_font.PlayCommon(&SFX_erfntdnf);
+    return;
+  }
 #ifndef DISABLE_TALKIE
 #ifdef KEEP_MINIMUM_TALKIE_ONLY
-  talkie.Say(talkie_no_15, 15);
-  talkie.Say(talkie_font_15, 15);
+  talkie.Say(spNO);
+  talkie.Say(spF);
+  talkie.Say(spO);
+  talkie.Say(spN);
+  talkie.Say(spT);
+  //talkie.Say(talkie_font_15, 15); // this one is garbage (place holder)
 #else // KEEP_MINIMUM_TALKIE_ONLY
   talkie.Say(talkie_font_directory_15, 15);
   talkie.Say(talkie_not_found_15, 15);
 #endif // KEEP_MINIMUM_TALKIE_ONLY
 #else // DISABLE_TALKIE
-  beeper.Beep(0.5, 261.63 * 2); // C4
-  beeper.Beep(0.5/3, 246.94 * 2); // B3
-  beeper.Beep(0.5/3, 220.00 * 2); // A3
-  beeper.Beep(0.5/3, 196.00 * 2); // G3
-  beeper.Beep(0.5, 174.61 * 2); // F3
-  beeper.Beep(0.5, 146.83 * 2); // D3
-  beeper.Beep(0.5, 130.81 * 2); // C3
+  beeper.Beep(0.5, 261.63 * 2);   // C4 -
+  beeper.Beep(0.5/3, 246.94 * 2); // B3 -
+  beeper.Beep(0.5/3, 220.00 * 2); // A3 -
+  beeper.Beep(0.5/3, 196.00 * 2); // G3 -
+  beeper.Beep(0.5, 174.61 * 2);   // F3 -
+  beeper.Beep(0.5, 146.83 * 2);   // D3 - not
+  beeper.Beep(0.5, 130.81 * 2);   // C3 - found
 #endif // DISABLE_TALKIE
 #endif
 }
 
 void ProffieOSErrors::error_in_blade_array() {
   SaberBase::DoEffect(EFFECT_ERROR_IN_BLADE_ARRAY, 0);
-  STDOUT.println("BAD BLADE");
+  STDOUT.println("BAD BLADE"); // should we replace STDOUT.println with PVLOG_ERROR ?
 #ifdef ENABLE_AUDIO
-  // play the fonts error in blade array sound if it exists
+  // play the fonts "error in blade array" sound if it exists
   if (SFX_erbladar) {
     hybrid_font.PlayCommon(&SFX_erbladar);
     return;
@@ -84,10 +100,10 @@ void ProffieOSErrors::error_in_blade_array() {
   beeper.Beep(0.25, 196.00 * 2); // G3 - ror
   beeper.Beep(0.25, 174.61 * 2); // F3 - in
   beeper.Beep(0.25, 164.81 * 2); // E3 - the
-  beeper.Beep(0.3, 146.83 * 2); // D3 - blade
+  beeper.Beep(0.3, 146.83 * 2);  // D3 - blade
   beeper.Beep(0.2, 0);
-  beeper.Beep(0.5, 146.83 * 2); // D3 - ar
-  beeper.Beep(1.0, 130.81 * 2); // C3 - ray
+  beeper.Beep(0.5, 146.83 * 2);  // D3 - ar
+  beeper.Beep(1.0, 130.81 * 2);  // C3 - ray
 #endif // KEEP_REGULAR_TALKIE
 #endif
 }
@@ -95,24 +111,33 @@ void ProffieOSErrors::error_in_blade_array() {
 void ProffieOSErrors::error_in_font_directory() {
   SaberBase::DoEffect(EFFECT_ERROR_IN_FONT_DIRECTORY, 0);
 #ifdef ENABLE_AUDIO
+  // play the fonts "error in font directory" sound if it exists
+  if (SFX_erinftdr) {
+    hybrid_font.PlayCommon(&SFX_erinftdr);
+    return;
+  }
 #ifndef DISABLE_TALKIE
 #ifdef KEEP_MINIMUM_TALKIE_ONLY
-  talkie.Say(talkie_font_15, 15);
-  talkie.Say(talkie_error_15, 15);
+  talkie.Say(spF);
+  talkie.Say(spO);
+  talkie.Say(spN);
+  talkie.Say(spT);
+  //talkie.Say(talkie_font_15, 15); // this one is garbage (place holder)
+  talkie.Say(spERROR);
 #else // KEEP_MINIMUM_TALKIE_ONLY
   talkie.Say(talkie_error_in_15, 15);
   talkie.Say(talkie_font_directory_15, 15);
 #endif // KEEP_MINIMUM_TALKIE_ONLY
 #else // DISABLE_TALKIE
-  beeper.Beep(0.25, 174.61 * 2); // F3
-  beeper.Beep(0.25, 196.0 * 2); // G3
-  beeper.Beep(0.25, 174.61 * 2); // F3
-  beeper.Beep(0.25, 164.81 * 2); // E3
-  beeper.Beep(0.5, 146.83 * 2); // D3
-  beeper.Beep(0.5, 164.81 * 2); // E3
-  beeper.Beep(0.5, 196.0 * 2); // G3
-  beeper.Beep(0.5, 246.94 * 2); // B3
-  beeper.Beep(0.5, 261.63 * 2); // C4
+  beeper.Beep(0.25, 174.61 * 2); // F3 - Er
+  beeper.Beep(0.25, 196.00 * 2); // G3 - ror
+  beeper.Beep(0.25, 174.61 * 2); // F3 - in
+  beeper.Beep(0.25, 164.81 * 2); // E3 -
+  beeper.Beep(0.5, 146.83 * 2);  // D3 - 
+  beeper.Beep(0.5, 164.81 * 2);  // E3 -
+  beeper.Beep(0.5, 196.00 * 2);  // G3 -
+  beeper.Beep(0.5, 246.94 * 2);  // B3 -
+  beeper.Beep(0.5, 261.63 * 2);  // C4 -
 #endif // DISABLE_TALKIE
 #endif
 }
@@ -129,6 +154,52 @@ void ProffieOSErrors::low_battery() {
 #else // KEEP_REGULAR_TALKIE
   beeper.Beep(1.0, 261.63 * 2); // C4
   beeper.Beep(1.0, 130.81 * 2); // C3
+#endif // KEEP_REGULAR_TALKIE
+#endif
+}
+
+void ProffieOSErrors::voice_pack_not_found() {
+  STDOUT.println("YOUR VOICE PACK IS MISSING"); // should we replace STDOUT.println with PVLOG_ERROR ?
+#ifdef ENABLE_AUDIO
+  // play the fonts "voice pack not found" sound if it exists
+  if (SFX_ervpntfd) {
+    hybrid_font.PlayCommon(&SFX_ervpntfd);
+    return;
+  }
+#ifdef KEEP_REGULAR_TALKIE
+  talkie.Say(talkie_voice_pack_15, 15);
+  talkie.Say(talkie_not_found_15, 15);
+#else // KEEP_REGULAR_TALKIE
+  // insert appropriate beeper.Beep codes here // ?? - voi
+  // insert appropriate beeper.Beep codes here // ?? - ce
+  // insert appropriate beeper.Beep codes here // ?? - pack
+  beeper.Beep(0.5, 146.83 * 2);   // D3 - not
+  beeper.Beep(0.5, 130.81 * 2);   // C3 - found
+#endif // KEEP_REGULAR_TALKIE
+#endif
+}
+
+void ProffieOSErrors::error_in_voice_pack_version() {
+  STDOUT.println("INCORRECT VOICE PACK VERSION"); // should we replace STDOUT.println with PVLOG_ERROR ?
+#ifdef ENABLE_AUDIO
+  // play the fonts "error in voicepack version" sound if it exists
+  if (SFX_erinvpvr) {
+    hybrid_font.PlayCommon(&SFX_erinvpvr);
+    return;
+  }
+#ifdef KEEP_REGULAR_TALKIE
+  talkie.Say(talkie_error_in_15, 15);
+  talkie.Say(talkie_voice_pack_15, 15);
+  talkie.Say(talkie_version_15 ,15);
+#else // KEEP_REGULAR_TALKIE
+  beeper.Beep(0.25, 174.61 * 2); // F3 - Er
+  beeper.Beep(0.25, 196.00 * 2); // G3 - ror
+  beeper.Beep(0.25, 174.61 * 2); // F3 - in
+  // insert appropriate beeper.Beep codes here // ?? - voi
+  // insert appropriate beeper.Beep codes here // ?? - ce
+  // insert appropriate beeper.Beep codes here // ?? - pack
+  // insert appropriate beeper.Beep codes here // ?? - ver
+  // insert appropriate beeper.Beep codes here // ?? - sion
 #endif // KEEP_REGULAR_TALKIE
 #endif
 }
