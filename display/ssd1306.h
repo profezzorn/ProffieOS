b1fc2f067fd14e375e7fd140ecd4b3a9ddf274cf
Olivier <olivierflying321@gmail.com>
2026-02-04 17:38:17 +0100

    ssd1306.h I missed a space

--- display/ssd1306.h
+++ display/ssd1306.h
@@ -1230,4 +1230,4 @@ constexpr uint8_t SSD1306Template<WIDTH, col_t, POWER_PIN>::transactions[];
 
 using SSD1306 = SSD1306Template<128, uint32_t>;
 
-#endif // DISPLAY_SSD1306_H
+#endif  // DISPLAY_SSD1306_H
